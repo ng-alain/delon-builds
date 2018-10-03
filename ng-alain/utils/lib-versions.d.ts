a6756632222b1c9bcd0e38b6071209e26592c3c9
卡色 <cipchk@qq.com>
2018-10-03 07:51:01 +0000

    master - 969f29b Merge bb7009adc3535ef982ea121f2b57f8ff4f44b7ab into f0e96f66437748dbd61283e91838b8085a14e287

--- ng-alain/utils/lib-versions.d.ts
+++ ng-alain/utils/lib-versions.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "^2.0.0-beta.3-ed90aa6";
+export declare const VERSION = "^2.0.0-beta.3-969f29b";
 export declare const ZORROVERSION = "^1.6.0";
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^2.0.0-beta.3-ed90aa6';
+export const VERSION = '^2.0.0-beta.3-969f29b';
 export const ZORROVERSION = '^1.6.0';
