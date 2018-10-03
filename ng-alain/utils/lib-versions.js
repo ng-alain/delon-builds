85d71b326040fe6fd1b27bb53e62a4d3e27fef82
cipchk <cipchk@qq.com>
2018-10-03 07:39:17 +0000

    dev-ci - bb7009a fix Permission

--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = '^2.0.0-beta.3-ed90aa6';
+exports.VERSION = '^2.0.0-beta.3-bb7009a';
 exports.ZORROVERSION = '^1.6.0';
 //# sourceMappingURL=lib-versions.js.map
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^2.0.0-beta.3-ed90aa6';
+export const VERSION = '^2.0.0-beta.3-bb7009a';
 export const ZORROVERSION = '^1.6.0';
