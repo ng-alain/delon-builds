9c7819a50a822c68ed840eb587d117fb04b80463
lovelangy <langwang@outlook.com>
2018-10-04 11:48:28 +0000

    master - eb7a5eb doc: fix typo in doc (#198)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1645,7 +1645,7 @@ AlainThemeModule.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.0-beta.3-1545178');
+const VERSION = new Version('2.0.0-beta.3-eb7a5eb');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = '^2.0.0-beta.3-1545178';
+exports.VERSION = '^2.0.0-beta.3-eb7a5eb';
 exports.ZORROVERSION = '^1.6.0';
 //# sourceMappingURL=lib-versions.js.map
