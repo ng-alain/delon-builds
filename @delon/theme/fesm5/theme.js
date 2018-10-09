36fb46b46417bb086eb397d5461c819b99b42cc2
卡色 <cipchk@qq.com>
2018-10-09 08:23:23 +0000

    master - 9fc4540 Merge b96ea57caca68e65b5ff1ba5f8bfaf8851a9a4a7 into 832f596472c23174d5b17168dfb2748b561467a3

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2192,7 +2192,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-beta.4-8bd4540');
+var VERSION = new Version('2.0.0-beta.4-9fc4540');
 
 /**
  * @fileoverview added by tsickle
