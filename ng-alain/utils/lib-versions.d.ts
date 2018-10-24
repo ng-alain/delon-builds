3115cb6028d58259a635936bcfbda657a9e503d1
卡色 <cipchk@qq.com>
2018-10-24 06:56:17 +0000

    master - 549f25f Merge 1d7a9ecba9e06cbb1ca144ccd9e6067141f05b78 into 0d70d26db4608a0a76ee84c4395f5fafc8451a87

--- ng-alain/utils/lib-versions.d.ts
+++ ng-alain/utils/lib-versions.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "^2.0.0-beta.5-c53ff66";
-export declare const ZORROVERSION = "^1.7.0";
+export declare const VERSION = "^2.0.0-beta.5-549f25f";
+export declare const ZORROVERSION = "^1.7.1";
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = '^2.0.0-beta.5-c53ff66';
-exports.ZORROVERSION = '^1.7.0';
+exports.VERSION = '^2.0.0-beta.5-549f25f';
+exports.ZORROVERSION = '^1.7.1';
 //# sourceMappingURL=lib-versions.js.map
