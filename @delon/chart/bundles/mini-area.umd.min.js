0b31b32fe1e5ae0bc18c5613d8791dda0819bdc2
卡色 <cipchk@qq.com>
2018-10-27 14:34:44 +0000

    master - 9ac74e9 Merge 8b9957678796cd6e50e6e6b0a2589660d40ab5a1 into 9f3399bee58836b53de02c7206742482b5317c7f

--- @delon/chart/bundles/mini-area.umd.min.js
+++ @delon/chart/bundles/mini-area.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v2.0.0-rc.1-9daf945
+ * @license ng-alain(cipchk@qq.com) v2.0.0-rc.1-9ac74e9
  * (c) 2018 Cipchk https://ng-alain.com/
  * License: MIT
  */
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^2.0.0-rc.1-9daf945';
+export const VERSION = '^2.0.0-rc.1-9ac74e9';
 export const ZORROVERSION = '^1.7.1';
