fbe4f03de5029b7d2b705d6593fc1747aa332439
卡色 <cipchk@qq.com>
2018-11-01 06:13:19 +0000

    master - 5384305 release(2.0.0-rc.2): release 2.0.0-rc.2 (#250)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2239,7 +2239,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.2-2efb2ac');
+var VERSION = new Version('2.0.0-rc.2');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.d.ts
+++ ng-alain/utils/lib-versions.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "^2.0.0-rc.2-2efb2ac";
+export declare const VERSION = "^2.0.0-rc.2";
 export declare const ZORROVERSION = "^1.8.0";
