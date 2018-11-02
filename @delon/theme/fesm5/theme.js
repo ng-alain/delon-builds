cb9fa8c3615bb84d1f1b703c8be0c330ca819137
卡色 <cipchk@qq.com>
2018-11-02 08:55:25 +0000

    master - b703806 Merge 43d80aeb2f839b613b940c3a0bc15affa2701578 into b64cb562c8f39c3d22c52c94cc25871b1f1ca55a

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2239,7 +2239,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.2-6d719e1');
+var VERSION = new Version('2.0.0-rc.2-b703806');
 
 /**
  * @fileoverview added by tsickle
