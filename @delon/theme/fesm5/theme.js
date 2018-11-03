7732f37e3d9bf67065eaf8c9e4da70e101df3063
卡色 <cipchk@qq.com>
2018-11-03 05:13:37 +0000

    master - 6707b75 Merge 462034ef822fe7414ee28e44bec58694852fbaf8 into b64cb562c8f39c3d22c52c94cc25871b1f1ca55a

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2239,7 +2239,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.2-3cab3bb');
+var VERSION = new Version('2.0.0-rc.2-6707b75');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.d.ts
+++ ng-alain/utils/lib-versions.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "^2.0.0-rc.2-3cab3bb";
+export declare const VERSION = "^2.0.0-rc.2-6707b75";
 export declare const ZORROVERSION = "^1.8.0";
