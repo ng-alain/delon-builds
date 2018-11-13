e50b15975540d1900bc2580fb87669e4a2c4aff9
卡色 <cipchk@qq.com>
2018-11-13 14:32:17 +0000

    master - 1c17ff8 fix(module:sf): fix missing size property of upload widget (#274)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2274,7 +2274,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.3-fd7a7e3');
+var VERSION = new Version('2.0.0-rc.3-1c17ff8');
 
 /**
  * @fileoverview added by tsickle
