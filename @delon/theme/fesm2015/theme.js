6abc305177155c796eac71a4bf9cf4c45ae2df76
卡色 <cipchk@qq.com>
2018-11-14 04:50:11 +0000

    master - acf2005 Merge f747a452f2394f63c8ed3799862147ebe64a46b1 into 1c17ff88c49871575ac35ea6f4ece8af6d5ab0bc

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1725,7 +1725,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.0-rc.3-1c17ff8');
+const VERSION = new Version('2.0.0-rc.3-acf2005');
 
 /**
  * @fileoverview added by tsickle
