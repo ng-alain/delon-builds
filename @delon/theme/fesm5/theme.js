c4c268cec5637419c58d136ef4a0e893c9b3ddf4
卡色 <cipchk@qq.com>
2018-11-15 09:01:14 +0000

    master - 5a31da5 Merge a63bead0acd692e4eebf72715c430d0e3065f2f6 into 6c5e010c87cc68da81de1181ae6ed93fe55a039a

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2274,7 +2274,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.3-f403ade');
+var VERSION = new Version('2.0.0-rc.3-5a31da5');
 
 /**
  * @fileoverview added by tsickle
