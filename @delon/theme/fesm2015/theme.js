097c84e137ef9de5cd4805ff804460dc7fc29542
卡色 <cipchk@qq.com>
2018-11-15 07:22:30 +0000

    master - 6c5e010 fix(module:sf:checkbox): fix delayed rendering when onpush strategy (#278)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1725,7 +1725,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.0-rc.3-6c2befc');
+const VERSION = new Version('2.0.0-rc.3-6c5e010');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2274,7 +2274,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.3-6c2befc');
+var VERSION = new Version('2.0.0-rc.3-6c5e010');
 
 /**
  * @fileoverview added by tsickle
