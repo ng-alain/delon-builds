ab75f6704f98fef7719d2c46b02ec9b80e1e54ec
卡色 <cipchk@qq.com>
2018-11-17 16:34:12 +0000

    master - 1ecaed1 Merge 23cb045dbadc1d2f4588ad61d63aecc3ee298fa8 into 2ea151528819a28c6597cf94147846af160627bb

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2274,7 +2274,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.0-rc.3-2ea1515');
+var VERSION = new Version('2.0.0-rc.3-1ecaed1');
 
 /**
  * @fileoverview added by tsickle
