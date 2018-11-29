e614596a2023f0e806f2e8acbadcafba0d013dea
卡色 <cipchk@qq.com>
2018-11-29 08:24:45 +0000

    master - 389fd92 Merge abf769fb41b52f318d2ca16f13abcc7b53b8febe into 839147e8ef15c24f416d355c567fae1ebe3f66a1

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1975,7 +1975,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.1-7ef450b');
+const VERSION = new Version('2.0.1-389fd92');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2536,7 +2536,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.1-7ef450b');
+var VERSION = new Version('2.0.1-389fd92');
 
 /**
  * @fileoverview added by tsickle
