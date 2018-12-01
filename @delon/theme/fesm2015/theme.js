5e3031f195d631d59276d5ed4749954d1c38da58
卡色 <cipchk@qq.com>
2018-12-01 13:10:57 +0000

    master - 96430d1 fix(module:st): should clean filter style when not filter (#303)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1975,7 +1975,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.1-02622e4');
+const VERSION = new Version('2.0.1-96430d1');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2536,7 +2536,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.1-02622e4');
+var VERSION = new Version('2.0.1-96430d1');
 
 /**
  * @fileoverview added by tsickle
