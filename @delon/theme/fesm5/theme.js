af991ee7b9ffbb7c03388791b71ceabb9f2b051e
卡色 <cipchk@qq.com>
2018-12-11 11:38:06 +0000

    master - 1b12bb8 Merge 9d2b83fe044db7f8756eaa388db4fedfdb5a74c2 into 032fd818e870734afabab47710f071807f1d3fc6

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2584,7 +2584,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.1-298b66f');
+var VERSION = new Version('2.0.1-1b12bb8');
 
 /**
  * @fileoverview added by tsickle
