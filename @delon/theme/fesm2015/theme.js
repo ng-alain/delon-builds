5c0f84969a68ed24febebf5e8bc05f81b6cea839
卡色 <cipchk@qq.com>
2018-12-13 13:29:52 +0000

    master - 325cdfe Merge 3fbe1002dea3458671ed7e77622ef02dc0574992 into 032fd818e870734afabab47710f071807f1d3fc6

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -1994,7 +1994,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.1-bd752dc');
+const VERSION = new Version('2.0.1-325cdfe');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2576,7 +2576,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.1-bd752dc');
+var VERSION = new Version('2.0.1-325cdfe');
 
 /**
  * @fileoverview added by tsickle
