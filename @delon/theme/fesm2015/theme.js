f1516e9bc610fb09db00f37fa7c5e13b439c6bbb
卡色 <cipchk@qq.com>
2018-12-14 10:13:16 +0000

    master - 8cb86b5 fix(module:notice-icon): fix missing class (#317)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2000,7 +2000,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('2.0.1-c5fe4cc');
+const VERSION = new Version('2.0.1-8cb86b5');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2582,7 +2582,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('2.0.1-c5fe4cc');
+var VERSION = new Version('2.0.1-8cb86b5');
 
 /**
  * @fileoverview added by tsickle
