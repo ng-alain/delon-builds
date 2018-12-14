aeb2e9bd93d48492f8f98888ddba66cd0549ca39
卡色 <cipchk@qq.com>
2018-12-14 14:29:16 +0000

    master - 84116ea Merge e94579b0b431019f411976b35f73c3de8aba9304 into f214304898768fb85c64d7bd9780f4ba397a5337

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2000,7 +2000,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0-c397a2e');
+const VERSION = new Version('7.0.0-rc.0-84116ea');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2582,7 +2582,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-c397a2e');
+var VERSION = new Version('7.0.0-rc.0-84116ea');
 
 /**
  * @fileoverview added by tsickle
