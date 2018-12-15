d036120a67ffffca81679ce697ce12be4ff4548d
卡色 <cipchk@qq.com>
2018-12-15 10:35:07 +0000

    master - d77e8e9 feat(module:mock): add executeOtherInterceptors config (#321)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2625,7 +2625,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-9e56b2e');
+var VERSION = new Version('7.0.0-rc.0-d77e8e9');
 
 /**
  * @fileoverview added by tsickle
