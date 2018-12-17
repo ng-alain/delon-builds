188f0883f794a9ae7ae804e6829febacbfbefe95
卡色 <cipchk@qq.com>
2018-12-17 03:51:43 +0000

    master - e8d9ed1 feat(module:util): export stringTemplateOutlet (#324)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2621,7 +2621,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-4e424d2');
+var VERSION = new Version('7.0.0-rc.0-e8d9ed1');
 
 /**
  * @fileoverview added by tsickle
