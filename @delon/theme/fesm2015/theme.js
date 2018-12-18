d00ed306d3464efada6f3a884fd178cff48e828c
卡色 <cipchk@qq.com>
2018-12-18 11:48:30 +0000

    master - 45d3d31 Merge 345a1f3fa40d2312a9612f564b56abe730ff94e1 into a8a6695bf038f19c42f9ca883b648e59c65c08f7

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2039,7 +2039,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0-5666315');
+const VERSION = new Version('7.0.0-rc.0-45d3d31');
 
 /**
  * @fileoverview added by tsickle
