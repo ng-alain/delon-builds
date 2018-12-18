aeacd7988a08d27e8abcd6186aa073802ed13074
卡色 <cipchk@qq.com>
2018-12-18 12:54:24 +0000

    master - 4e681dc feat(module:st): support response body is an array type (#327)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2621,7 +2621,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-ff5afcb');
+var VERSION = new Version('7.0.0-rc.0-4e681dc');
 
 /**
  * @fileoverview added by tsickle
