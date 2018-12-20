87dce80c827ebf8b13c7cd6d99966c5bb7ffd785
卡色 <cipchk@qq.com>
2018-12-20 09:08:13 +0000

    master - 6c8c2d1 fix(module:st): should recalculate no value when call removeRow method (#331)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2621,7 +2621,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-53c86a8');
+var VERSION = new Version('7.0.0-rc.0-6c8c2d1');
 
 /**
  * @fileoverview added by tsickle
