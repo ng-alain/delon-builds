3179523191436ee8de38f9c37af08934f331c639
卡色 <cipchk@qq.com>
2018-12-22 08:53:59 +0000

    master - 95c9719 Merge fcdc8721e71e87bbcaadb29bbba2d5df57320623 into ffd365b129d9a45c05baaf465196a3b9b2743fb1

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2621,7 +2621,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-2814589');
+var VERSION = new Version('7.0.0-rc.0-95c9719');
 
 /**
  * @fileoverview added by tsickle
