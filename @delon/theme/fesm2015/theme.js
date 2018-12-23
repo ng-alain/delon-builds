d8a8b01a031047cf0885c7c1283a487ec5ebbd5e
卡色 <cipchk@qq.com>
2018-12-23 05:43:06 +0000

    master - 451156c Merge 4b280c10096fbf1e522b0f557a2cfb0796693080 into 0510864b2ee1eeb9b8fedaf017e686425030b453

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2039,7 +2039,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0-2bed027');
+const VERSION = new Version('7.0.0-rc.0-451156c');
 
 /**
  * @fileoverview added by tsickle
