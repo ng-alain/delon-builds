ec7d5b8df86a7ffbdb7c1eff51fe5a65f9ea7180
卡色 <cipchk@qq.com>
2018-12-23 13:00:16 +0000

    master - 2eab515 Merge 299ec56daa83427375383b3e85664c1467d202a9 into abf84db4c030674dbe8649cd3c4ee98d7d4372b5

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2621,7 +2621,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.0-abf84db');
+var VERSION = new Version('7.0.0-rc.0-2eab515');
 
 /**
  * @fileoverview added by tsickle
