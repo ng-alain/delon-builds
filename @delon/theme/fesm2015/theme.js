ae7712a720f1b55b8cc48327ebad0da8730fd0bb
卡色 <cipchk@qq.com>
2018-12-24 09:35:17 +0000

    master - ebbeed3 feat(module:sidebar-nav): add disabledAcl property (#337)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2040,7 +2040,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0-f24a648');
+const VERSION = new Version('7.0.0-rc.0-ebbeed3');
 
 /**
  * @fileoverview added by tsickle
