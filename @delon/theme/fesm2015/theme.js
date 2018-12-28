2b1bf7cf03e8ae13b0304d2b75c0c85cfd546a3d
卡色 <cipchk@qq.com>
2018-12-28 07:17:39 +0000

    master - 862d8f4 feat(module:reuse-tab): add keeping scroll property (#347)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2061,7 +2061,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1-35fe182a');
+const VERSION = new Version('7.0.0-rc.1-862d8f4');
 
 /**
  * @fileoverview added by tsickle
