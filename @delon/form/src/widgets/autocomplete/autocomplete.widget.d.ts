9aa2e853588bd633894f69964a11a2f504759608
卡色 <cipchk@qq.com>
2019-01-03 06:58:57 +0000

    master - 667df71 Merge 2271dfd61a5bda829d4a981467e7676f2d7319e9 into da6d6d69b264694a71c936843cacd8ee89713d10

--- @delon/form/src/widgets/autocomplete/autocomplete.widget.d.ts
+++ @delon/form/src/widgets/autocomplete/autocomplete.widget.d.ts
@@ -1,16 +1,20 @@
-import { OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { NzAutocompleteOptionComponent } from 'ng-zorro-antd';
 import { Observable } from 'rxjs';
 import { SFValue } from '../../interface';
 import { SFSchemaEnum } from '../../schema';
 import { ControlWidget } from '../../widget';
 export declare const EMAILSUFFIX: string[];
-export declare class AutoCompleteWidget extends ControlWidget implements OnInit {
+export declare class AutoCompleteWidget extends ControlWidget implements AfterViewInit {
     i: any;
     fixData: SFSchemaEnum[];
     list: Observable<SFSchemaEnum[]>;
+    typing: string;
+    private ngModel;
     private filterOption;
     private isAsync;
-    ngOnInit(): void;
+    updateValue(item: NzAutocompleteOptionComponent): void;
+    ngAfterViewInit(): void;
     reset(value: SFValue): void;
     private filterData;
     private addEmailSuffix;
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2669,7 +2669,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.3-0352618');
+var VERSION = new Version('7.0.0-rc.3-667df71');
 
 /**
  * @fileoverview added by tsickle
