66c251cee2551dfcdd4b73f2c70d9e85e7694402
卡色 <cipchk@qq.com>
2019-01-04 04:57:38 +0000

    master - cba2dba2 Merge 8fd3f7ab25946bd26c4f06017e6f0531c08f5a1b into dda74ff2a568335e9960e065cf98edfb4171b1ee

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2669,7 +2669,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.3-dda74ff');
+var VERSION = new Version('7.0.0-rc.3-cba2dba2');
 
 /**
  * @fileoverview added by tsickle
