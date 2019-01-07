a5cb7f7f1b78e6976b72f4ffc71dc851143d6da2
卡色 <cipchk@qq.com>
2019-01-07 06:45:54 +0000

    master - 54e7a907 Merge 5a4a78e6b6499ae1495bdfe4a8714e3d66e794e5 into c5431f8e0b8d4a90cd83c82f2065f04a8f1b8d50

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2067,7 +2067,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.3-c5431f8');
+const VERSION = new Version('7.0.0-rc.3-54e7a907');
 
 /**
  * @fileoverview added by tsickle
