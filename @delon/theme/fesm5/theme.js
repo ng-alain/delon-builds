17916875d641fc6f6f7afffe2967e08ee2eb4c90
卡色 <cipchk@qq.com>
2019-01-11 13:31:02 +0000

    master - dff5085 refactor(module:ellipsis): refactor ellipsis component (#376)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2669,7 +2669,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.3-915e8c1');
+var VERSION = new Version('7.0.0-rc.3-dff5085');
 
 /**
  * @fileoverview added by tsickle
