7a4f36abe96582f74906cc0d6889cc653c3536b0
卡色 <cipchk@qq.com>
2019-01-15 11:05:46 +0000

    master - 19c06b35 Merge 748efa051e1ec6dc47350f93a19aba9c631a34d0 into 1f0226ce5cb83964dcc46479db4ba4dbd9d1f106

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2669,7 +2669,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.4-1f0226c');
+var VERSION = new Version('7.0.0-rc.4-19c06b35');
 
 /**
  * @fileoverview added by tsickle
