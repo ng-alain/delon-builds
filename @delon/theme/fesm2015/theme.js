7525475e108f8e52a080108ec2e3cf3854dafefa
卡色 <cipchk@qq.com>
2019-01-21 11:21:42 +0000

    master - afa25968 Merge da063417f7d894a17a0d5015c9771e1321ef7cde into 4411a196b2eadca21a8d799f53d6359825f4faeb

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2069,7 +2069,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.4-1da091c6');
+const VERSION = new Version('7.0.0-rc.4-afa25968');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2671,7 +2671,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.4-1da091c6');
+var VERSION = new Version('7.0.0-rc.4-afa25968');
 
 /**
  * @fileoverview added by tsickle
