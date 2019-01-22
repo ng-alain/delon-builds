4da63f51455f536ae7291d7b09d55d2259ebc467
卡色 <cipchk@qq.com>
2019-01-22 05:28:36 +0000

    master - 40bc605d Merge f303667f3254b1daac3a107a8bc655407c485c45 into bbf5fa186f84ec5c9bf1524b171d61c0067042d2

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2069,7 +2069,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6');
+const VERSION = new Version('7.0.0-rc.6-40bc605d');
 
 /**
  * @fileoverview added by tsickle
