868d355e6be1e1e014bd99a468016613f6a54e1a
卡色 <cipchk@qq.com>
2019-01-22 05:08:54 +0000

    master - bbf5fa1 release(7.0.0-rc.6): release 7.0.0-rc.6 (#402)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2069,7 +2069,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.5-e217940');
+const VERSION = new Version('7.0.0-rc.6');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^7.0.0-rc.5';
+export const VERSION = '^7.0.0-rc.6';
 export const ZORROVERSION = '^7.0.0-rc.3';
