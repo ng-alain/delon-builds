88876481fc39df692fb84680be6d61511aca44a0
卡色 <cipchk@qq.com>
2019-01-24 10:26:53 +0000

    master - 9c6be6c feat(module:st): add skip & limit pagination type (#404)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2671,7 +2671,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-2112b74');
+var VERSION = new Version('7.0.0-rc.6-9c6be6c');
 
 /**
  * @fileoverview added by tsickle
