86be43d495f3b99d5606b50da8f9a5ebfc37a2dd
卡色 <cipchk@qq.com>
2019-01-25 10:25:45 +0000

    master - f0cda6cc Merge 9913bcf00a2aaa4bf9042b9f671fee7a3483ff55 into 033713a95da485d4357b56e9376fe8d322c8e08b

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2666,7 +2666,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-67e6a472');
+var VERSION = new Version('7.0.0-rc.6-f0cda6cc');
 
 /**
  * @fileoverview added by tsickle
