fb5acc48d74a49576667d46e60acb566e26b8b12
卡色 <cipchk@qq.com>
2019-01-25 08:07:09 +0000

    master - 1b5bad71 Merge cd7dcdd9b8361035efe072c57f63d56cedaecf01 into 033713a95da485d4357b56e9376fe8d322c8e08b

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2069,7 +2069,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6-033713a');
+const VERSION = new Version('7.0.0-rc.6-1b5bad71');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2671,7 +2671,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-033713a');
+var VERSION = new Version('7.0.0-rc.6-1b5bad71');
 
 /**
  * @fileoverview added by tsickle
