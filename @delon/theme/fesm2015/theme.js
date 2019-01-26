4ee6604bfd82002e416c6ba4dfa6633f3761143e
卡色 <cipchk@qq.com>
2019-01-26 10:47:55 +0000

    master - b6901044 Merge bb343b1e6dd536e1467a3b77bca2df7ce0a8638c into 033713a95da485d4357b56e9376fe8d322c8e08b

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2064,7 +2064,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6-ac0b84bf');
+const VERSION = new Version('7.0.0-rc.6-b6901044');
 
 /**
  * @fileoverview added by tsickle
