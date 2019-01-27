733dc00afa514bff8e056ee2c8087b4b753cc873
卡色 <cipchk@qq.com>
2019-01-27 12:36:10 +0000

    master - 36cec09 Merge 9f1f129714c81146f96d3402a1aaab8d1491735b into 88def47d55068f002ff9dfe34c96cba44c45f19c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2064,7 +2064,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6-bb4c490');
+const VERSION = new Version('7.0.0-rc.6-36cec09');
 
 /**
  * @fileoverview added by tsickle
