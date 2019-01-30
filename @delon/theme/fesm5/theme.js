406326db5908066d19995365d694c15f7c70aebb
卡色 <cipchk@qq.com>
2019-01-30 05:09:41 +0000

    master - 42d8519 Merge 2c10ccfc0962291c7c1029e75b2d776e6c47f12e into 2c3ce0abba7f391f1e9d31966d79e7f13140ecfd

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2666,7 +2666,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-d09db00');
+var VERSION = new Version('7.0.0-rc.6-42d8519');
 
 /**
  * @fileoverview added by tsickle
