6d6a5058b5b87746b9af8e98abdabbadf1e28875
卡色 <cipchk@qq.com>
2019-02-01 10:22:49 +0000

    master - f6812b1 Merge 78ea67cb0c9e4d6e23789bc5e5c99001ecdf6d8f into 8b1b5812a0a3d702a8a119f2caf32e2784085200

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2064,7 +2064,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6-6c1279e');
+const VERSION = new Version('7.0.0-rc.6-f6812b1');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2666,7 +2666,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-6c1279e');
+var VERSION = new Version('7.0.0-rc.6-f6812b1');
 
 /**
  * @fileoverview added by tsickle
