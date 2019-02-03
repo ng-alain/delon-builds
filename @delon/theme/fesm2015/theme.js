c332c03ee07320f4d37b8e3b70f6de3704b0be26
卡色 <cipchk@qq.com>
2019-02-03 13:18:43 +0000

    master - 50f45b33 Merge df4efcee46c54a850de66384a1b3dd9e25434134 into 62a5749a8a567cfb7b1ee2ecdf86088f0091d674

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2064,7 +2064,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.6-659471fb');
+const VERSION = new Version('7.0.0-rc.6-50f45b33');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2666,7 +2666,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.6-659471fb');
+var VERSION = new Version('7.0.0-rc.6-50f45b33');
 
 /**
  * @fileoverview added by tsickle
