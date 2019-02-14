6069e18c99750352f6728efef6c21bc6dd76666f
卡色 <cipchk@qq.com>
2019-02-14 09:01:21 +0000

    master - fe4f5d9 Merge 829749a1c423127f7038483cb3101dabf98598a5 into 087c979c2ca137a209fd00015e2b03d2eea85585

--- @delon/theme/bundles/theme.umd.js
+++ @delon/theme/bundles/theme.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v7.0.0-rc.6
+ * @license ng-alain(cipchk@qq.com) v7.0.0-rc.7
  * (c) 2018 Cipchk https://ng-alain.com/
  * License: MIT
  */
@@ -2775,7 +2775,7 @@
      * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
      */
     /** @type {?} */
-    var VERSION = new i0.Version('7.0.0-rc.6');
+    var VERSION = new i0.Version('7.0.0-rc.7');
 
     /**
      * @fileoverview added by tsickle
