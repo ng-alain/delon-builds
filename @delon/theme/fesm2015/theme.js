89bb4dfb579720edf1050e600e49c3e3848881d1
卡色 <cipchk@qq.com>
2019-02-15 10:53:22 +0000

    master - 2b07901 fix(module:sf:checkbox): fix invalid indeterminate status (#433)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2062,7 +2062,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.7-3a6d900');
+const VERSION = new Version('7.0.0-rc.7-2b07901');
 
 /**
  * @fileoverview added by tsickle
