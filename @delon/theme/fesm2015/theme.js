820f246f6f1043d29bfaa613599c246638d39595
卡色 <cipchk@qq.com>
2019-02-15 05:04:12 +0000

    master - 25d83781 Merge d8818a9b5d95033eebce329eb94da4590cfeec50 into 2f83a4b83314ee31ef0e1c43e04d2416d9bef389

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2062,7 +2062,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.7-2f83a4b');
+const VERSION = new Version('7.0.0-rc.7-25d83781');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2664,7 +2664,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.7-2f83a4b');
+var VERSION = new Version('7.0.0-rc.7-25d83781');
 
 /**
  * @fileoverview added by tsickle
