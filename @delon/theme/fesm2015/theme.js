5ef756f303a30d475766b06ab6f9ed4c9e31f0aa
卡色 <cipchk@qq.com>
2019-02-18 14:20:33 +0000

    master - 136b27a chore: add robot config (#440)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2051,7 +2051,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.8-d3b790a');
+const VERSION = new Version('7.0.0-rc.8-136b27a');
 
 /**
  * @fileoverview added by tsickle
