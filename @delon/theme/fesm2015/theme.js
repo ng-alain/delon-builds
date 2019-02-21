75a11f58535af353a1be86b15dd2692e8fc26e05
卡色 <cipchk@qq.com>
2019-02-21 03:47:52 +0000

    master - 5d3c7ea fix(abc:sidebar): fix invalid menu item ellipsis when text overflow (#447)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2051,7 +2051,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.8-6eb89c75');
+const VERSION = new Version('7.0.0-rc.8-5d3c7ea');
 
 /**
  * @fileoverview added by tsickle
