315fdf59fcd2f551a4848509e1ec7dab60628ecc
卡色 <cipchk@qq.com>
2019-02-22 13:39:10 +0000

    master - f0f1408 Merge ef7a1c2c0a7e57bc28f0b19ad5ff698abfca7f31 into c5ce352e46fb489dfcb846bea4147525a7a6653c

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2641,7 +2641,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-rc.9-00a3615');
+var VERSION = new Version('7.0.0-rc.9-f0f1408');
 
 /**
  * @fileoverview added by tsickle
