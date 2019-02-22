a132bdfd61b7e27ad9c671755345c47984fd0306
卡色 <cipchk@qq.com>
2019-02-22 08:46:17 +0000

    master - 44b28fa8 Merge c619c4846208c9fbdf7e69b8e037abcfc0a563cf into ffeccdd3cf502dab68a830683b99849b4c9b6bb5

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2051,7 +2051,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.8-ffa59ae');
+const VERSION = new Version('7.0.0-rc.8-44b28fa8');
 
 /**
  * @fileoverview added by tsickle
