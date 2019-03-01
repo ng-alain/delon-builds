f0827c0219c279e3d23fcd1b44b0d428a4e3034c
卡色 <cipchk@qq.com>
2019-03-01 10:01:16 +0000

    master - b1c15d9 Merge 046e65d6b0b8f163555505e588525f5bdb1e0e62 into bd5696ab2931bad7a974640ee5a461d6ca871ebe

--- @delon/chart/bundles/mini-bar.umd.min.js
+++ @delon/chart/bundles/mini-bar.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v7.0.0
+ * @license ng-alain(cipchk@qq.com) v7.0.1
  * (c) 2019 cipchk https://ng-alain.com/
  * License: MIT
  */
--- @delon/theme/bundles/theme.umd.js
+++ @delon/theme/bundles/theme.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v7.0.0
+ * @license ng-alain(cipchk@qq.com) v7.0.1
  * (c) 2019 cipchk https://ng-alain.com/
  * License: MIT
  */
@@ -2922,7 +2922,7 @@
      * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
      */
     /** @type {?} */
-    var VERSION = new i0.Version('7.0.0');
+    var VERSION = new i0.Version('7.0.1');
 
     /**
      * @fileoverview added by tsickle
