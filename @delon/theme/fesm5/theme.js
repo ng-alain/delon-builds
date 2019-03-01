5bb9e2d222a91b2ae2e3a3bf59d64e0745b70445
卡色 <cipchk@qq.com>
2019-03-01 09:13:39 +0000

    master - d22ec60 release(7.0.0): release 7.0.0 (#459)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2848,7 +2848,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.0-aab8f51');
+var VERSION = new Version('7.0.0');
 
 /**
  * @fileoverview added by tsickle
