8316589f431707b7c38ab721b4c05f94da72c2b0
卡色 <cipchk@qq.com>
2019-03-20 13:41:56 +0000

    master - 76db123 Merge 481d845a7bc7170bd93b9474876e999b5a72d2f2 into ab34a44a106d9f1d69cd36d30413dc4308f27031

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2263,7 +2263,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.2-ab34a44');
+const VERSION = new Version('7.0.2-76db123');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2859,7 +2859,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.0.2-ab34a44');
+var VERSION = new Version('7.0.2-76db123');
 
 /**
  * @fileoverview added by tsickle
