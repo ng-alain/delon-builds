aca4e28ee1798d125f20ccf9771b1b5ebbb55dca
卡色 <cipchk@qq.com>
2019-03-23 06:37:02 +0000

    master - adfc5b3 build: fix build error when invlid user (#491)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2263,7 +2263,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.3-1535551');
+const VERSION = new Version('7.0.3-adfc5b3');
 
 /**
  * @fileoverview added by tsickle
