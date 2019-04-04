c222f23f4169a69854546fce9f0a65bd88adb967
卡色 <cipchk@qq.com>
2019-04-04 02:43:19 +0000

    master - 446ad12d Merge 426cb9caf9ec8650af85971656ecf4757ae92467 into ae20b2581a3b16eef9d2be21e81a9f64d72fac0e

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2306,7 +2306,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.1.0-ae20b25');
+const VERSION = new Version('7.1.0-446ad12d');
 
 /**
  * @fileoverview added by tsickle
