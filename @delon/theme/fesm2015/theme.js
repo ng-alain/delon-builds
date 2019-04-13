c89f141edb8c145f881dba9da11cfe96c8cba200
卡色 <cipchk@qq.com>
2019-04-13 00:59:44 +0000

    master - 715d5de feat(abc:st): add `widthMode` property (#518)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2306,7 +2306,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.2.0-8bd9304');
+const VERSION = new Version('7.2.0-715d5de');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2902,7 +2902,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.2.0-8bd9304');
+var VERSION = new Version('7.2.0-715d5de');
 
 /**
  * @fileoverview added by tsickle
