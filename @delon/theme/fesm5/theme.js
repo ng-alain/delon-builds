40d818a34860afd4bfe02bb5efaaa36a5276db91
卡色 <cipchk@qq.com>
2019-04-29 13:33:55 +0000

    master - 6d35fc4 chore: G2 is not defined (#544)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2936,7 +2936,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.3.0-c365a7b');
+var VERSION = new Version('7.3.0-6d35fc4');
 
 /**
  * @fileoverview added by tsickle
