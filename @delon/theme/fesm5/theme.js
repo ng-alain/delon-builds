4e3285c3e6d2cd3ee54421200e8603425d1d6f0f
卡色 <cipchk@qq.com>
2019-04-30 02:21:54 +0000

    master - 8d62194 Merge 99fa7b1b7ef10ea7cefa54a7da0b1b377e79b53f into 359159ca62734cdbb61cfba8b4e608872bfe5fc2

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -2936,7 +2936,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('7.3.0-359159c');
+var VERSION = new Version('7.3.1');
 
 /**
  * @fileoverview added by tsickle
