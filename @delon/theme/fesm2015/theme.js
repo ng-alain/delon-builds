0ac0f340c86187575e9f04752e9e072a62ff0604
卡色 <cipchk@qq.com>
2019-05-03 15:57:46 +0000

    master - 58a72af0 Merge 2cdb2ea70b4526334a5b4313aec73b547fc70c74 into 6d4c1cc2dfd708ae4fb1039a2acb47641740e184

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2327,7 +2327,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.3.1-6d4c1cc');
+const VERSION = new Version('7.3.2');
 
 /**
  * @fileoverview added by tsickle
