83498df0e4ebb675d88b02156d0e46de81d35caa
卡色 <cipchk@qq.com>
2019-05-06 02:13:13 +0000

    master - 4bd3486 Merge 03f0bdfaa63b598a3f283859d9488e8950a75978 into e447030f39db0865c0f49366389a68435e1d8bf8

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2327,7 +2327,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.3.2');
+const VERSION = new Version('7.3.2-4bd3486');
 
 /**
  * @fileoverview added by tsickle
