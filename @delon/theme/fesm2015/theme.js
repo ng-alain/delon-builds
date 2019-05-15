b9a995c4f65b97baa495e912f1c006cd26440f56
卡色 <cipchk@qq.com>
2019-05-15 02:44:44 +0000

    master - 56abb18 fix(form:select): fix invlaid tag count (#571)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2320,7 +2320,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.3.2-5ba1f82');
+const VERSION = new Version('7.3.2-56abb18');
 
 /**
  * @fileoverview added by tsickle
