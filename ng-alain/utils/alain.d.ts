aa22d180d11037f07be5a77a8f74fd8e155ec240
卡色 <cipchk@qq.com>
2019-06-20 16:32:06 +0000

    master - e1f94558 Merge 0898edab9a3b75773ecd32acb50f49b3b3351129 into d45fddf2be0e06d03551c59e763f9b6fe0e1876d

--- ng-alain/utils/alain.d.ts
+++ ng-alain/utils/alain.d.ts
@@ -14,7 +14,7 @@ export interface CommonSchema {
     prefix?: string;
     withoutPrefix?: boolean;
 }
-export declare function addValueToVariable(host: Tree, path: string, variableName: string, text: string): void;
+export declare function addValueToVariable(host: Tree, filePath: string, variableName: string, text: string): void;
 export declare function buildAlain(schema: CommonSchema): Rule;
-export declare function tryDelFile(host: Tree, path: string): void;
-export declare function tryAddFile(host: Tree, path: string, content: string): void;
+export declare function tryDelFile(host: Tree, filePath: string): void;
+export declare function tryAddFile(host: Tree, filePath: string, content: string): void;
--- ng-alain/utils/alain.js
+++ ng-alain/utils/alain.js
@@ -8,6 +8,8 @@ const find_module_1 = require("@schematics/angular/utility/find-module");
 const parse_name_1 = require("@schematics/angular/utility/parse-name");
 const validation_1 = require("@schematics/angular/utility/validation");
 const ts = require("typescript");
+const fs = require("fs");
+const path = require("path");
 const ast_1 = require("./ast");
 const project_1 = require("./project");
 function buildSelector(schema, projectPrefix) {
@@ -42,17 +44,6 @@ function buildComponentName(schema, _projectPrefix) {
     return core_1.strings.classify(ret.join('-'));
 }
 function resolveSchema(host, project, schema) {
-    if (!schema._filesPath) {
-        // 若基础页尝试从 `_cli-tpl/_${schema.schematicName!}` 下查找该目录，若存在则优先使用
-        if (['list', 'edit', 'view', 'empty'].includes(schema.schematicName)) {
-            const overrideDir = `/${project.root}/_cli-tpl/_${schema.schematicName}`;
-            const overridePath = `${overrideDir}/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts`;
-            if (host.exists(overridePath)) {
-                schema._filesPath = `.${overrideDir}`;
-            }
-        }
-        schema._filesPath = schema._filesPath || './files';
-    }
     // module name
     if (!schema.module) {
         throw new schematics_1.SchematicsException(`Must specify module name. (e.g: ng g ng-alain:list <list name> -m=<module name>)`);
@@ -66,7 +57,23 @@ function resolveSchema(host, project, schema) {
     const parsedPath = parse_name_1.parseName(schema.path, schema.name);
     schema.name = parsedPath.name;
     schema.path = parsedPath.path;
+    const fullPath = path.join(process.cwd(), schema.path, schema.name);
+    if (fs.existsSync(fullPath)) {
+        throw new Error(`The directory (${fullPath}) already exists`);
+    }
     schema.importModulePath = find_module_1.findModuleFromOptions(host, schema);
+    if (!schema._filesPath) {
+        // 若基础页尝试从 `_cli-tpl/_${schema.schematicName!}` 下查找该目录，若存在则优先使用
+        if (['list', 'edit', 'view', 'empty'].includes(schema.schematicName)) {
+            const overrideDir = '/' + [project.root, `_cli-tpl/_${schema.schematicName}`].filter(i => !!i).join('/');
+            const overridePath = `${overrideDir}/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts`;
+            if (host.exists(overridePath)) {
+                // 所在目录与命令目录同属一个目录结构，因此无须特殊处理
+                schema._filesPath = path.relative(__dirname, process.cwd()) + overrideDir;
+            }
+        }
+        schema._filesPath = schema._filesPath || './files';
+    }
     // fill target
     if (schema.target) {
         schema.path += '/' + schema.target;
@@ -77,29 +84,29 @@ function resolveSchema(host, project, schema) {
     validation_1.validateName(schema.name);
     validation_1.validateHtmlSelector(schema.selector);
 }
-function addImportToModule(host, path, symbolName, fileName) {
-    const source = ast_1.getSourceFile(host, path);
-    const change = ast_utils_1.insertImport(source, path, symbolName, fileName);
-    const declarationRecorder = host.beginUpdate(path);
+function addImportToModule(host, filePath, symbolName, fileName) {
+    const source = ast_1.getSourceFile(host, filePath);
+    const change = ast_utils_1.insertImport(source, filePath, symbolName, fileName);
+    const declarationRecorder = host.beginUpdate(filePath);
     declarationRecorder.insertLeft(change.pos, change.toAdd);
     host.commitUpdate(declarationRecorder);
 }
-function addValueToVariable(host, path, variableName, text) {
-    const source = ast_1.getSourceFile(host, path);
+function addValueToVariable(host, filePath, variableName, text) {
+    const source = ast_1.getSourceFile(host, filePath);
     const node = ast_utils_1.findNode(source, ts.SyntaxKind.Identifier, variableName);
     if (!node) {
-        throw new schematics_1.SchematicsException(`Could not find any [${variableName}] variable in path: ${path}.`);
+        throw new schematics_1.SchematicsException(`Could not find any [${variableName}] variable in path: ${filePath}.`);
     }
     const arr = node.parent.initializer;
-    const change = new change_1.InsertChange(path, arr.end - 1, `${arr.elements && arr.elements.length > 0 ? ',' : ''}\n  ${text}`);
-    const declarationRecorder = host.beginUpdate(path);
+    const change = new change_1.InsertChange(filePath, arr.end - 1, `${arr.elements && arr.elements.length > 0 ? ',' : ''}\n  ${text}`);
+    const declarationRecorder = host.beginUpdate(filePath);
     declarationRecorder.insertLeft(change.pos, change.toAdd);
     host.commitUpdate(declarationRecorder);
 }
 exports.addValueToVariable = addValueToVariable;
-function getRelativePath(path, schema) {
+function getRelativePath(filePath, schema) {
     const importPath = `/${schema.path}/${schema.flat ? '' : core_1.strings.dasherize(schema.name) + '/'}${core_1.strings.dasherize(schema.name)}.component`;
-    return find_module_1.buildRelativePath(path, importPath);
+    return find_module_1.buildRelativePath(filePath, importPath);
 }
 function addDeclaration(schema) {
     return (host) => {
@@ -129,10 +136,10 @@ function buildAlain(schema) {
         // Don't support inline
         schema.inlineTemplate = false;
         const templateSource = schematics_1.apply(schematics_1.url(schema._filesPath), [
-            schematics_1.filter(path => !path.endsWith('.DS_Store')),
-            schema.spec ? schematics_1.noop() : schematics_1.filter(path => !path.endsWith('.spec.ts')),
-            schema.inlineStyle ? schematics_1.filter(path => !path.endsWith('.__styleext__')) : schematics_1.noop(),
-            schema.inlineTemplate ? schematics_1.filter(path => !path.endsWith('.html')) : schematics_1.noop(),
+            schematics_1.filter(filePath => !filePath.endsWith('.DS_Store')),
+            schema.spec ? schematics_1.noop() : schematics_1.filter(filePath => !filePath.endsWith('.spec.ts')),
+            schema.inlineStyle ? schematics_1.filter(filePath => !filePath.endsWith('.__styleext__')) : schematics_1.noop(),
+            schema.inlineTemplate ? schematics_1.filter(filePath => !filePath.endsWith('.html')) : schematics_1.noop(),
             schematics_1.template(Object.assign({}, core_1.strings, { 'if-flat': (s) => (schema.flat ? '' : s) }, schema)),
             schematics_1.move(null, schema.path + '/'),
         ]);
@@ -140,15 +147,15 @@ function buildAlain(schema) {
     };
 }
 exports.buildAlain = buildAlain;
-function tryDelFile(host, path) {
-    if (host.exists(path)) {
-        host.delete(path);
+function tryDelFile(host, filePath) {
+    if (host.exists(filePath)) {
+        host.delete(filePath);
     }
 }
 exports.tryDelFile = tryDelFile;
-function tryAddFile(host, path, content) {
-    tryDelFile(host, path);
-    host.create(path, content);
+function tryAddFile(host, filePath, content) {
+    tryDelFile(host, filePath);
+    host.create(filePath, content);
 }
 exports.tryAddFile = tryAddFile;
 //# sourceMappingURL=alain.js.map
