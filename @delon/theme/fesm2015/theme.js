ce2269ea6a6ac3543728e2835c1eaba0e3819774
卡色 <cipchk@qq.com>
2019-08-31 10:16:19 +0000

    master - 42fcd3a Merge e3c5709108e6d1fe6f7ce1bba39be31b78cb4ccd into 48ca08f4f356f4804d0ebfc1048dbe4d25947887

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3368,7 +3368,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.3.0-b6c1fc6');
+const VERSION = new Version('8.3.0-42fcd3a');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -3956,7 +3956,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.3.0-b6c1fc6');
+var VERSION = new Version('8.3.0-42fcd3a');
 
 /**
  * @fileoverview added by tsickle
