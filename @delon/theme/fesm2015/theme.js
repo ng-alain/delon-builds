75a3897ad13275f66fee09935bc6baec063ea2de
卡色 <cipchk@qq.com>
2019-08-31 10:33:27 +0000

    master - 142b761 chore: update document site (#675)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3368,7 +3368,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.3.0-42fcd3a');
+const VERSION = new Version('8.3.0-142b761');
 
 /**
  * @fileoverview added by tsickle
