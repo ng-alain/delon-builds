2acba2b9d1c55bb8901888b8b2b9382dfcfe11fc
卡色 <cipchk@qq.com>
2019-09-03 13:33:16 +0000

    master - b1cfd65 docs: update style (#681)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3368,7 +3368,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.4.0-db4151d');
+const VERSION = new Version('8.4.0-b1cfd65');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -3956,7 +3956,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.4.0-db4151d');
+var VERSION = new Version('8.4.0-b1cfd65');
 
 /**
  * @fileoverview added by tsickle
