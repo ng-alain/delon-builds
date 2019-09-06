3834facf041a7849e72579370844ed8e3d14226d
卡色 <cipchk@qq.com>
2019-09-06 05:59:47 +0000

    master - 877541e Merge 3aa8eec797e5167d88d1d712311b8d51683ab0a8 into b1cfd65dd9a98c30e6945b4f889a454520fb76c1

--- ng-alain/utils/alain.d.ts
+++ ng-alain/utils/alain.d.ts
@@ -14,7 +14,7 @@ export interface CommonSchema {
     prefix?: string;
     withoutPrefix?: boolean;
 }
-export declare function addValueToVariable(host: Tree, filePath: string, variableName: string, text: string, needWrap?: boolean): void;
+export declare function addValueToVariable(host: Tree, filePath: string, variableName: string, text: string): void;
 export declare function buildAlain(schema: CommonSchema): Rule;
 export declare function tryDelFile(host: Tree, filePath: string): void;
 export declare function tryAddFile(host: Tree, filePath: string, content: string): void;
--- ng-alain/utils/alain.js
+++ ng-alain/utils/alain.js
@@ -91,14 +91,14 @@ function addImportToModule(host, filePath, symbolName, fileName) {
     declarationRecorder.insertLeft(change.pos, change.toAdd);
     host.commitUpdate(declarationRecorder);
 }
-function addValueToVariable(host, filePath, variableName, text, needWrap = true) {
+function addValueToVariable(host, filePath, variableName, text) {
     const source = ast_1.getSourceFile(host, filePath);
     const node = ast_utils_1.findNode(source, ts.SyntaxKind.Identifier, variableName);
     if (!node) {
         throw new schematics_1.SchematicsException(`Could not find any [${variableName}] variable in path '${filePath}'.`);
     }
     const arr = node.parent.initializer;
-    const change = new change_1.InsertChange(filePath, arr.end - 1, `${arr.elements && arr.elements.length > 0 ? ',' : ''}${needWrap ? '\n  ' : ''}${text}`);
+    const change = new change_1.InsertChange(filePath, arr.end - 1, `${arr.elements && arr.elements.length > 0 ? ',' : ''}\n  ${text}`);
     const declarationRecorder = host.beginUpdate(filePath);
     declarationRecorder.insertLeft(change.pos, change.toAdd);
     host.commitUpdate(declarationRecorder);
