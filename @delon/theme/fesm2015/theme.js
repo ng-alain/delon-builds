0406ef0601d089b82c75957629ffd21376440c2b
卡色 <cipchk@qq.com>
2019-09-24 11:46:56 +0000

    master - 2d0c695 Merge 95416399d8e5aea4fbe074b4022960358fd78b6a into 002eaf315af7f84bbf18c8d0188978845788e9c9

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3368,7 +3368,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.4.0-002eaf3');
+const VERSION = new Version('8.4.0-2d0c695');
 
 /**
  * @fileoverview added by tsickle
