05404dbc9b357529350498bc8168a0ab86993184
卡色 <cipchk@qq.com>
2019-10-09 15:02:15 +0000

    master - 9822fd34 Merge e5c4f1e8f3f2a9d2dd675051f23146564aea05c0 into a1d69ba44195ed5c5cadf1e10f8777c53b929243

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3445,7 +3445,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.4.0-48cd1c09');
+const VERSION = new Version('8.4.0-9822fd34');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4029,7 +4029,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.4.0-48cd1c09');
+var VERSION = new Version('8.4.0-9822fd34');
 
 /**
  * @fileoverview added by tsickle
