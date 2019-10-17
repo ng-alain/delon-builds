144ea6a9128744d51a18f9efe86be3035ed24e8d
卡色 <cipchk@qq.com>
2019-10-17 15:17:40 +0000

    master - 6cf8251 Merge fb2a78811e2b92e9d4d4c86488648cb6831a8ca1 into ae26f7edba7b819e0c193c00f60b40c1a4fb1287

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3445,7 +3445,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.5.0-246e6cc');
+const VERSION = new Version('8.5.0-6cf8251');
 
 /**
  * @fileoverview added by tsickle
