57d0c8f12a4e789cc4a84d15964eb8107a0c8a1e
卡色 <cipchk@qq.com>
2019-10-18 07:14:28 +0000

    master - ccfedb5 test(abc:st): fix test (#699)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4029,7 +4029,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.0-9a73ebe');
+var VERSION = new Version('8.5.0-ccfedb5');
 
 /**
  * @fileoverview added by tsickle
