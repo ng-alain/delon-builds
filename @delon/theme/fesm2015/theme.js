1b6c9ed8b7ce6869ffadbec8059f8c5081844a6e
卡色 <cipchk@qq.com>
2019-10-18 08:51:14 +0000

    master - 5b4b477 docs(chart:water-wave): add losing property (#697)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3445,7 +3445,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.5.0-0a96261a');
+const VERSION = new Version('8.5.0-5b4b477');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4029,7 +4029,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.0-0a96261a');
+var VERSION = new Version('8.5.0-5b4b477');
 
 /**
  * @fileoverview added by tsickle
