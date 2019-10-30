68359a723485d3cecf46a4809a3f746e90b70012
卡色 <cipchk@qq.com>
2019-10-30 06:46:56 +0000

    master - 9e131d7 feat(cli): add cnpm reminder (#704)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4029,7 +4029,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.1-d18a1cf2');
+var VERSION = new Version('8.5.1-9e131d7');
 
 /**
  * @fileoverview added by tsickle
