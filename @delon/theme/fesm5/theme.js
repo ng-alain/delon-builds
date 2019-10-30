a6efaca707ac7047ae124f5820a2bcd51418fee4
卡色 <cipchk@qq.com>
2019-10-30 12:31:46 +0000

    master - 88e63a2 fix(abc:xlsx): fix duplicate loading xlsx.js when XLSX  exists (#705)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4029,7 +4029,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.1-607fe55');
+var VERSION = new Version('8.5.1-88e63a2');
 
 /**
  * @fileoverview added by tsickle
