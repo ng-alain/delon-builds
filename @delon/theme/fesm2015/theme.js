75f64cc50233a16afaf8224f6bdc4619ef6feace
卡色 <cipchk@qq.com>
2019-11-13 02:52:20 +0000

    master - 0ca0b2d test(cli): add alain test (#682)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3484,7 +3484,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.5.1-7e55136');
+const VERSION = new Version('8.5.1-0ca0b2d');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4068,7 +4068,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.1-7e55136');
+var VERSION = new Version('8.5.1-0ca0b2d');
 
 /**
  * @fileoverview added by tsickle
