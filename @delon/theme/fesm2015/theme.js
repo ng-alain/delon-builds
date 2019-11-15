b81765649657e53185fe64ce0ea7f4f0a44471c9
卡色 <cipchk@qq.com>
2019-11-15 00:07:09 +0000

    master - ca37353 Merge ddacea9c03e52f16c984724eb00f0596824d8349 into 0ca0b2d3bdbd8b4e3dcafed4b312e8caedcada87

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3484,7 +3484,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.5.1-75fa1ff');
+const VERSION = new Version('8.5.1-ca37353');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
 export const VERSION = '^8.5.1';
-export const ZORROVERSION = '^8.5.0';
+export const ZORROVERSION = '^8.4.1';
