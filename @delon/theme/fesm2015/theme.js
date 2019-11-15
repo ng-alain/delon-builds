dda4802e29d5b5c446305157b42bee462d6385a1
卡色 <cipchk@qq.com>
2019-11-15 02:22:39 +0000

    master - 8c574ef fix(form:array): fix losing `optionalHelp` property (#713)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3484,7 +3484,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.5.1-b8f744a');
+const VERSION = new Version('8.5.1-8c574ef');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4068,7 +4068,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.5.1-b8f744a');
+var VERSION = new Version('8.5.1-8c574ef');
 
 /**
  * @fileoverview added by tsickle
