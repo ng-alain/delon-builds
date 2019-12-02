1f8768c4662216a3dbe2068415b6f3a8fe490b54
卡色 <cipchk@qq.com>
2019-12-02 12:11:27 +0000

    master - 2c05493 feat(theme:MenuService): add `getItem`, `setItem` methods (#722)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.6.0-45db936');
+const VERSION = new Version('8.6.0-2c05493');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.6.0-45db936');
+var VERSION = new Version('8.6.0-2c05493');
 
 /**
  * @fileoverview added by tsickle
