9baedc80fad61757e5b46b8e5357784b87739fe2
卡色 <cipchk@qq.com>
2019-12-05 04:13:38 +0000

    master - d58f5b1 feat(abc:sidebar-nav): support html in `text` or `i18n` property (#723)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.6.0-ab16b49c');
+const VERSION = new Version('8.6.0-d58f5b1');
 
 /**
  * @fileoverview added by tsickle
