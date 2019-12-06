d59aef0ef66107d94748c19e79f7a3ef49937d6f
卡色 <cipchk@qq.com>
2019-12-06 11:21:52 +0000

    master - cfe2996 Merge 86e0e0d386b7f3452abd5d4ce18877281715a27d into 54e3ea8537f1bf1a494c9de2cc6150a25650d46e

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.0-08e996c');
+var VERSION = new Version('8.7.0-cfe2996');
 
 /**
  * @fileoverview added by tsickle
