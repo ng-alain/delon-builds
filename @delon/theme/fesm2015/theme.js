93820d044effd6a89bd9db6bc3932f0bddd73d45
卡色 <cipchk@qq.com>
2019-12-06 11:05:20 +0000

    master - 08e996c Merge 21c910b844340a45f76d6781df2f458d692f1b70 into 54e3ea8537f1bf1a494c9de2cc6150a25650d46e

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.7.0-6ddcc21');
+const VERSION = new Version('8.7.0-08e996c');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.0-6ddcc21');
+var VERSION = new Version('8.7.0-08e996c');
 
 /**
  * @fileoverview added by tsickle
