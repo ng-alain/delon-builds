3de6c9dd6a1535afdf909da095502f357e01807f
卡色 <cipchk@qq.com>
2019-12-12 14:53:32 +0000

    master - 8a51910 build: update cn snapshot (#732)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.7.1');
+const VERSION = new Version('8.7.1-8a51910');
 
 /**
  * @fileoverview added by tsickle
