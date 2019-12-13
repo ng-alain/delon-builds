030e2affc89acd8f8872c1a2093e4f00d0d079f5
卡色 <cipchk@qq.com>
2019-12-13 04:46:02 +0000

    master - ff55f2e8 Merge f69326170e118e7174503c86075a0ec79c7c5042 into 7de70b2642cc063c0d1978df770cb16057f0b07b

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.7.1-2c60a74e');
+const VERSION = new Version('8.7.1-ff55f2e8');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.1-2c60a74e');
+var VERSION = new Version('8.7.1-ff55f2e8');
 
 /**
  * @fileoverview added by tsickle
