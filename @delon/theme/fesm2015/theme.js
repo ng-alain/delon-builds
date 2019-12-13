fc939b4585e9155ccd8ea8a8d24a52d4580c0a80
卡色 <cipchk@qq.com>
2019-12-13 04:06:15 +0000

    master - c53a6b0 Merge e23f3722b2b7cb4c10b7bd77610f78b00d40570e into 8a51910933ce8dd7c3be2273517838d1bc228286

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3528,7 +3528,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.7.1-8a51910');
+const VERSION = new Version('8.7.1-c53a6b0');
 
 /**
  * @fileoverview added by tsickle
