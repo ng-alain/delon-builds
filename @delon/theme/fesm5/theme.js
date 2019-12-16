44f0f217ea3fa9f2e9ce70c6d76847a46afb717e
卡色 <cipchk@qq.com>
2019-12-16 08:39:09 +0000

    master - 0f8d715 Merge 5b92497cf0036eeb3b2b5aa6a5abaea2a1a9ed76 into 8bd3729107e67c2bc0239effb302e680ed5bdca1

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4132,7 +4132,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.1-8bd3729');
+var VERSION = new Version('8.7.1-0f8d715');
 
 /**
  * @fileoverview added by tsickle
