0067090799072f13de02ad28d8806916ac0debd5
卡色 <cipchk@qq.com>
2019-12-17 06:50:54 +0000

    master - 353f18e Merge 90f1102357078f8465b6621b9a9427ec1a9287a2 into a32cc55827471f48bf284c69f59d5b904899903a

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4132,7 +4132,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.2-f16d8a2');
+var VERSION = new Version('8.7.2-353f18e');
 
 /**
  * @fileoverview added by tsickle
