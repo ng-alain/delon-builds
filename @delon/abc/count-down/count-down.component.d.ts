e59241e8f2f826988e2927fd83611f228caafc17
卡色 <cipchk@qq.com>
2019-12-20 10:28:42 +0000

    master - be12dbc Merge a4375cfb7ed7c859edc86a2bbaa89de3b705f9cc into 0c02bb6846cc3f7e96c5aa30748177bfd8c5916e

--- @delon/abc/count-down/count-down.component.d.ts
+++ @delon/abc/count-down/count-down.component.d.ts
@@ -1,16 +1,12 @@
-import { EventEmitter, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { CountdownEvent, CountdownConfig, CountdownComponent } from 'ngx-countdown';
-export declare class CountDownComponent implements OnInit {
+export declare class CountDownComponent {
     readonly instance: CountdownComponent;
     config: CountdownConfig;
     /**
      * 目标时间
      */
     target: number | Date;
-    readonly begin: EventEmitter<void>;
-    readonly notify: EventEmitter<number>;
-    readonly end: EventEmitter<void>;
     readonly event: EventEmitter<CountdownEvent>;
-    ngOnInit(): void;
     handleEvent(e: CountdownEvent): void;
 }
