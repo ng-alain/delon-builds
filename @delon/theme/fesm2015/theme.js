3e1ccfb11899ebaf4a1ae7e03475b77f25b1ecb9
卡色 <cipchk@qq.com>
2019-12-26 03:43:46 +0000

    master - 42f1bf4 Merge afcc53030d48eb0b1ed0170c26215353ef6b92e5 into 889d75af9b330a6deb6ebbe91b4873ec611f2cab

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.7.3-889d75a');
+const VERSION = new Version('8.7.3-42f1bf4');
 
 /**
  * @fileoverview added by tsickle
