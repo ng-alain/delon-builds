dcdbc9a38b30065cb2c4af505a2f55bea7fcb4ec
卡色 <cipchk@qq.com>
2019-12-29 05:45:40 +0000

    master - bafb44c feat(abc:down-file): support custom file name (#757)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.3-350ccc2');
+var VERSION = new Version('8.7.3-bafb44c');
 
 /**
  * @fileoverview added by tsickle
