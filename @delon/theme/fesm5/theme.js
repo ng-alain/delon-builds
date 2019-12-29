7d5a8c5b2c7572eafcb09f33f8a1b544e003ae6a
卡色 <cipchk@qq.com>
2019-12-29 14:04:40 +0000

    master - c7b3a6f4 Merge d7571506fed610dd976f929b97c20f06ccb543f1 into bafb44cbfdcad73eb9bfed6a155b089e7c77c3e6

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.7.3-bafb44c');
+var VERSION = new Version('8.7.3-c7b3a6f4');
 
 /**
  * @fileoverview added by tsickle
