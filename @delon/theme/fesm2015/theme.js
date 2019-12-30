fa8908cfcec0aca675b8a633c0f9035eb5af8bb4
卡色 <cipchk@qq.com>
2019-12-30 15:09:30 +0000

    master - ba76a80a Merge 33b1a658823ff753e40c8d26860a8c636c369685 into e81e756dec8c5ad48525ac97ba620359b64e5ae6

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0');
+const VERSION = new Version('8.8.0-ba76a80a');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0');
+var VERSION = new Version('8.8.0-ba76a80a');
 
 /**
  * @fileoverview added by tsickle
