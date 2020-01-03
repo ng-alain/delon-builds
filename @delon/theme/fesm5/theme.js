d22ec49ecb06117ec8339a7c5da0534a3b816860
卡色 <cipchk@qq.com>
2020-01-03 10:56:38 +0000

    master - ebb5e54 refactor: only allow secondary imports (#756)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-a971ac0');
+var VERSION = new Version('8.8.0-ebb5e54');
 
 /**
  * @fileoverview added by tsickle
