512310e871ad5c380c1c49f50e3964a5fa2cb844
卡色 <cipchk@qq.com>
2020-01-09 06:04:17 +0000

    master - 21eec5b feat(abc:st): add `count`, `list` properites of component (#767)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-748f7e1');
+const VERSION = new Version('8.8.0-21eec5b');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-748f7e1');
+var VERSION = new Version('8.8.0-21eec5b');
 
 /**
  * @fileoverview added by tsickle
