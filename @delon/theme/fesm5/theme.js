c163a26e1fd2666f5f2627cf4d019881b08defe0
卡色 <cipchk@qq.com>
2020-01-09 05:01:15 +0000

    master - 748f7e1 feat(abc:down-file): add `http-body` property (#768)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-d912ddcd');
+var VERSION = new Version('8.8.0-748f7e1');
 
 /**
  * @fileoverview added by tsickle
