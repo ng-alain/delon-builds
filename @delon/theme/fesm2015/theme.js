89b43792f547d35466c30827e98cf380a55d4277
卡色 <cipchk@qq.com>
2020-01-17 08:03:29 +0000

    master - caf48da Merge 328dfcd300558ddcb69acdfc377471f0db1a936d into 7dc834ede2dc0d0d43d84a7c1ebbc47ddfc94209

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-7e3ad5e');
+const VERSION = new Version('8.8.0-caf48da');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-7e3ad5e');
+var VERSION = new Version('8.8.0-caf48da');
 
 /**
  * @fileoverview added by tsickle
