81ca37871d6471e717f58859f8cfc565958e56ef
卡色 <cipchk@qq.com>
2020-01-17 10:23:58 +0000

    master - ad2a3e6 fix(form:array): fix invalid error when removed item (#772)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-caf48da');
+const VERSION = new Version('8.8.0-ad2a3e6');
 
 /**
  * @fileoverview added by tsickle
