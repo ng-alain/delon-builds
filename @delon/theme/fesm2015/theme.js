2f4b93f420af4d94ba8930b80e787f7faf64be2a
卡色 <cipchk@qq.com>
2020-01-17 06:34:49 +0000

    master - 6aa9a67 chore: fix travis (#771)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-04c9748');
+const VERSION = new Version('8.8.0-6aa9a67');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-04c9748');
+var VERSION = new Version('8.8.0-6aa9a67');
 
 /**
  * @fileoverview added by tsickle
