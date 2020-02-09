063b0baf388cde60bdb3330a65ef50a1f5b54aa0
卡色 <cipchk@qq.com>
2020-02-09 05:01:00 +0000

    master - 1c93524 refactor: remove deprecated api (#748)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-361b4715');
+const VERSION = new Version('8.8.0-1c93524');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-361b4715');
+var VERSION = new Version('8.8.0-1c93524');
 
 /**
  * @fileoverview added by tsickle
