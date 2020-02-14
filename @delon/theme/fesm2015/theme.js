1a4aaa8a88cdc3bced9f52911c8e23b245255575
卡色 <cipchk@qq.com>
2020-02-14 08:53:05 +0000

    master - bd4313b refactor: potential xss (#726)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3544,7 +3544,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-e7dcf7f8');
+const VERSION = new Version('8.8.0-bd4313b');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-e7dcf7f8');
+var VERSION = new Version('8.8.0-bd4313b');
 
 /**
  * @fileoverview added by tsickle
