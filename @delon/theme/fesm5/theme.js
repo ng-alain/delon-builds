dd4fddcca6a8d9d178b861460473486a40f1f4d4
cipchk <cipchk@qq.com>
2020-02-18 08:24:15 +0000

    issues-cli - 84096ce chore: fix artifacts

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4145,7 +4145,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-bd4313b');
+var VERSION = new Version('8.8.0-84096ce');
 
 /**
  * @fileoverview added by tsickle
