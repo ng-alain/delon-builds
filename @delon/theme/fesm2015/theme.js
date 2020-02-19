2114b889a308daf61d4d3d5cd8395bdf55ff92b8
卡色 <cipchk@qq.com>
2020-02-19 03:11:16 +0000

    8.x - 770ec8b fix(theme:_HtpClient): fix loading status invalid when not subsc… (#774)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3549,7 +3549,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.8.0-19cd28e2');
+const VERSION = new Version('8.8.0-770ec8b');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4150,7 +4150,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.8.0-19cd28e2');
+var VERSION = new Version('8.8.0-770ec8b');
 
 /**
  * @fileoverview added by tsickle
