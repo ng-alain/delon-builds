45ad7254f12da68d466e981ddbd96127b81e3af8
cipchk <cipchk@qq.com>
2020-04-07 07:13:19 +0000

    publish-8.9.2 - ba6bc12 chore: update dos

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4204,7 +4204,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.9.2');
+var VERSION = new Version('8.9.2-ba6bc12');
 
 /**
  * @fileoverview added by tsickle
