e78caf9a46943a1be55fbcf8993c778a3100f822
卡色 <cipchk@qq.com>
2020-04-07 07:26:20 +0000

    8.x - 90ba3ab release(8.9.2): release 8.9.2 (#794)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3589,7 +3589,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('8.9.2-587665a7');
+const VERSION = new Version('8.9.2');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4204,7 +4204,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('8.9.2-587665a7');
+var VERSION = new Version('8.9.2');
 
 /**
  * @fileoverview added by tsickle
