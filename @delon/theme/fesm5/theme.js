cf0d51e8e2b94685c17109c7ac4cb134cf705889
卡色 <cipchk@qq.com>
2020-04-13 04:34:59 +0000

    master - 7177091b Merge 4e3c02070a7162dda81381dc4b5943fd544f8328 into a17fac901febd809863d1a7bc38b5e3a8c88cb84

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-7d9f0816');
+var VERSION = new Version('9.0.0-alpha.1-7177091b');
 
 /**
  * @fileoverview added by tsickle
