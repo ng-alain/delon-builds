09b06217c136d7e3a6e830b176ea41c3114efa41
卡色 <cipchk@qq.com>
2020-04-14 08:40:18 +0000

    master - 9ac35ab3 Merge 7e00715fa1973a82b0dc7bc288de51d424d65fe8 into a17fac901febd809863d1a7bc38b5e3a8c88cb84

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3530,7 +3530,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.1-2e7db21a');
+const VERSION = new Version('9.0.0-alpha.1-9ac35ab3');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-2e7db21a');
+var VERSION = new Version('9.0.0-alpha.1-9ac35ab3');
 
 /**
  * @fileoverview added by tsickle
