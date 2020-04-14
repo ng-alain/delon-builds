9cdc9cc913ca8cda7dcba41a73078a1818e8f83f
卡色 <cipchk@qq.com>
2020-04-14 11:56:38 +0000

    master - 9f63e076 Merge 9c2660dcf10dbae623fe98a834ded06ef18e0028 into a17fac901febd809863d1a7bc38b5e3a8c88cb84

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4129,7 +4129,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-95b09580');
+var VERSION = new Version('9.0.0-alpha.1-9f63e076');
 
 /**
  * @fileoverview added by tsickle
