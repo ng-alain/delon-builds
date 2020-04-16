8cae51d15f4aa3e9682d850bc2a257ed2de48c76
卡色 <cipchk@qq.com>
2020-04-16 07:30:58 +0000

    master - 55f1d24b refactor: merge 8.9.x (#796)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4271,7 +4271,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-ca088223');
+var VERSION = new Version('9.0.0-alpha.1-55f1d24b');
 
 /**
  * @fileoverview added by tsickle
