7158880d85121d1e2674b5ff4330760204793543
卡色 <cipchk@qq.com>
2020-04-16 11:40:14 +0000

    master - b4641c1f refactor: only allow secondary imports (#798)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4271,7 +4271,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-0982aa26');
+var VERSION = new Version('9.0.0-alpha.1-b4641c1f');
 
 /**
  * @fileoverview added by tsickle
