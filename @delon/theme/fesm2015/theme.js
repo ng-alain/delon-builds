951b89f275778ea32ac4bd6edd625094b45f2411
卡色 <cipchk@qq.com>
2020-04-16 17:11:33 +0000

    master - a4bcacd0 refactor: using date-fns (#804)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.1-d381eada');
+const VERSION = new Version('9.0.0-alpha.1-a4bcacd0');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4284,7 +4284,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-d381eada');
+var VERSION = new Version('9.0.0-alpha.1-a4bcacd0');
 
 /**
  * @fileoverview added by tsickle
