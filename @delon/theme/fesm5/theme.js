b5ff23e4252b227f686a6515b2d94614f8d79987
卡色 <cipchk@qq.com>
2020-04-16 08:29:33 +0000

    master - c68a4f60 refactor: refactor directory structure (#797)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4271,7 +4271,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.1-b290d905');
+var VERSION = new Version('9.0.0-alpha.1-c68a4f60');
 
 /**
  * @fileoverview added by tsickle
