30a4d4dbdaa901ad0a5c4c3206a77c4bb40495b4
卡色 <cipchk@qq.com>
2020-04-16 07:55:53 +0000

    master - 02e0b2a1 Merge 4a0d150ed3cabd48f6324036c15464cd6c9cae42 into 55f1d24b0020c3e6ba72ea37e5e4063ac6c42eca

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3658,7 +3658,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.1-c48c62ff');
+const VERSION = new Version('9.0.0-alpha.1-02e0b2a1');
 
 /**
  * @fileoverview added by tsickle
