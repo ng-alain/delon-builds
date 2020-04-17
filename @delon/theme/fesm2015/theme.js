6cc25a920c7608a43779b209ec9d217fec36ada6
卡色 <cipchk@qq.com>
2020-04-17 15:31:01 +0000

    master - 53b985a0 Merge c19e8508ccb4381053dc806e03b97f136ee01dea into 2637185df2d7724ebcec15c756d02f16f4de9406

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.2-45405b4c');
+const VERSION = new Version('9.0.0-alpha.2-53b985a0');
 
 /**
  * @fileoverview added by tsickle
