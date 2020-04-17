54947428c2e27a2c23c1fb4d55508ab4c341f91a
卡色 <cipchk@qq.com>
2020-04-17 19:37:39 +0000

    master - 2110ed35 Merge 31e399969ea7a329cdb7357e157fe1cd770a8606 into 12b1d51aa9e6b4d550ceb13c67ff734998524e77

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4284,7 +4284,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.2-025b8b88');
+var VERSION = new Version('9.0.0-alpha.2-2110ed35');
 
 /**
  * @fileoverview added by tsickle
