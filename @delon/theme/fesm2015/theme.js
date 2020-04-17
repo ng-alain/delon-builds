776e238372c7763a3de84eb89eb690f470a2aa12
卡色 <cipchk@qq.com>
2020-04-17 12:32:37 +0000

    master - 8b0216e4 Merge 2477066e248ed6c01f7bedddebbc8c80a0d0614f into b684864e9d3d475e90e658a639a53c7ed126912a

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.2-f035acaa');
+const VERSION = new Version('9.0.0-alpha.2-8b0216e4');
 
 /**
  * @fileoverview added by tsickle
