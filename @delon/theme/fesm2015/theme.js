fe3174c84798bf9a5abfe806aa241afc1d79442b
卡色 <cipchk@qq.com>
2020-04-18 06:06:37 +0000

    master - f08755a2 Merge fbf70f56ef7f0769a60b66712ca90fbc9b6137da into 6b68b2826e6d8b5b4afa890d084ae36997b6c298

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.2-cb70bb79');
+const VERSION = new Version('9.0.0-alpha.2-f08755a2');
 
 /**
  * @fileoverview added by tsickle
