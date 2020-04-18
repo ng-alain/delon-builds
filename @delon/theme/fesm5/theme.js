7654aab396befcc655a7f323525832ff63be444a
卡色 <cipchk@qq.com>
2020-04-18 03:57:22 +0000

    master - 0e88490e Merge 914d8f4a358717ecf6d275a766e220b6b58f94b9 into 6b68b2826e6d8b5b4afa890d084ae36997b6c298

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4284,7 +4284,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.2-1b903536');
+var VERSION = new Version('9.0.0-alpha.2-0e88490e');
 
 /**
  * @fileoverview added by tsickle
