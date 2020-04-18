5d2432bdd91325180ab2bb5bb8e13a36ba5aa0a5
卡色 <cipchk@qq.com>
2020-04-18 06:43:53 +0000

    master - e16f7a39 docs: update web site (#809)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-alpha.2-2754ae39');
+const VERSION = new Version('9.0.0-alpha.2-e16f7a39');
 
 /**
  * @fileoverview added by tsickle
