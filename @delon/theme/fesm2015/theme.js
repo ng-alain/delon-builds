aa273ca74019930a65b777c08eef0504ae5dc7ec
卡色 <cipchk@qq.com>
2020-04-18 18:48:27 +0000

    master - 7f316487 fix(chart:tag-cloud): fix overlap (#816)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3676,7 +3676,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.1-d80a578d');
+const VERSION = new Version('9.0.0-rc.1-7f316487');
 
 /**
  * @fileoverview added by tsickle
