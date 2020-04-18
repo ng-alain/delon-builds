456a69798756c33dac03fd78ed042e63cf924d42
卡色 <cipchk@qq.com>
2020-04-18 05:56:56 +0000

    master - cb70bb79 Merge 5a864a6bcecd26258a92b7470b62a8e105919801 into 6b68b2826e6d8b5b4afa890d084ae36997b6c298

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4284,7 +4284,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-alpha.2-1522d77f');
+var VERSION = new Version('9.0.0-alpha.2-cb70bb79');
 
 /**
  * @fileoverview added by tsickle
