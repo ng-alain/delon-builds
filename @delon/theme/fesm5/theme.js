fb2cbd4e193724b902ad969bbc848c863f62a9a9
卡色 <cipchk@qq.com>
2020-04-19 04:37:41 +0000

    master - 6cebfef0 docs: fix stackblitz (#815)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.1-6f9bba58');
+var VERSION = new Version('9.0.0-rc.1-6cebfef0');
 
 /**
  * @fileoverview added by tsickle
