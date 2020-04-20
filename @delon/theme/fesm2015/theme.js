233ce7d7b78e17e96f057c328e29f9c0573bdf78
卡色 <cipchk@qq.com>
2020-04-20 12:49:42 +0000

    master - 04e1ce7f Merge 9c88f97c4091af7a3d16201f4c01c8ab99735ff5 into f0a9443c8191a69b334a7f6ae935ef1092b69326

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3684,7 +3684,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-f0a9443c');
+const VERSION = new Version('9.0.0-rc.2-04e1ce7f');
 
 /**
  * @fileoverview added by tsickle
