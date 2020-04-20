20bd2ca9e0aee002487a828307a26c9317cefdfb
卡色 <cipchk@qq.com>
2020-04-20 04:23:38 +0000

    master - 4bac6d2c perf: performance depreation (#823)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-16de5eb0');
+var VERSION = new Version('9.0.0-rc.2-4bac6d2c');
 
 /**
  * @fileoverview added by tsickle
