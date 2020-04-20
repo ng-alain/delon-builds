877bef975a9addf33f9eb62e74dd959b908585da
卡色 <cipchk@qq.com>
2020-04-20 09:10:20 +0000

    master - 68b56c42 fix(cli): fix Invalid "extends" configuration value (#824)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3684,7 +3684,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-6b4f1ac3');
+const VERSION = new Version('9.0.0-rc.2-68b56c42');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-6b4f1ac3');
+var VERSION = new Version('9.0.0-rc.2-68b56c42');
 
 /**
  * @fileoverview added by tsickle
