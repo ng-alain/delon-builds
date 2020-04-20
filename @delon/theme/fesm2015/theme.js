91c563a068bd7761ed90e1e68cd0930ecda80fbf
卡色 <cipchk@qq.com>
2020-04-20 12:48:12 +0000

    master - f0a9443c fix(module:all): fix enableIvy:false (#826)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3684,7 +3684,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-7395b136');
+const VERSION = new Version('9.0.0-rc.2-f0a9443c');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-7395b136');
+var VERSION = new Version('9.0.0-rc.2-f0a9443c');
 
 /**
  * @fileoverview added by tsickle
