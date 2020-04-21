73e189be869b0a6aa4b166128fef3557df4004b6
卡色 <cipchk@qq.com>
2020-04-21 18:05:06 +0000

    master - 04e69835 fix(form:widget:date): fix invalid end date when clean date (#831)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3684,7 +3684,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-9c7042b4');
+const VERSION = new Version('9.0.0-rc.2-04e69835');
 
 /**
  * @fileoverview added by tsickle
