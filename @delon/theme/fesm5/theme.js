4eab2587d6aae2dea459d9fb02866a13be4e2264
卡色 <cipchk@qq.com>
2020-04-21 17:29:46 +0000

    master - 6ff3db5f fix(form): fix update value and validity when removing the last… (#829)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-1c3b1251');
+var VERSION = new Version('9.0.0-rc.2-6ff3db5f');
 
 /**
  * @fileoverview added by tsickle
