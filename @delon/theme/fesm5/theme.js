0d1ead69ecaecff36a6cf17dece952741b1d6812
卡色 <cipchk@qq.com>
2020-04-21 07:43:38 +0000

    master - 4d5a767d feat(abc:st): add `widget` type (#827)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-3297297b');
+var VERSION = new Version('9.0.0-rc.2-4d5a767d');
 
 /**
  * @fileoverview added by tsickle
