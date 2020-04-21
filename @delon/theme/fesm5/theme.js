494a71f9381ca43edeadaecd04ac04c0a40af68b
卡色 <cipchk@qq.com>
2020-04-21 15:19:49 +0000

    master - abc541ad fix(cli): add `widget` of `st` (#828)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-12fbcef1');
+var VERSION = new Version('9.0.0-rc.2-abc541ad');
 
 /**
  * @fileoverview added by tsickle
