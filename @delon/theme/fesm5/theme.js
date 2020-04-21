d22e0c8ae222209bd2ec20dd4e9075b77a5a2d35
卡色 <cipchk@qq.com>
2020-04-21 08:32:35 +0000

    master - 12fbcef1 Merge 7e6c24bede2e7a4f867d0e9e30b3368d4aca6cca into 4d5a767d94a248908465e59a6af9eae110205b68

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.2-4a6f2fd3');
+var VERSION = new Version('9.0.0-rc.2-12fbcef1');
 
 /**
  * @fileoverview added by tsickle
