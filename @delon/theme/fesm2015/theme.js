ce21de66ba5f45cb69b0da277bffc98e512bb59d
卡色 <cipchk@qq.com>
2020-04-22 11:45:24 +0000

    master - fa408229 feat(chart:time-line): add `maxAxis` property (#838)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3684,7 +3684,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.3-b399d22d');
+const VERSION = new Version('9.0.0-rc.3-fa408229');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4292,7 +4292,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.3-b399d22d');
+var VERSION = new Version('9.0.0-rc.3-fa408229');
 
 /**
  * @fileoverview added by tsickle
