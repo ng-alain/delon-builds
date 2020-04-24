b9de483258b2c60e2ffe24953211bbd42a8c55ce
卡色 <cipchk@qq.com>
2020-04-24 14:13:02 +0000

    master - 293964ab release(9.0.0-rc.4): release 9.0.0-rc.4 (#844)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4385,7 +4385,7 @@ if (false) {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.4-8cd5c752');
+const VERSION = new Version('9.0.0-rc.4');
 
 /**
  * @fileoverview added by tsickle
