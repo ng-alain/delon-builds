6abc2d972f33b048a6e798d1aa65f478f36a3751
卡色 <cipchk@qq.com>
2020-04-24 13:17:18 +0000

    master - 8cd5c752 Merge 93af179fe956598f8e1f69c1b634402bda0d402f into cfe14c410c33fea192e61bb06b2a0c40856f7491

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -5027,7 +5027,7 @@ if (false) {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.4');
+var VERSION = new Version('9.0.0-rc.4-8cd5c752');
 
 /**
  * @fileoverview added by tsickle
