5258663882f041ead52cccf31a93cd0a1607dc94
卡色 <cipchk@qq.com>
2020-04-25 15:41:01 +0000

    master - 884075d4 Merge 3da3c79f425ece71870673ca0252541fe2c7ef61 into 9b111bdcf4a7102373e0123fea525aa59c48f827

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4244,7 +4244,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-rc.4-3a0e1ac6');
+var VERSION = new Version('9.0.0-rc.4-884075d4');
 
 /**
  * @fileoverview added by tsickle
