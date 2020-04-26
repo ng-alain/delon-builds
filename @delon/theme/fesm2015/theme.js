80ff7246beaa2bee435f2bf6cb428bea37667ec9
卡色 <cipchk@qq.com>
2020-04-26 15:31:54 +0000

    master - d6f93bb5 Merge bd96932669a24bf9cae3c77db627e4b28de06754 into 4f2b3dff05894b0987b453e6defff125a7c8b17c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3639,7 +3639,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0-d6f53800');
+const VERSION = new Version('9.0.0-d6f93bb5');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4244,7 +4244,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-d6f53800');
+var VERSION = new Version('9.0.0-d6f93bb5');
 
 /**
  * @fileoverview added by tsickle
