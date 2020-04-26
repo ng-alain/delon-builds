59bf77414199a119cdba26e38f81a0bf299fc338
卡色 <cipchk@qq.com>
2020-04-26 16:27:35 +0000

    master - e46b5271 docs: fix stackblitz (#851)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4244,7 +4244,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-d6f93bb5');
+var VERSION = new Version('9.0.0');
 
 /**
  * @fileoverview added by tsickle
