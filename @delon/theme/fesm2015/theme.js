8ed7102aa415ad53e0679a601332dbe0e02f8129
卡色 <cipchk@qq.com>
2020-04-26 12:01:43 +0000

    master - dc172e81 Merge d786dfdf76af219ab4337ec6ea60f8203094f430 into bd32b1f5870eab8cb24e0ad4c739927ca28a1643

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3639,7 +3639,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.0');
+const VERSION = new Version('9.0.0-dc172e81');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4244,7 +4244,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0');
+var VERSION = new Version('9.0.0-dc172e81');
 
 /**
  * @fileoverview added by tsickle
