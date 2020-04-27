65a932fece88bd1476a795b7d9aab60d7b6554e2
卡色 <cipchk@qq.com>
2020-04-27 15:09:28 +0000

    master - 890e4ca2 Merge 83388b378f2943552033a91e8a50bacea675e233 into b033c81e2eddbcd32f1a312d87ca4cd1cd5d777e

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3638,7 +3638,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.1');
+const VERSION = new Version('9.0.1-890e4ca2');
 
 /**
  * @fileoverview added by tsickle
