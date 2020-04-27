e8d4321f28a859db0920e8e99865a95c2d45c6a9
卡色 <cipchk@qq.com>
2020-04-27 15:31:57 +0000

    master - 349f4532 release(9.0.1): release 9.0.1 (#856)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4243,7 +4243,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.1-890e4ca2');
+var VERSION = new Version('9.0.1');
 
 /**
  * @fileoverview added by tsickle
