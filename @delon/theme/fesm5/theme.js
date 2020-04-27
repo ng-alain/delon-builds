fb4ea03b195c6388b9a9be9f1b607453c7cc2ac7
卡色 <cipchk@qq.com>
2020-04-27 14:34:24 +0000

    master - b033c81e feat(cli:plugin:ie): add `ie` plugin (#855)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4243,7 +4243,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.0-116b1de8');
+var VERSION = new Version('9.0.0-b033c81e');
 
 /**
  * @fileoverview added by tsickle
