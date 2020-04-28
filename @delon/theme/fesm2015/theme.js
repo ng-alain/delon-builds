5f024560933121df5cf9b537581c4766fcd5201e
卡色 <cipchk@qq.com>
2020-04-28 14:09:18 +0000

    master - 773f5465 fix(abc:st): fix style misplacement when expand is true (#860)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3638,7 +3638,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.1-ecb0c445');
+const VERSION = new Version('9.0.1-773f5465');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4243,7 +4243,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.1-ecb0c445');
+var VERSION = new Version('9.0.1-773f5465');
 
 /**
  * @fileoverview added by tsickle
