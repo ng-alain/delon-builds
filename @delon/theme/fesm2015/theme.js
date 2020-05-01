de788875fafdcb6d3bbe751b3885ff4b5d5c4546
卡色 <cipchk@qq.com>
2020-05-01 08:23:34 +0000

    master - a47cfe66 fix(sf:widget:autocomplete): fix widget using `ngAfterViewInit` event will be overwrite (#867)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3640,7 +3640,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.1-16b434e6');
+const VERSION = new Version('9.0.1-a47cfe66');
 
 /**
  * @fileoverview added by tsickle
