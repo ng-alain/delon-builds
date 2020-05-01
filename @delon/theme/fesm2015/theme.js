9872166dbc162a2d46af5e8dc1a7c6ab2ad4f369
卡色 <cipchk@qq.com>
2020-05-01 06:35:09 +0000

    master - ea588b2d refactor(abc:reuse-tab): refactor reuse tab (#863)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3640,7 +3640,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.0.1-e2ccf3bc');
+const VERSION = new Version('9.0.1-ea588b2d');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4245,7 +4245,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.0.1-e2ccf3bc');
+var VERSION = new Version('9.0.1-ea588b2d');
 
 /**
  * @fileoverview added by tsickle
