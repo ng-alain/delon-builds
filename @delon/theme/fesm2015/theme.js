62ded1d517e5da07229492a6318670f9382809ff
卡色 <cipchk@qq.com>
2020-05-03 10:50:53 +0000

    master - fbc09d18 fix(cli:ng-add): fix lose component import (#873)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3628,7 +3628,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.1.0-691b0c53');
+const VERSION = new Version('9.1.0-fbc09d18');
 
 /**
  * @fileoverview added by tsickle
