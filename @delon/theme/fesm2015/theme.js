7af48d24a917b03aaef67fdd83a798bfe85e2f08
卡色 <cipchk@qq.com>
2020-05-04 09:04:15 +0000

    master - 9d3068fa docs: update (#870)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3628,7 +3628,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.1.0-4c45148b');
+const VERSION = new Version('9.1.0-9d3068fa');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4233,7 +4233,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.1.0-4c45148b');
+var VERSION = new Version('9.1.0-9d3068fa');
 
 /**
  * @fileoverview added by tsickle
