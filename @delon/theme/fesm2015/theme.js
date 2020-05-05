f07e44420d478612d383f55243fbeb316e89e42c
卡色 <cipchk@qq.com>
2020-05-05 07:31:13 +0000

    master - 6009ae25 feat(abc:qr): add lazy load libary (#875)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3628,7 +3628,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.1.0-7bb5a713');
+const VERSION = new Version('9.1.0-6009ae25');
 
 /**
  * @fileoverview added by tsickle
