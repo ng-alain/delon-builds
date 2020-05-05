fe48219a3378ac3295403b2fe612683bbca541f2
卡色 <cipchk@qq.com>
2020-05-05 06:05:57 +0000

    master - 05ce8a86 fix(util:lazy): add `loading` status when load is non-complete (#874)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4233,7 +4233,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.1.0-4406306d');
+var VERSION = new Version('9.1.0-05ce8a86');
 
 /**
  * @fileoverview added by tsickle
