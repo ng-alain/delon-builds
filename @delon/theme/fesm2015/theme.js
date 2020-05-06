a4c39a7f3668a925b35a295a752c0d1fcee39bf3
卡色 <cipchk@qq.com>
2020-05-06 16:30:24 +0000

    master - 41e25d5e feat(abc:st): add `enum` type (#884)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3628,7 +3628,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.2.0-cc2439b2');
+const VERSION = new Version('9.2.0-41e25d5e');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4233,7 +4233,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.2.0-cc2439b2');
+var VERSION = new Version('9.2.0-41e25d5e');
 
 /**
  * @fileoverview added by tsickle
