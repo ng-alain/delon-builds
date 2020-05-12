a8a0c6bd41b3ec56da6c709576e0e86b4ba435f3
卡色 <cipchk@qq.com>
2020-05-12 12:57:02 +0000

    master - 1376d83f Merge 091509c1d0ab8a6b0263671d125e959ff06ba54f into 2b3f9983006dfe174dfd46bd6b1f8c355512621c

--- ng-alain/plugin/plugin.ie.ts
+++ ng-alain/plugin/plugin.ie.ts
@@ -42,7 +42,7 @@ function setPackage(host: Tree, options: PluginOptions) {
   // libs
   (options.type === 'add' ? addPackageToPackageJson : removePackageFromPackageJson)(
     host,
-    ['classlist.js@DEP-9.2.1', 'web-animations-js@DEP-9.2.1'],
+    ['classlist.js@^1.1.0', 'web-animations-js@^2.3.2'],
     'dependencies',
   );
   // scripts
