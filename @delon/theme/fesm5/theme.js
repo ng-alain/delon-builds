0e267192b81b243bfcba0743a0f064d03c09a066
卡色 <cipchk@qq.com>
2020-05-22 09:51:49 +0000

    master - 46ce95ab Merge 903c068df0253ce0896541e576f45c9cbf41278e into 248e8c8961e593fb525f752c16ff2cef6d3a213c

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.2.4-8387ab07');
+var VERSION = new Version('9.2.4-46ce95ab');
 
 /**
  * @fileoverview added by tsickle
