2aeee8e1a136f4ebddf2eed5eef96c6e78316acd
卡色 <cipchk@qq.com>
2020-05-23 07:14:04 +0000

    master - d658025a Merge 7ce11067866a19ede0d2d6089b81ef98dc2a116f into 248e8c8961e593fb525f752c16ff2cef6d3a213c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.2.4-548e658b');
+const VERSION = new Version('9.2.4-d658025a');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.2.4-548e658b');
+var VERSION = new Version('9.2.4-d658025a');
 
 /**
  * @fileoverview added by tsickle
