b049ebf184ec4f291344597d76d708f90c48931d
卡色 <cipchk@qq.com>
2020-05-24 08:19:25 +0000

    master - 7ae1f357 feat(chart:water-wave): add `animate` property (#918)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.2.4-c526254f');
+var VERSION = new Version('9.2.4-7ae1f357');
 
 /**
  * @fileoverview added by tsickle
