05ea03a3b63d034ec0a3c4e59c923b5c4bc22dba
卡色 <cipchk@qq.com>
2020-05-24 11:56:36 +0000

    master - 981307dc release(9.3.0): release 9.3.0 (#920)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.0-776415bc');
+const VERSION = new Version('9.3.0');
 
 /**
  * @fileoverview added by tsickle
