d0c290477db4764a2900b00837ec4466b57adb24
卡色 <cipchk@qq.com>
2020-05-24 15:59:03 +0000

    master - 94babc62 docs: add cn mirror (#921)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.0-460f0981');
+const VERSION = new Version('9.3.0');
 
 /**
  * @fileoverview added by tsickle
