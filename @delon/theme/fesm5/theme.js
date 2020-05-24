c02f6ba2fee14ab041fb256b1b2ce2d3c3ddb5a4
卡色 <cipchk@qq.com>
2020-05-24 06:48:05 +0000

    master - 22a2a065 refactor(theme): refactor theme (#912)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.2.4-9ac0af5b');
+var VERSION = new Version('9.2.4-22a2a065');
 
 /**
  * @fileoverview added by tsickle
