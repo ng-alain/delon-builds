dfe1ff72c1129480ff109113b01d3bcc0508c2ca
卡色 <cipchk@qq.com>
2020-05-26 06:05:07 +0000

    master - f35c6170 fix(abc:reuse-tab): fix dark style of close button (#923)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.0-ff7069ba');
+var VERSION = new Version('9.3.0-f35c6170');
 
 /**
  * @fileoverview added by tsickle
