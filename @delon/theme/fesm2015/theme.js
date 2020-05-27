7897218c957fd2b45c0f347bfd668a5b1d12f368
卡色 <cipchk@qq.com>
2020-05-27 08:59:41 +0000

    master - afb8ad1e fix(theme): fix flex (#927)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.0-1d36fd9c');
+const VERSION = new Version('9.3.0-afb8ad1e');
 
 /**
  * @fileoverview added by tsickle
