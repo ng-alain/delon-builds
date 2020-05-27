6e0b66e390af217a021198376673e9cf1a375993
卡色 <cipchk@qq.com>
2020-05-27 08:41:42 +0000

    master - 1d36fd9c Merge 03f9ede9d481079a1c7018a20a9b9d08f29a3b78 into 16787185c733792473f769152726f661deb5a3f6

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.0-31337902');
+var VERSION = new Version('9.3.0-1d36fd9c');
 
 /**
  * @fileoverview added by tsickle
