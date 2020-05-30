f08c34e13d1bbfbfb3685cdac38fd0bbe85cbb9e
卡色 <cipchk@qq.com>
2020-05-30 05:00:40 +0000

    master - a8933df0 Merge c317741c0d51256dcf42a692602a42046cb4eebe into 0080efdfe32e2fa23900562c11c386e3d76bd31b

--- @delon/form/src/widgets/tree-select/tree-select.widget.d.ts
+++ @delon/form/src/widgets/tree-select/tree-select.widget.d.ts
@@ -1,13 +1,13 @@
 import { OnInit } from '@angular/core';
 import { NzFormatEmitEvent } from 'ng-zorro-antd/core/tree';
-import { NzSafeAny } from 'ng-zorro-antd/core/types';
 import { SFValue } from '../../interface';
 import { SFSchemaEnum } from '../../schema';
 import { ControlUIWidget } from '../../widget';
 import { SFTreeSelectWidgetSchema } from './schema';
 export declare class TreeSelectWidget extends ControlUIWidget<SFTreeSelectWidgetSchema> implements OnInit {
-    i: NzSafeAny;
+    i: SFTreeSelectWidgetSchema;
     data: SFSchemaEnum[];
+    asyncData: boolean;
     ngOnInit(): void;
     reset(value: SFValue): void;
     change(value: string[] | string): void;
