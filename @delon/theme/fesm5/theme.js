a948fd941ce20f04f4c14b33fa672c4798beaf29
卡色 <cipchk@qq.com>
2020-05-30 04:06:43 +0000

    master - 8f26c71e fix(cli): fix ng-update (#934)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.1-ddbcec9c');
+var VERSION = new Version('9.3.1-8f26c71e');
 
 /**
  * @fileoverview added by tsickle
