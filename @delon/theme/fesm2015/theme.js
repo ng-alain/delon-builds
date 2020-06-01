20c2c8d6817f07916aac6091529c7c4b3466d08b
卡色 <cipchk@qq.com>
2020-06-01 12:16:20 +0000

    master - 50d16178 fix(form): fix optional color of dark theme (#941)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.1-f46c5872');
+const VERSION = new Version('9.3.1-50d16178');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.1-f46c5872');
+var VERSION = new Version('9.3.1-50d16178');
 
 /**
  * @fileoverview added by tsickle
