8888ae0373dc387f45eca6a36232396f37552b15
卡色 <cipchk@qq.com>
2020-06-02 03:20:58 +0000

    master - 09202ddb fix(abc:st): fix recalculate no value of removeRows method (#944)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-52e371a5');
+var VERSION = new Version('9.3.2-09202ddb');
 
 /**
  * @fileoverview added by tsickle
