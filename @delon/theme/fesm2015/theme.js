7c90b9eb0893493484910c3241295175a3879ffd
卡色 <cipchk@qq.com>
2020-06-02 15:20:02 +0000

    master - e0d1e194 Merge b778dc83e3b7002046a6320ad31c6fbd412e8c20 into 09202ddb5ce8abff2692a0ddd206d8af5aef04d2

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.2-bdc1deda');
+const VERSION = new Version('9.3.2-e0d1e194');
 
 /**
  * @fileoverview added by tsickle
