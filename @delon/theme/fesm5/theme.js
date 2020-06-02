bab509396d875a819bb1e4503a10131d637ec475
its404 <35132886+its404@users.noreply.github.com>
2020-06-02 09:14:14 +0000

    master - 0e21f9b3 Merge 32b2fdc582f31bd6e00f8d61dd970b8e02025388 into 09202ddb5ce8abff2692a0ddd206d8af5aef04d2

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-09202ddb');
+var VERSION = new Version('9.3.2-0e21f9b3');
 
 /**
  * @fileoverview added by tsickle
