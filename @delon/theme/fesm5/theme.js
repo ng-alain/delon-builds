0550f9d977f3f80ede2e3c8018f539c1802cacf5
卡色 <cipchk@qq.com>
2020-06-09 05:49:56 +0000

    master - c527dfba docs: fix callback in modal (#946)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-5f5dfccd');
+var VERSION = new Version('9.3.2-c527dfba');
 
 /**
  * @fileoverview added by tsickle
