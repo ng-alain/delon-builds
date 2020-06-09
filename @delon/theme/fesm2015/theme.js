a2b389398ea8fadc3a00bd41268a56f0e3ab5914
卡色 <cipchk@qq.com>
2020-06-09 07:23:22 +0000

    master - 3881014d fix(form:widget:array): fix clean value of `cleanValue` (#948)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.2-4e689cc6');
+const VERSION = new Version('9.3.2-3881014d');
 
 /**
  * @fileoverview added by tsickle
