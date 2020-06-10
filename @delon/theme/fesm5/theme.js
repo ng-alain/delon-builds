59f602d6bcb3b000b6a54cf18acbc4e60c2a58d2
卡色 <cipchk@qq.com>
2020-06-10 07:45:57 +0000

    master - dc0b0460 Merge 5d44e8e2534604577f57ef2a717e3fc5fdd9f768 into 3881014d4b137ab38dbba85dcfe62a6b12733352

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-3881014d');
+var VERSION = new Version('9.3.2-dc0b0460');
 
 /**
  * @fileoverview added by tsickle
