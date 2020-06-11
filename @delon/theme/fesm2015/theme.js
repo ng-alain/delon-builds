408e511faf1d6fd199a296dccfbe667308a8b23a
卡色 <cipchk@qq.com>
2020-06-11 05:30:30 +0000

    master - 22833c01 feat(form:widget:object): add expand of type is card (#952)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.2-a0b371a4');
+const VERSION = new Version('9.3.2-22833c01');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-a0b371a4');
+var VERSION = new Version('9.3.2-22833c01');
 
 /**
  * @fileoverview added by tsickle
