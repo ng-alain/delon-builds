98c3fd541fb82355c10c80005003f3a05c012406
卡色 <cipchk@qq.com>
2020-06-12 14:45:20 +0000

    master - c5606d6e test: add test (#957)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.2-5c9fb842');
+const VERSION = new Version('9.3.2-c5606d6e');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.3.2-5c9fb842');
+var VERSION = new Version('9.3.2-c5606d6e');
 
 /**
  * @fileoverview added by tsickle
