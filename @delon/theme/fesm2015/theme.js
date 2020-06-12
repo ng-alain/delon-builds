2ba4cd63f59fe947d65f0be012ff773849d02488
卡色 <cipchk@qq.com>
2020-06-12 13:44:56 +0000

    master - 36ebab56 Merge 16db6106c4198bb05276f6b06a3f824c5a49a469 into 2c2aceb800f5208a283222976359987d133a13c4

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.3.2-3082a8d0');
+const VERSION = new Version('9.3.2-36ebab56');
 
 /**
  * @fileoverview added by tsickle
