8d0cdc861e89c6eeb886289696d351d64b520bdb
卡色 <cipchk@qq.com>
2020-06-14 12:15:12 +0000

    master - ee7bb94f Merge d175e93f6be0104a9c27de27c5319b67f3bb6527 into cc081b864c231df0835a76ca8eb590eee8f37c68

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-49021bf0');
+var VERSION = new Version('9.4.0-ee7bb94f');
 
 /**
  * @fileoverview added by tsickle
