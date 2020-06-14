e20faa2c1fb32b4ca735017479545ab7bd4512dd
卡色 <cipchk@qq.com>
2020-06-14 11:45:17 +0000

    master - ac559af7 Merge e4ea5cb979b5cebae90b2a12bea1fed065431613 into cc081b864c231df0835a76ca8eb590eee8f37c68

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-cc081b86');
+var VERSION = new Version('9.4.0-ac559af7');
 
 /**
  * @fileoverview added by tsickle
