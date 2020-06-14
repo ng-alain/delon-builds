a966bfaa0af8b9787f0f0165961fe8e53dadfc8d
卡色 <cipchk@qq.com>
2020-06-14 12:28:14 +0000

    master - b2033c7c Merge f20ef95b87cb0942cf0aaf42cd76f66c890f8389 into cc081b864c231df0835a76ca8eb590eee8f37c68

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-ee7bb94f');
+var VERSION = new Version('9.4.0-b2033c7c');
 
 /**
  * @fileoverview added by tsickle
