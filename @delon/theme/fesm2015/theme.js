13001bce3809b6509d3356e39a6caac39aeabded
卡色 <cipchk@qq.com>
2020-06-14 16:01:10 +0000

    master - 490d26fd docs: typo (#960)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3662,7 +3662,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.4.0-b2033c7c');
+const VERSION = new Version('9.4.0-490d26fd');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-b2033c7c');
+var VERSION = new Version('9.4.0-490d26fd');
 
 /**
  * @fileoverview added by tsickle
