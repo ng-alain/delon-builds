ee9928812f0cbfa8f275b9a81e903b79be4c0ef4
卡色 <cipchk@qq.com>
2020-06-21 07:57:03 +0000

    master - bf0aa887 fix(abc:st): fix losing `nzWidthConfig` (#964)

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-16f9af6d');
+var VERSION = new Version('9.4.0-bf0aa887');
 
 /**
  * @fileoverview added by tsickle
