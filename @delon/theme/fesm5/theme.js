0a9da7067349f6a5c7559600f08def6d77a812a5
卡色 <cipchk@qq.com>
2020-06-22 05:23:52 +0000

    master - e3927ba8 Merge 26c9e38838d6b67b6a3414b5f025cea4f5291a42 into bf0aa8874817bf9d4fac40978cce29dcfea9b76a

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4307,7 +4307,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-64897685');
+var VERSION = new Version('9.4.0-e3927ba8');
 
 /**
  * @fileoverview added by tsickle
