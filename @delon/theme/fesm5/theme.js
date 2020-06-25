85a1b69fd1fcb8951ab2c9c8b93b5bed500d173e
卡色 <cipchk@qq.com>
2020-06-25 04:09:49 +0000

    master - 167058ad Merge 7987709a85c47eb2ff0672248db81715f7a8bffc into 4512f93239142dd216c1aeacd3b085f1b4717a7c

--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4267,7 +4267,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.4.0-4512f932');
+var VERSION = new Version('9.4.1');
 
 /**
  * @fileoverview added by tsickle
