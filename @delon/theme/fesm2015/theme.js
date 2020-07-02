624171ab800a91d3595ef769d076fd941d2786ee
卡色 <cipchk@qq.com>
2020-07-02 16:46:18 +0000

    master - d445935c fix(chart:timeline): fix invalid format of slider (#981)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3711,7 +3711,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.5.0-f97712ee');
+const VERSION = new Version('9.5.0-d445935c');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4319,7 +4319,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.5.0-f97712ee');
+var VERSION = new Version('9.5.0-d445935c');
 
 /**
  * @fileoverview added by tsickle
