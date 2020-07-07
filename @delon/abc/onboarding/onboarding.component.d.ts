2caad87b70b0178a62399a54a1f561b1bb9e2af8
卡色 <cipchk@qq.com>
2020-07-07 16:27:49 +0000

    master - 0513e713 Merge 3b3b2dc358a23e86e503591003f4e46216929041 into c3f29a0f45d1bc5bda55982d75484f7ed1ca5306

---
+++ @delon/abc/onboarding/onboarding.component.d.ts
@@ -0,0 +1,30 @@
+import { Platform } from '@angular/cdk/platform';
+import { ChangeDetectorRef, ElementRef, EventEmitter, OnDestroy } from '@angular/core';
+import { OnboardingConfig, OnboardingItem, OnboardingOpType } from './onboarding.types';
+export declare class OnboardingComponent implements OnDestroy {
+    private el;
+    private doc;
+    private platform;
+    private cdr;
+    private time;
+    private prevSelectorEl;
+    config: OnboardingConfig;
+    item: OnboardingItem;
+    active: number;
+    max: number;
+    readonly op: EventEmitter<OnboardingOpType>;
+    running: boolean;
+    get first(): boolean;
+    get last(): boolean;
+    private _getDoc;
+    private _getWin;
+    constructor(el: ElementRef<HTMLElement>, doc: any, platform: Platform, cdr: ChangeDetectorRef);
+    private getLightData;
+    private scroll;
+    updateRunning(status: boolean): void;
+    private updatePosition;
+    private updatePrevElStatus;
+    to(type: OnboardingOpType): void;
+    handleMask(): void;
+    ngOnDestroy(): void;
+}
