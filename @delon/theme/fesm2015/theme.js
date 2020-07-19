300150e5ebc9ab9df55459172860213ad75708a9
卡色 <cipchk@qq.com>
2020-07-19 07:56:51 +0000

    master - d4f8694f refactor: refactor @delon `dependencies` (#1006)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3793,7 +3793,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('9.5.4-89edb8f0');
+const VERSION = new Version('9.5.4-d4f8694f');
 
 /**
  * @fileoverview added by tsickle
--- @delon/theme/fesm5/theme.js
+++ @delon/theme/fesm5/theme.js
@@ -4401,7 +4401,7 @@ var AlainThemeModule = /** @class */ (function () {
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('9.5.4-89edb8f0');
+var VERSION = new Version('9.5.4-d4f8694f');
 
 /**
  * @fileoverview added by tsickle
