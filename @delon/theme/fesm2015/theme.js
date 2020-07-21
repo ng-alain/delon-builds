26911028e73fe3522b2db806192e98b2144eefd5
卡色 <cipchk@qq.com>
2020-07-21 12:17:12 +0000

    master - 31a547ff Merge 68b8d34a0bc9c65f5e9f12197fa6a6a6b28eee2b into 22f42f7cca654d88c371fdd1480b5d4027619184

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3793,7 +3793,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.1-1c341af4');
+const VERSION = new Version('10.0.0-beta.2');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
-exports.VERSION = '^10.0.0-beta.1';
+exports.VERSION = '^10.0.0-beta.2';
 exports.ZORROVERSION = '^10.0.0-beta.0';
 //# sourceMappingURL=lib-versions.js.map
