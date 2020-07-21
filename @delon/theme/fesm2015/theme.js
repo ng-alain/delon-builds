1427edb04cadbf2d3c78ef15aab77d4610080e5e
卡色 <cipchk@qq.com>
2020-07-21 11:55:32 +0000

    master - 1c341af4 Merge 13edaeef22679271c3b0a68b83329a8ccf23da59 into 22f42f7cca654d88c371fdd1480b5d4027619184

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3793,7 +3793,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.1');
+const VERSION = new Version('10.0.0-beta.1-1c341af4');
 
 /**
  * @fileoverview added by tsickle
