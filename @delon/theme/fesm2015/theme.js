6aad4732c2bdd484475ef8393f244d7845208d72
卡色 <cipchk@qq.com>
2020-07-23 11:19:02 +0000

    master - 9bf5bd59 Merge 080fe4f5af6c6d2e2f46446d1b98de70fbab3e74 into 3388e1fb071b99e02f87168ad8223101fe7f9c4d

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3866,7 +3866,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-998bf72a');
+const VERSION = new Version('10.0.0-beta.2-9bf5bd59');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
 exports.VERSION = '^10.0.0-beta.2';
-exports.ZORROVERSION = '^10.0.0-beta.0';
+exports.ZORROVERSION = '^10.0.0-beta.1';
 //# sourceMappingURL=lib-versions.js.map
