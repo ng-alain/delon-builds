b774e44e0b363bb7190fb85e9295bc2566aaaea3
卡色 <cipchk@qq.com>
2020-07-23 04:48:35 +0000

    master - 3388e1fb feat(auth): add `refresh` event (#1011)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3866,7 +3866,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-0351395a');
+const VERSION = new Version('10.0.0-beta.2-3388e1fb');
 
 /**
  * @fileoverview added by tsickle
