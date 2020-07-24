a3341318bb7c3dab85e88b5c062247fe267a24f6
卡色 <cipchk@qq.com>
2020-07-24 03:02:58 +0000

    master - c3d31c4a feat(chart:water-wave): add `render()` method (#1014)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3866,7 +3866,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-4d5a1f16');
+const VERSION = new Version('10.0.0-beta.2-c3d31c4a');
 
 /**
  * @fileoverview added by tsickle
