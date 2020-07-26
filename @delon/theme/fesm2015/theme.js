3f865126bd6ddb341a5f7476aab48c2db4d44fb7
卡色 <cipchk@qq.com>
2020-07-26 08:11:53 +0000

    master - 24e68a21 Merge 72b763b5550d40c901558c319bd5ebc61f896fc1 into c3d31c4a1b6f116e88bb849c88999fccd8ee7f69

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3866,7 +3866,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-c3d31c4a');
+const VERSION = new Version('10.0.0-beta.2-24e68a21');
 
 /**
  * @fileoverview added by tsickle
