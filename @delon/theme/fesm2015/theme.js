1752c07a54e7417d4390497d20929efa3c5bfb18
卡色 <cipchk@qq.com>
2020-07-31 14:08:34 +0000

    master - 945ed4b8 Merge 3616277c772ce654b6a6cb2a45b9aef3210b0cb1 into e1119f5c98e883f8544e9e181464495095f34c14

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3869,7 +3869,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-d5054bab');
+const VERSION = new Version('10.0.0-beta.2-945ed4b8');
 
 /**
  * @fileoverview added by tsickle
