df8b833080d6f350d9e531876fea6590745d60db
卡色 <cipchk@qq.com>
2020-07-31 10:30:11 +0000

    master - 06edf860 Merge b1eb5c3089c588b942cd92459081f90c7f3c9094 into e1119f5c98e883f8544e9e181464495095f34c14

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3869,7 +3869,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-7c2b55c7');
+const VERSION = new Version('10.0.0-beta.2-06edf860');
 
 /**
  * @fileoverview added by tsickle
