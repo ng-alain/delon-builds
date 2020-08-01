790db8c707462c8a601123276313bbb9de4baef5
卡色 <cipchk@qq.com>
2020-08-01 12:08:56 +0000

    master - e9f8eadc fix(abc:reuse-tab): fix remove tab when reuse is `false` (#1021)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3869,7 +3869,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-83481c10');
+const VERSION = new Version('10.0.0-beta.2-e9f8eadc');
 
 /**
  * @fileoverview added by tsickle
