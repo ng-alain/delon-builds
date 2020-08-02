0dbbc557d1aaffc49b8ab69df402778de23670e5
卡色 <cipchk@qq.com>
2020-08-02 07:52:05 +0000

    master - 98ef3ca4 docs: fix loading (#1022)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3869,7 +3869,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.2-6eea9a39');
+const VERSION = new Version('10.0.0-beta.2-98ef3ca4');
 
 /**
  * @fileoverview added by tsickle
