ae1a04289a356a0d647042acb0404ccf4829f4cb
卡色 <cipchk@qq.com>
2020-08-03 07:19:33 +0000

    master - ff83c291 Merge fee2e9845d62dcae1d5d38a30e250aaf0ab4a239 into c5dd253c930095248baeabb61ed6a679f743d131

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3-5f151ece');
+const VERSION = new Version('10.0.0-beta.3-ff83c291');
 
 /**
  * @fileoverview added by tsickle
