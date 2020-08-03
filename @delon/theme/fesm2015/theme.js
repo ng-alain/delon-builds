ea028047dcb0ff5e24ff683fffd82dd4446c8f1a
卡色 <cipchk@qq.com>
2020-08-03 11:13:19 +0000

    master - 69a04ee1 feat(abc:reuse-tab): add type of `_onReuseInit` (#1026)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3-fd735d0c');
+const VERSION = new Version('10.0.0-beta.3');
 
 /**
  * @fileoverview added by tsickle
