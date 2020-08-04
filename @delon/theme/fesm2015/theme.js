bb595f3dcf5e47fd13234790fa8d46ec63a527aa
卡色 <cipchk@qq.com>
2020-08-04 14:47:31 +0000

    master - 6f86d8db fix(abc:st): fix `width` can't be reset when refresh group columns (#1027)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3-3f033c56');
+const VERSION = new Version('10.0.0-beta.3-6f86d8db');
 
 /**
  * @fileoverview added by tsickle
