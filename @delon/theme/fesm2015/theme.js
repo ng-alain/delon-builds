2bef20f8826bada2211b371218836918c3c2d85e
卡色 <cipchk@qq.com>
2020-08-05 05:08:09 +0000

    master - ef13248e docs: add beian (#1029)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3');
+const VERSION = new Version('10.0.0-beta.3-ef13248e');
 
 /**
  * @fileoverview added by tsickle
