d85fdf5d7bd674c0fba723c5c0594e65687857b4
卡色 <cipchk@qq.com>
2020-08-05 09:07:20 +0000

    master - 2919f41c Merge 1072ea54039a369e5eb70710d6c2c4d42c6c09c4 into a64f9efdf73f3a3fea46ae664dca742dba8af18a

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3-a64f9efd');
+const VERSION = new Version('10.0.0-beta.3-2919f41c');
 
 /**
  * @fileoverview added by tsickle
