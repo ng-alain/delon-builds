9bd1d7b27924251e12772f119e80ed0688b85b24
卡色 <cipchk@qq.com>
2020-08-10 05:25:07 +0000

    master - 3bece70a Merge ab132449ddc207cb977c595ef1bd9a89dd80d796 into 26f60cec7e2a53f5d91c1c6c0a8428567fc39781

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.3-d032cbf0');
+const VERSION = new Version('10.0.0-beta.3-3bece70a');
 
 /**
  * @fileoverview added by tsickle
