5e94e213a2e66bc9d6242bd3b76429935054e014
卡色 <cipchk@qq.com>
2020-08-15 13:53:04 +0000

    master - 6c8e5710 feat: use `ng-alain-plugin-theme` instand of `scripts/themes.js` (#1039)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3867,7 +3867,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.4-5b4b08ea');
+const VERSION = new Version('10.0.0-beta.4');
 
 /**
  * @fileoverview added by tsickle
