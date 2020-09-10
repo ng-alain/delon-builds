e1e2d530f8d5935860b9d4061352fa823dae7efc
卡色 <cipchk@qq.com>
2020-09-10 02:19:17 +0000

    master - 952fe052 Merge d1d50ca9b0e6e97f9e4f5343ae1959d878a3c90c into cba82eae078a5e2d419ea12f8350d76ac76dac06

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.4-cba82eae');
+const VERSION = new Version('10.0.0-beta.4-952fe052');
 
 /**
  * @fileoverview added by tsickle
