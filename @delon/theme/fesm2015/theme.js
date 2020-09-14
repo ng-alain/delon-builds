fb6da43d6e150ef8141aef19d62227a6fc76ddbc
卡色 <cipchk@qq.com>
2020-09-14 03:24:10 +0000

    master - 08de51ce feat(abc:st): support resizable headers (#1049)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.4-6c20ffc1');
+const VERSION = new Version('10.0.0-beta.4-08de51ce');
 
 /**
  * @fileoverview added by tsickle
