1b90479f6b9350a40a449177931920a0f630a8ad
卡色 <cipchk@qq.com>
2020-09-14 02:47:26 +0000

    master - 391b2789 Merge 337b9ee4489dd416459fb903212e3497afb9efeb into 419c80e85ccdf66ad554551985cadfce739e4152

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.4-419c80e8');
+const VERSION = new Version('10.0.0-beta.4-391b2789');
 
 /**
  * @fileoverview added by tsickle
