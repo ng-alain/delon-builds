a21ab19eddc0297bc749e392da408c2de8ab5ea9
卡色 <cipchk@qq.com>
2020-09-28 09:54:00 +0000

    master - c5aec300 fix: change `extends` from `tsconfig.base.json` to `tsconfig.json` (#1051)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.6');
+const VERSION = new Version('10.0.0-beta.6-c5aec300');
 
 /**
  * @fileoverview added by tsickle
