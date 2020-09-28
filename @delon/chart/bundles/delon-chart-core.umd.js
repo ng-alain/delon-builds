c6ae2fa6f1b5605b60148221a724a55783e746a3
卡色 <cipchk@qq.com>
2020-09-28 09:24:40 +0000

    master - 72679c1d Merge fe9b44cc54fa0fbfd8846a8a0f6d6b3e5a008554 into be4f6dab85ccaa06ac9522a64842ef5bdba8f548

--- @delon/chart/bundles/delon-chart-core.umd.js
+++ @delon/chart/bundles/delon-chart-core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v10.0.0-beta.5
+ * @license ng-alain(cipchk@qq.com) v10.0.0-beta.6
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-beta.5');
+const VERSION = new Version('10.0.0-beta.6');
 
 /**
  * @fileoverview added by tsickle
