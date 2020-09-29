81ec508c1ce2e30991eee38514e291daa99502aa
56cargo-lzy <67145889+56cargo-lzy@users.noreply.github.com>
2020-09-29 09:13:40 +0000

    master - 32d359cd Merge 783a8d306881db0e18563878b148f6927598e996 into 889c1f00c67dd8f59c4f778e56a951b7665c3853

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3877,7 +3877,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0');
+const VERSION = new Version('10.0.0-32d359cd');
 
 /**
  * @fileoverview added by tsickle
