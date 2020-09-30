b15156426c6150df317025dbf54b43e428282919
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2020-09-30 07:52:41 +0000

    master - a8abd2f6 refactor: forward-compatibility for upcoming type improvements in Angular Pipes (#1054)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3894,7 +3894,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.0-fe287c14');
+const VERSION = new Version('10.0.0-a8abd2f6');
 
 /**
  * @fileoverview added by tsickle
