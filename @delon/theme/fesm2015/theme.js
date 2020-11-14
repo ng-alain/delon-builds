720f9e6897de440d9413b423ef50a5ade8aa98d8
Xy718 <45084242+Xy718@users.noreply.github.com>
2020-11-14 07:43:53 +0000

    master - a512d61f docs: improved the reading experience of some docs (#1065)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.0.3-3a5fe201');
+const VERSION = new Version('10.0.3');
 
 /**
  * @fileoverview added by tsickle
