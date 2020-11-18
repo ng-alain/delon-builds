6ec6aeb3d299caa224c341bc77109809eefd43f3
卡色 <cipchk@qq.com>
2020-11-18 12:01:46 +0000

    master - 41cada6c Merge b5a9eeb1c59e4e8d80304aab9ac9eafb8ef51323 into 45374a96ede62f2b36e63c62e98026170406aa6c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.0-2f7946c2');
+const VERSION = new Version('10.1.0-41cada6c');
 
 /**
  * @fileoverview added by tsickle
