74b88035cd4602d53b029a5a8787422861f7e735
卡色 <cipchk@qq.com>
2020-11-18 10:47:41 +0000

    master - 6f7efe77 fix(form): fix invalid error style (#1073)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.0-a0358c79');
+const VERSION = new Version('10.1.0');
 
 /**
  * @fileoverview added by tsickle
