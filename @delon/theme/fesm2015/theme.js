fa421a9102a32d5515284cfa0b52db55e4c94dac
卡色 <cipchk@qq.com>
2020-11-19 10:16:58 +0000

    master - 5de1560b chore: remove primary entry points (#1078)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.1-d9141287');
+const VERSION = new Version('10.1.1-5de1560b');
 
 /**
  * @fileoverview added by tsickle
