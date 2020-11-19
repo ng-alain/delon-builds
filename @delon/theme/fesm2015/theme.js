dded8aef1335cfc2dfdea33bb7d65b7009d19e46
卡色 <cipchk@qq.com>
2020-11-19 06:23:05 +0000

    master - a4e85577 Merge d2f22ab75d40ac947234b731821dc695fa9a3bba into dfbabce05d1903176011351e372fef73c2dc6449

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.1-c0b95873');
+const VERSION = new Version('10.1.1-a4e85577');
 
 /**
  * @fileoverview added by tsickle
