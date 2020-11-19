6bcf118affac4d84fd19840fc65df1751f7e85fd
卡色 <cipchk@qq.com>
2020-11-19 06:44:04 +0000

    master - 0fd1adcf Merge 04e1f77d2f4b9153e5fbe846374e864f361ba64f into dfbabce05d1903176011351e372fef73c2dc6449

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.1-4ae99adf');
+const VERSION = new Version('10.1.1-0fd1adcf');
 
 /**
  * @fileoverview added by tsickle
