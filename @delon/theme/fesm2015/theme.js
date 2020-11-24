e3cb72eb0bae9d7958eeea0dc12ce7e8406bfc7c
卡色 <cipchk@qq.com>
2020-11-24 07:28:05 +0000

    master - d25996c3 fix(abc:st): fix don't interrupt when invalid get data (#1079)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.1-aac3a5e4');
+const VERSION = new Version('10.1.1-d25996c3');
 
 /**
  * @fileoverview added by tsickle
