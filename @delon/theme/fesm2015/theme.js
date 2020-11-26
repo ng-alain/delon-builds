9a0c05c5e1bbb97d12b3b70911d569313207575e
卡色 <cipchk@qq.com>
2020-11-26 09:40:05 +0000

    master - 53eabbf6 chore: bump angular from 10 to 11 (#1080)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-31a9c272');
+const VERSION = new Version('10.1.2');
 
 /**
  * @fileoverview added by tsickle
