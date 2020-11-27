5934ca2203a03a937f843ecbda881de80a98c3bc
卡色 <cipchk@qq.com>
2020-11-27 06:44:39 +0000

    master - 9db80ece Merge 61d3c94413154bcd2408f778f5683e4763665500 into 53eabbf66c0e7b6e6a9e5f318d8d9030c8d3ec0d

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-a807b3fd');
+const VERSION = new Version('10.1.2-9db80ece');
 
 /**
  * @fileoverview added by tsickle
