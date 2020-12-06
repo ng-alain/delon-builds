815766a6871f64a2f6663ae30a2bbb8fd3492a9c
卡色 <cipchk@qq.com>
2020-12-06 05:49:24 +0000

    master - 82868cc4 Merge 2496458d2260d5a77b5108e220707d89b67b72ff into a43d689c067989f15edadf78113a63cace45c559

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-a43d689c');
+const VERSION = new Version('10.1.2-82868cc4');
 
 /**
  * @fileoverview added by tsickle
