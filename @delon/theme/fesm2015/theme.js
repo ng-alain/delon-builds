1c495414949635558189672a8ec496e92b88cadc
卡色 <cipchk@qq.com>
2020-12-08 08:46:34 +0000

    master - fa5a42f0 Merge 55ed9bcf8afeba0f3d0d31d09d28822e84fe5c17 into e5fa0ee0d1e8f1fc84b83cd4cf2d152648fc1b64

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-701a08da');
+const VERSION = new Version('10.1.2-fa5a42f0');
 
 /**
  * @fileoverview added by tsickle
