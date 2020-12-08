28cd7b48873e7c3bc3dab830776b4093c5ad2246
卡色 <cipchk@qq.com>
2020-12-08 09:41:08 +0000

    master - a43aade1 Merge 297fd3b366f110d1c435a5239c53c542f338fccd into e5fa0ee0d1e8f1fc84b83cd4cf2d152648fc1b64

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-1d89c2e9');
+const VERSION = new Version('10.1.2-a43aade1');
 
 /**
  * @fileoverview added by tsickle
