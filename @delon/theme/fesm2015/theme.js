344e1a7e3a4481197c246defe6d0cbc59518a9dc
卡色 <cipchk@qq.com>
2020-12-08 09:11:21 +0000

    master - 1e239aeb Merge eb18f3d402d8c31d38bb7f61f0dd5aef36e05af8 into e5fa0ee0d1e8f1fc84b83cd4cf2d152648fc1b64

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-fa5a42f0');
+const VERSION = new Version('10.1.2-1e239aeb');
 
 /**
  * @fileoverview added by tsickle
