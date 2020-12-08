6bfecd2c9d605862735ca3e9392a28db2fd5254c
卡色 <cipchk@qq.com>
2020-12-08 12:10:25 +0000

    master - 81d5f1ff Merge 98e2d3aa9ade58cbb302d764372dc77185798e04 into deed7950b7f788ed8af9ded7cc7106ce4cb4185c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-deed7950');
+const VERSION = new Version('10.1.2-81d5f1ff');
 
 /**
  * @fileoverview added by tsickle
