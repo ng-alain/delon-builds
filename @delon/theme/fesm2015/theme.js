a0f1c72052500402a9f9acad60aca3540e2c3c87
卡色 <cipchk@qq.com>
2020-12-08 09:35:25 +0000

    master - 1d89c2e9 Merge cb203c2b459de79dd94d7083f6687aa26790425d into e5fa0ee0d1e8f1fc84b83cd4cf2d152648fc1b64

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-1e239aeb');
+const VERSION = new Version('10.1.2-1d89c2e9');
 
 /**
  * @fileoverview added by tsickle
