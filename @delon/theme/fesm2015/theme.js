69ed737c3d4e4f2cb6388c486e6ab0f9c2326671
卡色 <cipchk@qq.com>
2020-12-08 12:04:55 +0000

    master - deed7950 feat(theme): add layout components (#1089)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-a6be96c3');
+const VERSION = new Version('10.1.2-deed7950');
 
 /**
  * @fileoverview added by tsickle
