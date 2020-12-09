2651b0dd44d1718e0788c0a5e37a7fadb9b38c25
Benzara Tahar Benlahcene <71077112+benzara-tahar@users.noreply.github.com>
2020-12-09 22:05:43 +0000

    master - 8e754687 Merge ceb5f7c3fc6d2a6c4a41640ee1f1dda53a41f3eb into 3b97fc3f33a58ff2701df52d1a0a055dfc523165

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-6b794f09');
+const VERSION = new Version('10.1.2-8e754687');
 
 /**
  * @fileoverview added by tsickle
