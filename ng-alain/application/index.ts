9097d263c7eb95ed72119bd5315473540d7f66ec
卡色 <cipchk@qq.com>
2020-12-09 10:04:32 +0000

    master - 0c94a33a Merge 43b2b98b723b9f2024d9cc6e07d0b1f97be0718d into deed7950b7f788ed8af9ded7cc7106ce4cb4185c

--- ng-alain/application/index.ts
+++ ng-alain/application/index.ts
@@ -111,7 +111,8 @@ function addDependenciesToPackageJson(options: ApplicationOptions): (host: Tree)
       [
         `ng-alain@${VERSION}`,
         `ng-alain-codelyzer@^0.0.1`,
-        `ng-alain-plugin-theme@^11.0.1`,
+        `ng-alain-plugin-theme@^11.0.0`,
+        `source-map-explorer@^2.5.1`,
         `@delon/testing@${VERSION}`,
       ],
       'devDependencies',
@@ -131,10 +132,12 @@ function addRunScriptToPackageJson(): (host: Tree) => void {
   return (host: Tree) => {
     const json = getPackage(host, 'scripts');
     if (json == null) return host;
+    json.scripts['ng-high-memory'] = `node --max_old_space_size=8000 ./node_modules/@angular/cli/bin/ng`;
     json.scripts.start = `ng s -o`;
     json.scripts.hmr = `ng s -o --hmr`;
-    json.scripts.build = `node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod`;
-    json.scripts.analyze = `node --max_old_space_size=5120 ./node_modules/@angular/cli/bin/ng build --prod --stats-json`;
+    json.scripts.build = `npm run ng-high-memory build -- --prod`;
+    json.scripts.analyze = `npm run ng-high-memory build -- --prod --source-map`;
+    json.scripts['analyze:view'] = `source-map-explorer dist/**/*.js`;
     json.scripts['test-coverage'] = `ng test --code-coverage --watch=false`;
     json.scripts['color-less'] = `ng-alain-plugin-theme -t=colorLess`;
     json.scripts.theme = `ng-alain-plugin-theme -t=themeCss`;
