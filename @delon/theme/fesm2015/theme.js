06417a48979aab788839700d258a86fe62a3788c
卡色 <cipchk@qq.com>
2020-12-09 11:55:39 +0000

    master - 3b97fc3f fix(util:time): fix invalid format `toDate` (#1093)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3885,7 +3885,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-4f3966d3');
+const VERSION = new Version('10.1.2-3b97fc3f');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/ng-update/upgrade-rules/v11/index.js
+++ ng-alain/ng-update/upgrade-rules/v11/index.js
@@ -23,14 +23,25 @@ function fixThirdVersion(tree, context) {
         `ngx-ueditor@^11.0.0`,
     ], 'dependencies');
     // dependencies
-    json_1.addPackageToPackageJson(tree, [`ng-alain-plugin-theme@^11.0.0`, `webpack-bundle-analyzer@^3.6.1`], 'devDependencies');
+    json_1.addPackageToPackageJson(tree, [`ng-alain-plugin-theme@^11.0.0`], 'devDependencies');
     log_1.logStart(context, `Upgrade third libs version number`);
+}
+function fixAnalyze(tree, context) {
+    const packageJson = json_1.getPackage(tree);
+    delete packageJson.devDependencies['webpack-bundle-analyzer'];
+    packageJson.devDependencies['source-map-explorer'] = '^2.5.1';
+    if (packageJson.scripts.analyze) {
+        packageJson.scripts.analyze = packageJson.scripts.analyze.replace(`--stats-json`, `--source-map`);
+        packageJson.scripts['analyze:view'] = `source-map-explorer dist/**/*.js`;
+    }
+    log_1.logStart(context, `Usd source-map-explorer instead of webpack-bundle-analyzer`);
 }
 function v11Rule() {
     return (tree, context) => {
         project = project_1.getProjectFromWorkspace(project_1.getWorkspace(tree));
         fixVersion(tree, context);
         fixThirdVersion(tree, context);
+        fixAnalyze(tree, context);
         hmr_1.fixHmr(project.sourceRoot, tree, context);
         layout_1.fixLayout(project, tree, context);
         context.logger.info(color_1.colors.green(`  ✓  Congratulations, Abort more detail please refer to upgrade guide https://github.com/ng-alain/ng-alain/issues/1863`));
