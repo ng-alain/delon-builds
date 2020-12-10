f65aca643ff89668b5cce93201f8557f4e8878ed
卡色 <cipchk@qq.com>
2020-12-10 13:42:57 +0000

    master - 997a66eb Merge 2a13f5cd578a15574d8c75085f3f76273fa56520 into f480acbaaa9c7fe4159d4676012069fb372dd7a6

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-f480acba');
+const VERSION = new Version('10.1.2-997a66eb');
 
 /**
  * @fileoverview added by tsickle
