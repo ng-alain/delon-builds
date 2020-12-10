3a486496fa2998cddc7a06b9754df535f1e749ec
卡色 <cipchk@qq.com>
2020-12-10 12:53:34 +0000

    master - 9252fffd Merge 2086ea7920903ad398463aac3785a4f965d69801 into b20259a787ca76671e9578ada060737ce32a6a92

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-544b1763');
+const VERSION = new Version('10.1.2-9252fffd');
 
 /**
  * @fileoverview added by tsickle
