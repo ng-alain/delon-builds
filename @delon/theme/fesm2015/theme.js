06591d18a8fba1602bcf7df9e4054023ee63c160
卡色 <cipchk@qq.com>
2020-12-11 11:59:30 +0000

    master - 6fa91d29 Merge f963668abab34e263c4766cd828bc3501ede44a1 into 8ae609c8459cce3db0d9973a63f3e1e35547ec98

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-8ae609c8');
+const VERSION = new Version('10.1.2-6fa91d29');
 
 /**
  * @fileoverview added by tsickle
