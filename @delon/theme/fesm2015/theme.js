820ac6cf8e4405567123681b8d2f33964667e3a2
卡色 <cipchk@qq.com>
2020-12-11 09:53:39 +0000

    master - 8ae609c8 feat(form:textarea): add `change`, `focus`, `blur` events (#1096)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-cc276bea');
+const VERSION = new Version('10.1.2-8ae609c8');
 
 /**
  * @fileoverview added by tsickle
