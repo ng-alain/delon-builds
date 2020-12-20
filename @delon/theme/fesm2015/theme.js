78106088adc01b6ca60dbb06ffb0b6202fce9942
卡色 <cipchk@qq.com>
2020-12-20 08:24:49 +0000

    master - 80ef0eaa Merge 4a78ea1da58da9b7f4dfc34babbf9999208637ca into dad5ed045d5c559cb2e4ab3b92f297e7c1ee9f3c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-97b422f6');
+const VERSION = new Version('10.1.2-80ef0eaa');
 
 /**
  * @fileoverview added by tsickle
