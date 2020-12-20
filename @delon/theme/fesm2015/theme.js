9dac7e9db064a59f0740eeefedea35dfb2c2a2b5
卡色 <cipchk@qq.com>
2020-12-20 06:11:13 +0000

    master - 97b422f6 Merge 48900242d8bf71526aecc48d3d8f3806eaeb7580 into dad5ed045d5c559cb2e4ab3b92f297e7c1ee9f3c

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-dad5ed04');
+const VERSION = new Version('10.1.2-97b422f6');
 
 /**
  * @fileoverview added by tsickle
