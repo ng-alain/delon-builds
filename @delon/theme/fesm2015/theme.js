e9b22b353882761b85673ad40d2c1a6a232a53b7
卡色 <cipchk@qq.com>
2020-12-20 06:05:15 +0000

    master - f7f6e3db Merge 89fc19cb63df913cda5cf7114c1c06e75a9d7cb4 into 0fa1e70a88a6cc400ff036b92556ebf3a67e33db

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('10.1.2-0fa1e70a');
+const VERSION = new Version('10.1.2-f7f6e3db');
 
 /**
  * @fileoverview added by tsickle
