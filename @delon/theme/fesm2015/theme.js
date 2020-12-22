987dec30908ce990321488474380c3b65d52e5be
卡色 <cipchk@qq.com>
2020-12-22 09:51:48 +0000

    master - a9f494a4 Merge 306af10603977cc2b5b98a7b355d9d3d7e24bced into 70ebb94c98116381c5dc32a07f3254675a56f6a3

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3973,7 +3973,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.0-b7dd1ccf');
+const VERSION = new Version('11.0.0-a9f494a4');
 
 /**
  * @fileoverview added by tsickle
