41311ac88e962207780649f35e613bc5c65840a8
卡色 <cipchk@qq.com>
2020-12-22 06:14:25 +0000

    master - f879cd21 Merge c0952f218bdd3eb316d27c59f7bed57acbb97168 into b43ecdecba1750e3275f9e6906d60bcb1588fa8b

--- @delon/abc/bundles/pageHeader.umd.js
+++ @delon/abc/bundles/pageHeader.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v10.1.2
+ * @license ng-alain(cipchk@qq.com) v11.0.0
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
-exports.VERSION = '^10.1.2';
+exports.VERSION = '^11.0.0';
 exports.ZORROVERSION = '^11.0.0';
 //# sourceMappingURL=lib-versions.js.map
