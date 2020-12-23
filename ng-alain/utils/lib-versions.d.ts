2f93de12f185a36a9e4c6a14c837541d686cf043
卡色 <cipchk@qq.com>
2020-12-23 13:55:05 +0000

    master - 0800a166 Merge fb056f82df6387352d0e56c16c615baa94cc9418 into a147f95cc1d8d0c7524827cdd732b3ece47490cb

--- ng-alain/utils/lib-versions.d.ts
+++ ng-alain/utils/lib-versions.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "^11.0.1";
+export declare const VERSION = "^11.0.2";
 export declare const ZORROVERSION = "^11.0.0";
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^11.0.1';
+export const VERSION = '^11.0.2';
 export const ZORROVERSION = '^11.0.0';
