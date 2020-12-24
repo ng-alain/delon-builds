05ae8c44305c7953b087d379baeaa7ff23d5a338
卡色 <cipchk@qq.com>
2020-12-24 10:24:20 +0000

    master - 70812aa4 Merge 13fd446f86dbd948d10dc1b2054b05d834898339 into 9ecaaf8380d95eb3daf90e4ac40c40eb548e71b0

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3984,7 +3984,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-9ecaaf83');
+const VERSION = new Version('11.0.2-70812aa4');
 
 /**
  * @fileoverview added by tsickle
