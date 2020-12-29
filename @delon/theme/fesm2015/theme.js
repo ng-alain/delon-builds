3de611facff7eb7084cbc9d9b5746a01a7197838
卡色 <cipchk@qq.com>
2020-12-29 06:33:01 +0000

    master - ae208e57 Merge 27e177557d534df5b95844d05d81985bb294a5ae into 0ea79ce0ecd994c50ed2283bae85595171029a66

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -3986,7 +3986,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-aff1de3d');
+const VERSION = new Version('11.0.2-ae208e57');
 
 /**
  * @fileoverview added by tsickle
