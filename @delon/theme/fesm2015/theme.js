9c19482f40a9cd37a611c1df1b7a234ec968ac44
卡色 <cipchk@qq.com>
2021-01-03 09:24:54 +0000

    master - 399f7547 Merge c34be67ed732b28630779ff4cf163230d2291989 into 1f4f67b239f6ea7227aca046ba9110642a43e774

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-bbf4642d');
+const VERSION = new Version('11.0.2-399f7547');
 
 /**
  * @fileoverview added by tsickle
