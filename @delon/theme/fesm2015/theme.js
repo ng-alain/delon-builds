5245f1870199ab11fd19103403cd66013fcda007
卡色 <cipchk@qq.com>
2021-01-03 06:26:41 +0000

    master - 1f4f67b2 feat(theme): add `RTLService` service (#1120)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-45d0dbc6');
+const VERSION = new Version('11.0.2-1f4f67b2');
 
 /**
  * @fileoverview added by tsickle
