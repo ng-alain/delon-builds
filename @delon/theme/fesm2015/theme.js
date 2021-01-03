1f2ffe83edcbe87719fcaf1603b87f8ac068b7b8
卡色 <cipchk@qq.com>
2021-01-03 06:48:48 +0000

    master - 6636c775 Merge a54ee9d1379baf361d5f192b48fbb1ffdeb657e5 into 1f4f67b239f6ea7227aca046ba9110642a43e774

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-1f4f67b2');
+const VERSION = new Version('11.0.2-6636c775');
 
 /**
  * @fileoverview added by tsickle
