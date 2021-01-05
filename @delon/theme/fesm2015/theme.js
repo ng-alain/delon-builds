891db25e562a7963cad670d0c36fad21fb2640b3
卡色 <cipchk@qq.com>
2021-01-05 05:11:31 +0000

    master - f3610545 Merge 950a4fd1011e4237ad785b3fc267fd99cb5989af into a481c1c257edaac603852c0096c5f4a4ecb371f5

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-a481c1c2');
+const VERSION = new Version('11.0.2-f3610545');
 
 /**
  * @fileoverview added by tsickle
