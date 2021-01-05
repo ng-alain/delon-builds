d91e2923f95ee1b1c0fbc519eb528f864bda8738
卡色 <cipchk@qq.com>
2021-01-05 06:04:36 +0000

    master - 637dcb54 Merge 18a61d4220b6495235d27bd8a9b6c3bef348a392 into a481c1c257edaac603852c0096c5f4a4ecb371f5

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-f3610545');
+const VERSION = new Version('11.0.2-637dcb54');
 
 /**
  * @fileoverview added by tsickle
--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
 exports.VERSION = '^11.0.2';
-exports.ZORROVERSION = '^11.0.0';
+exports.ZORROVERSION = '^11.0.1';
 //# sourceMappingURL=lib-versions.js.map
