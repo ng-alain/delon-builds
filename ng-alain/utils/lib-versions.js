95ac2fedf15d152a6bc84d81b801cfaf8c3c7408
卡色 <cipchk@qq.com>
2021-01-05 08:49:16 +0000

    master - 8370b9bd Merge becd5305ec699db3c862f84af3266f1ba8effb50 into a481c1c257edaac603852c0096c5f4a4ecb371f5

--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
-exports.VERSION = '^11.1.0-rc.0';
+exports.VERSION = '^11.0.2';
 exports.ZORROVERSION = '^11.0.1';
 //# sourceMappingURL=lib-versions.js.map
