a6a486f8d02d5aa69fe20d5af2d787c2743824a7
卡色 <cipchk@qq.com>
2021-01-05 05:10:50 +0000

    master - a481c1c2 fix(cli:plugin:icon): fix support conditional expression (#1121)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.0.2-cb7b5bbb');
+const VERSION = new Version('11.0.2-a481c1c2');
 
 /**
  * @fileoverview added by tsickle
