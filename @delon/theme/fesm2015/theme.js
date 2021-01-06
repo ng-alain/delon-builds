6e44e8dab9754384713f235822f6d28ce2212d09
卡色 <cipchk@qq.com>
2021-01-06 09:07:13 +0000

    master - 0ac979e4 feat(cli): use .template suffix for schematic template files (#1128)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.1.0-7d7a7b04');
+const VERSION = new Version('11.1.0-0ac979e4');
 
 /**
  * @fileoverview added by tsickle
