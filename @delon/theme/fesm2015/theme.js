fec93ca2a4c3a5dfa88caacda06d5bc4c328a547
卡色 <cipchk@qq.com>
2021-01-06 07:19:45 +0000

    master - 5ca5f8c3 Merge 2edb5953e0fc9556ae7bd520f88d163fd8d345b7 into 7ad6fd126877fdbf350d7b9741b986c6a3c814f9

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.1.0-ae108a6b');
+const VERSION = new Version('11.1.0-5ca5f8c3');
 
 /**
  * @fileoverview added by tsickle
