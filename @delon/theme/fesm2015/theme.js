1fc25dc6ba660a022945800895ada6babe1bcd4b
Len <30324072+lenvaz@users.noreply.github.com>
2021-01-10 09:08:00 +0000

    master - f64f6049 Merge af94a88dd5f5359e4218bed9ef965095ac114dd6 into 0ac979e4cc7dcbdfba923be6327e414f3991b2d7

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.1.0-0ac979e4');
+const VERSION = new Version('11.1.0-f64f6049');
 
 /**
  * @fileoverview added by tsickle
