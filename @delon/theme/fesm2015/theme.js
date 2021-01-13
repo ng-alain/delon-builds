2f76329560f5fb986e9834f9b9803f33d3c564b8
卡色 <cipchk@qq.com>
2021-01-13 08:58:42 +0000

    master - 876d4db7 feat(chart:number-info): support multiple style (#1133)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.1.0-dcb62849');
+const VERSION = new Version('11.1.0-876d4db7');
 
 /**
  * @fileoverview added by tsickle
