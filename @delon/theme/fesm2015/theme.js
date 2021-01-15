9229733b639b0eabc03e8ddf34b15142947a13e9
卡色 <cipchk@qq.com>
2021-01-15 06:17:22 +0000

    master - 2865c2d9 fix(theme): fix router animation is optional (#1136)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-f998c7e5');
+const VERSION = new Version('11.2.0-2865c2d9');
 
 /**
  * @fileoverview added by tsickle
