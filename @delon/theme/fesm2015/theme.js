f36f79bcea9b5d92705584f0b5940e40f6206404
卡色 <cipchk@qq.com>
2021-01-16 08:19:01 +0000

    master - a5db1699 Merge 2c6945301ea29d7b355b586059a77dee44923123 into 944ce60dfa60a7f1308a0cee1fd024702f3d0c7f

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-2765e0b1');
+const VERSION = new Version('11.2.0-a5db1699');
 
 /**
  * @fileoverview added by tsickle
