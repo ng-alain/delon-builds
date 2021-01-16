c00e0ac2cc99a368f65a08e20d49c0cde246e285
卡色 <cipchk@qq.com>
2021-01-16 07:31:56 +0000

    master - 3125d39f Merge b8358f34a6011b9ac55728798971272e6ab71936 into 2865c2d9f0e6398f5dd4eeba433e9aa3c0a57d77

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-de4dbf95');
+const VERSION = new Version('11.2.0-3125d39f');
 
 /**
  * @fileoverview added by tsickle
