288ae1e160002624e8442a0aa901557fc393b735
卡色 <cipchk@qq.com>
2021-01-23 02:58:20 +0000

    master - 0fd0c406 fix(abc:full-content): fix scroll bar cannot be displayed in the work… (#1140)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-e341c93f');
+const VERSION = new Version('11.2.0-0fd0c406');
 
 /**
  * @fileoverview added by tsickle
