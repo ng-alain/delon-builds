9c44f46fcdb285f95f75a99c81020c80f57990eb
卡色 <cipchk@qq.com>
2021-01-25 07:35:53 +0000

    master - 1da99c51 Merge 69ad950442e4a2031f2bac7dbe457d51d6dd823a into 0fd0c4064010b4286b1b7fa223ca4d9c966ac8ad

---
+++ @delon/abc/sv/sv-value.component.d.ts
@@ -0,0 +1,6 @@
+export declare class SVValueComponent {
+    prefix: string;
+    unit: string;
+    tooltip: string;
+    size: 'large' | 'small' | 'default';
+}
--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-0fd0c406');
+const VERSION = new Version('11.2.0-1da99c51');
 
 /**
  * @fileoverview added by tsickle
