fc63ae881874da0ec27d89e03ed10e568f21ffb2
卡色 <cipchk@qq.com>
2021-01-25 08:39:29 +0000

    master - 3a0d6e1c build: fix ng-zorro-antd 11.1.0 (#1142)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.2.0-7f89857b');
+const VERSION = new Version('11.2.0-3a0d6e1c');
 
 /**
  * @fileoverview added by tsickle
