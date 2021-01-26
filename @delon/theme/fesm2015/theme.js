f764544dbfca78e605ea6be3382942ebaf364ec5
卡色 <cipchk@qq.com>
2021-01-26 06:22:47 +0000

    master - b5e12ca4 Merge 979e01e0683fd01ea0eb780159c8944c25d5747c into a0d088f9bb995b54af0779be9662d057d239d147

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -4180,7 +4180,7 @@ AlainThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('11.3.0');
+const VERSION = new Version('11.3.0-b5e12ca4');
 
 /**
  * @fileoverview added by tsickle
