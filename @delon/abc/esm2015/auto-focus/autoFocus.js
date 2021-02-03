d5c84587d2f7816be7fe9df63d3113e01752d619
卡色 <cipchk@qq.com>
2021-02-03 13:10:51 +0000

    master - 13c6d12d Merge 19dd10e2691399018e98dab450547428676b92c7 into 0d940c354421ddb110ac2ccbe229bf0332703dda

---
+++ @delon/abc/esm2015/auto-focus/autoFocus.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview added by tsickle
+ * Generated from: autoFocus.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { AutoFocusDirective, AutoFocusModule } from './index';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXV0b0ZvY3VzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vLi4vLi4vcGFja2FnZXMvYWJjL2F1dG8tZm9jdXMvYXV0b0ZvY3VzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBSUEsb0RBQWMsU0FBUyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL2luZGV4JztcbiJdfQ==
