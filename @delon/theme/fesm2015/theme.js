eaf8172c6dce03b99692fb28ccdc6859b5c95ea9
卡色 <cipchk@qq.com>
2021-02-08 08:44:47 +0000

    master - 7375cdfb fix(util:ArrayService): fix cancel return readonly of `flat`, `uniq` (#1171)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2405,7 +2405,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.6.0-d37133e8');
+const VERSION = new Version('11.6.0');
 
 /**
  * Generated bundle index. Do not edit.
