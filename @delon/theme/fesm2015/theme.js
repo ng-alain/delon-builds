696a70bcd75ab0fa0b938a90b889ba7db0464275
卡色 <cipchk@qq.com>
2021-02-23 10:19:13 +0000

    master - 84ab014d feat(util:form): add `MatchControl` (#1178)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2488,7 +2488,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.6.0-ae0ff3b6');
+const VERSION = new Version('11.6.0-84ab014d');
 
 /**
  * Generated bundle index. Do not edit.
