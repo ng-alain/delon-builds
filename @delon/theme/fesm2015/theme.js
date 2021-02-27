cb9afe87a7f8af4684c013ed8b6366850394362e
卡色 <cipchk@qq.com>
2021-02-27 07:58:43 +0000

    master - 4bddcd82 chore: remove qrious lib of cli (#1189)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2494,7 +2494,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.6.0-7b0c3dce');
+const VERSION = new Version('11.6.0-4bddcd82');
 
 /**
  * Generated bundle index. Do not edit.
