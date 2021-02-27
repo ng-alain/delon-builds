76fdaf2b1a0ebfb86c2ce8243857d09784680880
卡色 <cipchk@qq.com>
2021-02-27 12:18:46 +0000

    master - 34c1e57e perf: optimize cli (#1192)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2494,7 +2494,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.6.0-0242e0b4');
+const VERSION = new Version('11.6.0-34c1e57e');
 
 /**
  * Generated bundle index. Do not edit.
