2953c6f7a535a34501432af0a900c954878042d5
卡色 <cipchk@qq.com>
2021-03-07 12:07:50 +0000

    master - b4806491 perf(abc:st): optimize the button text performance of rendered (#1203)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2494,7 +2494,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.7.1-c3615177');
+const VERSION = new Version('11.7.1-b4806491');
 
 /**
  * Generated bundle index. Do not edit.
