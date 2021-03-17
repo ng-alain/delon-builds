33c82040ec6f339ec353482ee47e4446160a19f2
卡色 <cipchk@qq.com>
2021-03-17 09:59:33 +0000

    master - bf89985b Merge 5c38ecc781c496c1b7980e15395761e42e75211a into d9fd20f30edfb34f38dc57a478a1fcd4b419de63

--- @delon/chart/bundles/mini-progress.umd.js
+++ @delon/chart/bundles/mini-progress.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v11.8.0
+ * @license ng-alain(cipchk@qq.com) v11.8.1
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
--- @delon/theme/bundles/theme.umd.js
+++ @delon/theme/bundles/theme.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v11.8.0
+ * @license ng-alain(cipchk@qq.com) v11.8.1
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
@@ -2960,7 +2960,7 @@
         { type: icon.NzIconService }
     ]; };
 
-    var VERSION = new i0.Version('11.8.0');
+    var VERSION = new i0.Version('11.8.1');
 
     /**
      * Generated bundle index. Do not edit.
