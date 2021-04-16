d58d39fc6cd89d3918c4c7eba699255e4cb74850
卡色 <cipchk@qq.com>
2021-04-16 12:03:35 +0000

    master - c20a5dae Merge f55192b8006de22ac404545cb26f28ba2adb156c into f49d45c99fffcf4bd7f08cbc1f89870432702fd0

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.9.0-afc1191d');
+const VERSION = new Version('11.9.0-c20a5dae');
 
 /**
  * Generated bundle index. Do not edit.
