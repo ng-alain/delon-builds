f0fe349c86fa7ef6529da977516d14a870b6bdec
卡色 <cipchk@qq.com>
2021-04-22 09:20:42 +0000

    master - 393f0a3a fix(form): fix losing format rules of ajv in 7.x version (#1239)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.9.0-ba834428');
+const VERSION = new Version('11.9.0-393f0a3a');
 
 /**
  * Generated bundle index. Do not edit.
