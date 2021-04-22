8245f45243e84dde6cb983ebab8caec6984ac8aa
卡色 <cipchk@qq.com>
2021-04-22 15:23:19 +0000

    master - 32a026b2 Merge 9bce50ebcde2fc9f4682444ac77b387c0e5cd22d into ce0432742649a2e375793720146ddfbf9df158f4

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.9.0-ce043274');
+const VERSION = new Version('11.9.0-32a026b2');
 
 /**
  * Generated bundle index. Do not edit.
