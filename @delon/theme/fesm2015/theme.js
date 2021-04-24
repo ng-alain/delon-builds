57f5cc54b236ea0e057c106e003b7a5f90c6ba9c
卡色 <cipchk@qq.com>
2021-04-24 07:42:44 +0000

    master - b1bc5372 fix(cli): fix excess `allowedCommonJsDependencies` (#1243)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.0-01a86ffe');
+const VERSION = new Version('11.10.0');
 
 /**
  * Generated bundle index. Do not edit.
