a4a86c60c47ec41deaf92aa20500a8d2c26eb37f
chenkaiC4 <chenkaiC4@gmail.com>
2021-04-28 16:38:10 +0000

    master - 2badc0cb Merge 4763f2631414f2dd248eed7aa137e331cc7a635e into faf2e9031d80c54ae7db5f0b8eab9068f3e72e0d

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.0-faf2e903');
+const VERSION = new Version('11.10.0-2badc0cb');
 
 /**
  * Generated bundle index. Do not edit.
