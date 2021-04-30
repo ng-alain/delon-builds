4cd484ef39cbf86f8795078645a1d90c8b817873
chenkaiC4 <chenkaiC4@gmail.com>
2021-04-30 07:16:02 +0000

    master - e60ffd71 docs(abc:reuse-tab): fix typo (#1245)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.0-1e3572a0');
+const VERSION = new Version('11.10.0-e60ffd71');
 
 /**
  * Generated bundle index. Do not edit.
