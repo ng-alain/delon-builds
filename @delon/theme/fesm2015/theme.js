00933d8fa610ac5b7f6566e1e453ec37879eed2f
卡色 <cipchk@qq.com>
2021-05-26 09:58:49 +0000

    master - bc11e1c7 Merge e0d30df2e5860a0045c4ccdfa28ebb684dc5e93b into 296b4f46e1b412a47ba5e998eabacb3953ccbccb

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.1-296b4f46');
+const VERSION = new Version('11.10.1-bc11e1c7');
 
 /**
  * Generated bundle index. Do not edit.
