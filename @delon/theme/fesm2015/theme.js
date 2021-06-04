3ae28b1b314c632e42d91d7f3c0375142e6774a7
卡色 <cipchk@qq.com>
2021-06-04 04:33:47 +0000

    master - 3a0b61cd Merge f8dad2ccf5f229e14f4558a5386532b842e604b0 into d544c1c37064b0e310f19bb6f9905c342180f5b7

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.2-177f677f');
+const VERSION = new Version('11.10.2-3a0b61cd');
 
 /**
  * Generated bundle index. Do not edit.
