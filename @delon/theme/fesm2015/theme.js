14f00f72c50522a15063648e3d5210015a91665a
卡色 <cipchk@qq.com>
2021-06-05 05:53:08 +0000

    master - 28aea066 Merge 878832eb89c210c02e591d764b754787076aca30 into d544c1c37064b0e310f19bb6f9905c342180f5b7

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.2-285e0d74');
+const VERSION = new Version('11.10.2-28aea066');
 
 /**
  * Generated bundle index. Do not edit.
