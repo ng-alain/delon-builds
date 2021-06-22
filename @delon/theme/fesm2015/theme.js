62ddeb379b95edda3359522c4b5c2b17e8958041
卡色 <cipchk@qq.com>
2021-06-22 06:49:47 +0000

    master - 10431c00 fix(form): fix should be triggered `reset` every time when can be displayed (#1260)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.2-25cb27b1');
+const VERSION = new Version('11.10.2');
 
 /**
  * Generated bundle index. Do not edit.
