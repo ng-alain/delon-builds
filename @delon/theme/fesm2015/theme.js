851a58ee668dbfbcad04ca7843f88e355fab7686
卡色 <cipchk@qq.com>
2021-07-12 08:32:56 +0000

    master - 9ac3a99c feat(chart:echarts): support resize (#1263)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2507,7 +2507,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.4');
+const VERSION = new Version('11.10.4-9ac3a99c');
 
 /**
  * Generated bundle index. Do not edit.
