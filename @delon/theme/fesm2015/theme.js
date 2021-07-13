14bef837993d1a94ac32720dc6ea6062d2367d48
卡色 <cipchk@qq.com>
2021-07-13 09:00:16 +0000

    master - 164d4b61 build: upgrade to node 14 (#1273)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2343,7 +2343,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('11.10.4-6413b022');
+const VERSION = new Version('11.10.4-164d4b61');
 
 /**
  * Generated bundle index. Do not edit.
