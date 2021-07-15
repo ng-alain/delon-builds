55abd56d520eea634dc73e9099d80d95e1ce1235
卡色 <cipchk@qq.com>
2021-07-15 10:38:33 +0000

    master - cd4e98d6 chore: remove simplemde (#1278)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-beta.0-92e05ebc');
+const VERSION = new Version('12.0.0-beta.0-cd4e98d6');
 
 /**
  * Generated bundle index. Do not edit.
