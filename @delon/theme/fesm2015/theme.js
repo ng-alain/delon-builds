756eafd1437773c68606591b08f971155edff1e6
卡色 <cipchk@qq.com>
2021-07-15 11:32:33 +0000

    master - 273aeede feat(theme): add enabled of util classes (#1280)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-beta.0-705338f8');
+const VERSION = new Version('12.0.0-beta.0-273aeede');
 
 /**
  * Generated bundle index. Do not edit.
