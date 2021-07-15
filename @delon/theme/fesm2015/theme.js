b4404f7a6d79a36f06c6b4c853e8345fc14c1070
卡色 <cipchk@qq.com>
2021-07-15 11:46:01 +0000

    master - 59ab6bc2 Merge 4c1aecd55726bd8dfa60d7e35b78660585054858 into cd983593e05fbdcdb83cb155d6b5f7fbba435702

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-beta.0-980b8a52');
+const VERSION = new Version('12.0.0-beta.0-59ab6bc2');
 
 /**
  * Generated bundle index. Do not edit.
