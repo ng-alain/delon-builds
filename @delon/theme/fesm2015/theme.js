4333d797c3633491a595b794d973d089e2cf52f5
卡色 <cipchk@qq.com>
2021-07-15 09:52:03 +0000

    master - 9646c946 Merge fd2813a04de2441948247a0bed7321d253bc0d26 into ba48cc3f574837064940f720323351172dd4e3df

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-beta.0-ba48cc3f');
+const VERSION = new Version('12.0.0-beta.0-9646c946');
 
 /**
  * Generated bundle index. Do not edit.
