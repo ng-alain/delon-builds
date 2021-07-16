5fa0d28bd20efc37e9c7c8f38db30e49d42dc711
卡色 <cipchk@qq.com>
2021-07-16 06:31:09 +0000

    master - 5ab0853a Merge 4de6fbab2ea5e82729427d78fb173d3a7c4ba095 into 858c616e1957ed78205753fd4a204c48ebc2ad36

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-beta.0-858c616e');
+const VERSION = new Version('12.0.0');
 
 /**
  * Generated bundle index. Do not edit.
--- @delon/util/bundles/browser.umd.js
+++ @delon/util/bundles/browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v12.0.0-beta.0
+ * @license ng-alain(cipchk@qq.com) v12.0.0
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
