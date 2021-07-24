529bdd44d6369ff643758836c4d7e47eb8bd0e6c
卡色 <cipchk@qq.com>
2021-07-24 07:47:30 +0000

    master - a394400c fix(theme:http): fix missing `HttpContext` (#1292)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.0-08f733a1');
+const VERSION = new Version('12.0.0-a394400c');
 
 /**
  * Generated bundle index. Do not edit.
