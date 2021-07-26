ab0bc37f55ece8638879c2b37b5b147690d552ef
卡色 <cipchk@qq.com>
2021-07-26 07:38:31 +0000

    master - dbe06424 fix(cli): add `fixAngularJson` option (#1294)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2359,7 +2359,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.1-2965967f');
+const VERSION = new Version('12.0.1');
 
 /**
  * Generated bundle index. Do not edit.
