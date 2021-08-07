3103378b06e1830c9d5c7f88d153aab9f3742e91
卡色 <cipchk@qq.com>
2021-08-07 06:23:53 +0000

    master - 6a94dbd6 feat(abc:exception): add `backRouterLink` property (#1299)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2360,7 +2360,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.0.3-f465d6d7');
+const VERSION = new Version('12.0.3-6a94dbd6');
 
 /**
  * Generated bundle index. Do not edit.
