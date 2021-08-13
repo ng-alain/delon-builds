9e1627192b2c4280fae34a11a88c5131d4f5dbf2
卡色 <cipchk@qq.com>
2021-08-13 09:30:28 +0000

    master - ce37d884 Merge a6a9433eee15225a419733248e7e0fb0d623e623 into fd97e51fd37d4e011940e3ae518a0fa24badf819

--- @delon/theme/bundles/theme.umd.js
+++ @delon/theme/bundles/theme.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-alain(cipchk@qq.com) v12.1.0
+ * @license ng-alain(cipchk@qq.com) v12.1.1
  * (c) 2020 cipchk https://ng-alain.com/
  * License: MIT
  */
@@ -2839,7 +2839,7 @@
         { type: icon.NzIconService }
     ]; };
 
-    var VERSION = new i0.Version('12.1.0');
+    var VERSION = new i0.Version('12.1.1');
 
     /**
      * Generated bundle index. Do not edit.
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^12.1.0';
+export const VERSION = '^12.1.1';
 export const ZORROVERSION = '^12.0.1';
