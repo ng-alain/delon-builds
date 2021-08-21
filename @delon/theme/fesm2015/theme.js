be24d0789e9e66105c540065461d90c7f891fdab
卡色 <cipchk@qq.com>
2021-08-21 07:38:58 +0000

    master - 71d3ef6b feat(theme): add `@nz-table-even-background` (#1325)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2360,7 +2360,7 @@ AlainThemeModule.ctorParameters = () => [
     { type: NzIconService }
 ];
 
-const VERSION = new Version('12.1.1-29302f54');
+const VERSION = new Version('12.1.1-71d3ef6b');
 
 /**
  * Generated bundle index. Do not edit.
