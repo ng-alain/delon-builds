f98b9d2fc0bb784e82d49a2588cc6918311ebe1c
卡色 <cipchk@qq.com>
2021-09-08 08:28:42 +0000

    master - da924d58 Merge b564fd1c2b96b492beb351b775e9ea898d011065 into 519adbe9c5f86068d2b97acf3a3921b491470521

--- ng-alain/application/files/src/app/shared/json-schema/json-schema.module.ts
+++ ng-alain/application/files/src/app/shared/json-schema/json-schema.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { DelonFormModule, WidgetRegistry } from '@delon/form';
+
 import { SharedModule } from '../shared.module';
+import { TestWidget } from './test/test.widget';
 
-// import { TinymceWidget } from './widgets/tinymce/tinymce.widget';
-// import { UEditorWidget } from './widgets/ueditor/ueditor.widget';
-
-export const SCHEMA_THIRDS_COMPONENTS = [
-  // TinymceWidget,
-  // UEditorWidget
-];
+export const SCHEMA_THIRDS_COMPONENTS = [TestWidget];
 
 @NgModule({
   declarations: SCHEMA_THIRDS_COMPONENTS,
-  imports: [
-    SharedModule,
-    DelonFormModule.forRoot()
-  ],
-  exports: [
-    ...SCHEMA_THIRDS_COMPONENTS
-  ]
+  imports: [SharedModule, DelonFormModule.forRoot()],
+  exports: SCHEMA_THIRDS_COMPONENTS
 })
 export class JsonSchemaModule {
   constructor(widgetRegistry: WidgetRegistry) {
-    // widgetRegistry.register(TinymceWidget.KEY, TinymceWidget);
-    // widgetRegistry.register(UEditorWidget.KEY, UEditorWidget);
+    widgetRegistry.register(TestWidget.KEY, TestWidget);
   }
 }
