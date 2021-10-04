85a79847a181fe71615136a1487b0734c7f41e22
卡色 <cipchk@qq.com>
2021-10-04 14:25:20 +0000

    master - db15c790 Merge 49d7ba21c971f924d6bf17f0f924d59eb30d23e3 into 64ac8223526e97384904d26c36e1cd44370d9acf

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2388,7 +2388,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('12.2.3-64ac8223');
+const VERSION = new Version('12.2.3-db15c790');
 
 /**
  * Generated bundle index. Do not edit.
