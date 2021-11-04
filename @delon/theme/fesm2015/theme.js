31a60dd4eed4e39752233317c214a12c67d9ac78
wyq-L <51746356+wyq-L@users.noreply.github.com>
2021-11-04 04:18:54 +0000

    master - 4d9df223 docs: 修改单词拼写错误 (#1372)

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2471,7 +2471,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('12.3.0-0c901c9b');
+const VERSION = new Version('12.3.0');
 
 /**
  * Generated bundle index. Do not edit.
