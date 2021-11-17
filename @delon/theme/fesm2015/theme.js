86a20da7b385cb0fdf4df211c8043514193be574
卡色 <cipchk@qq.com>
2021-11-17 15:59:09 +0000

    master - 4144fb4e Merge a1fa01903a24470f750e8c9680acfcde2fb9da08 into 4b27d6b5264194d898493c326941b1db2f3bdf40

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2503,7 +2503,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('12.3.0-48fcd6d0');
+const VERSION = new Version('12.3.0-4144fb4e');
 
 /**
  * Generated bundle index. Do not edit.
