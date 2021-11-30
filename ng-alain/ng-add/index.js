a8ff5cc802ef84eca13db3b9c9913fec733596a8
卡色 <cipchk@qq.com>
2021-11-30 06:53:20 +0000

    master - 81792e46 Merge 2f13bedc1e958c253a4003f2f90a2e7db31cc443 into a57170dd7d1b7955f334f910a3b4276952974289

--- ng-alain/ng-add/index.js
+++ ng-alain/ng-add/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getNodeMajorVersion = void 0;
 const color_1 = require("@angular/cli/utilities/color");
 const schematics_1 = require("@angular-devkit/schematics");
 const tasks_1 = require("@angular-devkit/schematics/tasks");
@@ -60,21 +61,31 @@ NG-ALAIN documentation site: https://ng-alain.com
 `));
     };
 }
+function getNodeMajorVersion() {
+    return +process.version.match(/^v(\d+)/)[1];
+}
+exports.getNodeMajorVersion = getNodeMajorVersion;
 function default_1(options) {
     return (tree, context) => {
         if (isUseCNPM()) {
-            throw new Error(`Sorry, Don't use cnpm to install dependencies, pls refer to: https://ng-alain.com/docs/faq#Installation`);
+            throw new schematics_1.SchematicsException(`Sorry, Don't use cnpm to install dependencies, pls refer to: https://ng-alain.com/docs/faq#Installation`);
+        }
+        const nodeVersion = getNodeMajorVersion();
+        const allowNodeVersions = [12, 14];
+        if (!allowNodeVersions.some(v => nodeVersion === v)) {
+            const versions = allowNodeVersions.join(', ');
+            throw new schematics_1.SchematicsException(`Sorry, currently only supports ${versions} major version number of node, pls refer to https://gist.github.com/LayZeeDK/c822cc812f75bb07b7c55d07ba2719b3`);
         }
         const pkg = utils_1.readPackage(tree);
         if (pkg.devDependencies['ng-alain']) {
-            throw new Error(`Already an NG-ALAIN project and can't be executed again: ng add ng-alain`);
+            throw new schematics_1.SchematicsException(`Already an NG-ALAIN project and can't be executed again: ng add ng-alain`);
         }
         let ngCoreVersion = pkg.dependencies['@angular/core'];
         if (/^[\^|\~]/g.test(ngCoreVersion)) {
             ngCoreVersion = ngCoreVersion.substr(1);
         }
         if (!ngCoreVersion.startsWith(`${V}.`)) {
-            throw new Error(`Sorry, the current version only supports angular ${V}.x, pls downgrade the global Anguar-cli version: [yarn global add @angular/cli@${V}] (or via npm: [npm install -g @angular/cli@${V}])`);
+            throw new schematics_1.SchematicsException(`Sorry, the current version only supports angular ${V}.x, pls downgrade the global Anguar-cli version: [yarn global add @angular/cli@${V}] (or via npm: [npm install -g @angular/cli@${V}])`);
         }
         return schematics_1.chain([genRules(options), finished()])(tree, context);
     };
