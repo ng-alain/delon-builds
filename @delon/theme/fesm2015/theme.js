2d350801d209ab74e8235ba74fb826295b8672e8
卡色 <cipchk@qq.com>
2021-12-16 07:39:09 +0000

    master - 2d47ebe6 Merge 0bd1899c172da9ca5249f3ff9d2354d7372bb608 into 8b827db0b694b80362e143df3f07a7618bc2ea79

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2503,7 +2503,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('12.4.2-8b827db0');
+const VERSION = new Version('12.4.2-2d47ebe6');
 
 /**
  * Generated bundle index. Do not edit.
