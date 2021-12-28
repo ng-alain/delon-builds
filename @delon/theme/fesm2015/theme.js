d304d05eea29567d161a94287d65b0d5144d9692
卡色 <cipchk@qq.com>
2021-12-28 04:40:41 +0000

    master - f7b95aa8 Merge 7b8913e4c603dda6e2883575d8943cef66672a62 into d18aa43e85772eb7231bb5970f5c9716abde7c4d

--- @delon/theme/fesm2015/theme.js
+++ @delon/theme/fesm2015/theme.js
@@ -2503,7 +2503,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('12.4.2-05301262');
+const VERSION = new Version('12.4.2-f7b95aa8');
 
 /**
  * Generated bundle index. Do not edit.
