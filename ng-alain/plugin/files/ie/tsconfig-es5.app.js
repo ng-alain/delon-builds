a06d3f4411c8a7fda20f3eba77938eb6bea03964
卡色 <cipchk@qq.com>
2022-01-01 07:59:09 +0000

    master - f7ea1747 Merge 4368c10e72e1a6c073b65586c94e71b8b90f7520 into c2279c47449360576609b3da47b47ff2b2449e94

--- ng-alain/plugin/files/ie/tsconfig-es5.app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    extends: './tsconfig.app.json',
-    compilerOptions: {
-        target: 'es5',
-    },
-};
-//# sourceMappingURL=tsconfig-es5.app.js.map
