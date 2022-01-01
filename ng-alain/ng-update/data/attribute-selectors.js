d9c7e4ccf2fa0cac80cc2eb264cfbc210d1aff5c
卡色 <cipchk@qq.com>
2022-01-01 07:26:04 +0000

    master - 78224850 Merge 7b32bf9a834018c4caf5ed40de42acd236d67920 into 11147c71f8066a3d5c094425528d35b5b3e0ad72

--- ng-alain/ng-update/data/attribute-selectors.js
+++ ng-alain/ng-update/data/attribute-selectors.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.attributeSelectors = void 0;
 const schematics_1 = require("@angular/cdk/schematics");
 exports.attributeSelectors = {
-    [schematics_1.TargetVersion.V12]: []
+    [schematics_1.TargetVersion.V13]: []
 };
 //# sourceMappingURL=attribute-selectors.js.map
--- ng-alain/ng-update/data/property-names.js
+++ ng-alain/ng-update/data/property-names.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.propertyNames = void 0;
 const schematics_1 = require("@angular/cdk/schematics");
 exports.propertyNames = {
-    [schematics_1.TargetVersion.V12]: []
+    [schematics_1.TargetVersion.V13]: []
 };
 //# sourceMappingURL=property-names.js.map
