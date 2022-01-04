f4e0a6a17a9228b09bc21a785b7f8b161a7ee902
卡色 <cipchk@qq.com>
2022-01-04 09:37:49 +0000

    master - 0ff1988c perf: more friendly generic support (#1422)

--- @delon/theme/src/services/settings/settings.service.d.ts
+++ @delon/theme/src/services/settings/settings.service.d.ts
@@ -27,9 +27,15 @@ export declare class SettingsService<L extends Layout = Layout, U extends User =
     get app(): A;
     get user(): U;
     get notify(): Observable<SettingsNotify>;
+    setLayout<T extends Layout = Layout>(name: T, value?: NzSafeAny): boolean;
     setLayout(name: string | L, value?: NzSafeAny): boolean;
+    getLayout<T>(): T;
+    setApp<T extends App = App>(value: T): void;
     setApp(value: A): void;
+    getApp<T>(): T;
+    setUser<T extends User = User>(value: T): void;
     setUser(value: U): void;
+    getUser<T>(): T;
     static ɵfac: i0.ɵɵFactoryDeclaration<SettingsService<any, any, any>, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<SettingsService<any, any, any>>;
 }
