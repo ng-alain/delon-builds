fea5db7234b3b4f782136215858a656219f48ad0
卡色 <cipchk@qq.com>
2022-01-15 12:05:47 +0000

    master - 24c80153 Merge 6ddeb07720ccb50bbb72b62ce87dc7a947561ea9 into c3dba05ca33a16f321098e46b5d88aea5ef3b98d

--- @delon/theme/fesm2015/theme.mjs
+++ @delon/theme/fesm2015/theme.mjs
@@ -2541,7 +2541,7 @@ class PreloadOptionalModules {
     }
 }
 
-const VERSION = new Version('13.0.0');
+const VERSION = new Version('13.1.0');
 
 /**
  * Generated bundle index. Do not edit.
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^13.0.0';
+export const VERSION = '^13.1.0';
 export const ZORROVERSION = '^13.0.0';
