a47c90caf29b4872efa154081673f7db93ff6136
卡色 <cipchk@qq.com>
2022-03-20 14:16:01 +0000

    master - da27ff14 Merge 4894939d095c433343cace956427113df7deb3f7 into 5f449c6b381822c01cddf5ff0b7eaa82ec042f1b

--- ng-alain/utils/code-style.js
+++ ng-alain/utils/code-style.js
@@ -4,6 +4,6 @@ exports.LINT_STAGED_CONFIG = exports.LINT_STAGED = void 0;
 exports.LINT_STAGED = 'lint-staged';
 exports.LINT_STAGED_CONFIG = {
     '(src)/**/*.{html,ts}': ['eslint --fix'],
-    '(src)/**/*.less': ['npx stylelint --syntax less --fix']
+    '(src)/**/*.less': ['npm run lint:style']
 };
 //# sourceMappingURL=code-style.js.map
