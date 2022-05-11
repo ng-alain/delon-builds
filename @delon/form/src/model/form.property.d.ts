c96ae08fe3193f9407fe0413017aa464b8bf7595
卡色 <cipchk@qq.com>
2022-05-11 10:46:22 +0000

    master - 1716f8da Merge fba945051011b24198b6bb9a511329f7e21f439c into 6f13f2287a26387e5f0ad5087b56d14d0a48882d

--- @delon/form/src/model/form.property.d.ts
+++ @delon/form/src/model/form.property.d.ts
@@ -60,8 +60,20 @@ export declare abstract class FormProperty {
     findRoot(): PropertyGroup;
     private isEmptyData;
     private setCustomErrors;
-    private mergeErrors;
-    protected setErrors(errors: ErrorData[], emitFormat?: boolean): void;
+    /**
+     * Set the current error message
+     *
+     * 设置当前错误消息
+     *
+     * @param emitFormat 若提供的消息带有 `{xx}` 会自动根据参数进行转化，包含自定义函数
+     *
+     * @example
+     *
+     * this.sf.getProperty('/name')?.setErrors({ keyword: 'required' });
+     * this.sf.getProperty('/name')?.setErrors({ message: 'Please input your username!' });
+     * this.sf.getProperty('/name')?.setErrors([]); // Clean error
+     */
+    setErrors(errors: ErrorData | ErrorData[], emitFormat?: boolean): void;
     setParentAndPlatErrors(errors: ErrorData[], path: string): void;
     /**
      * Set the hide or display of widget
