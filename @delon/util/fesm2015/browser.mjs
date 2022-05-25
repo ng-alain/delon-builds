d8937d103cd74b629100d731c6d8ada1dbd82d73
卡色 <cipchk@qq.com>
2022-05-25 12:03:36 +0000

    master - 12354f17 fix(abc:qr): fix can't center (#1481)

--- @delon/util/fesm2015/browser.mjs
+++ @delon/util/fesm2015/browser.mjs
@@ -110,26 +110,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.3.9", ngImpor
  * 复制字符串文档至剪贴板
  */
 function copy(value) {
-    return new Promise((resolve) => {
-        let copyTextArea = null;
-        try {
-            copyTextArea = document.createElement('textarea');
-            copyTextArea.style.height = '0px';
-            copyTextArea.style.opacity = '0';
-            copyTextArea.style.width = '0px';
-            document.body.appendChild(copyTextArea);
-            copyTextArea.value = value;
-            copyTextArea.select();
-            // eslint-disable-next-line deprecation/deprecation
-            document.execCommand('copy');
-            resolve(value);
-        }
-        finally {
-            if (copyTextArea && copyTextArea.parentNode) {
-                copyTextArea.parentNode.removeChild(copyTextArea);
-            }
-        }
-    });
+    return navigator.clipboard.writeText(value);
 }
 
 /**
