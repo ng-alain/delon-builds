223c2d67f3f68752b66eee5e3c08ffe8dfc53227
卡色 <cipchk@qq.com>
2022-05-29 06:30:08 +0000

    master - 0fdbcf70 Merge 1f47b2eb2ca016e5fe9156eb1d3ef64c5a8845f7 into b3072ac04c0b343ca9a4bf613c60a9bdf9c39df6

--- @delon/util/config/abc/media.type.d.ts
+++ @delon/util/config/abc/media.type.d.ts
@@ -1,4 +1,4 @@
-import type Plyr from 'plyr';
+import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 export interface AlainMediaConfig {
     /**
      * Plyr library path, default: `["https://cdn.bootcdn.net/ajax/libs/plyr/3.5.10/plyr.min.js", "https://cdn.bootcdn.net/ajax/libs/plyr/3.5.10/plyr.css"]`
@@ -7,5 +7,5 @@ export interface AlainMediaConfig {
     /**
      * Please refer to [plyr options](https://github.com/sampotts/plyr#options)
      */
-    options?: Plyr.Options;
+    options?: NzSafeAny;
 }
