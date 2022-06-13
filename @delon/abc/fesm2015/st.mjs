f45092031a2f2fe5f7a3775b7936bf86da92a43f
卡色 <cipchk@qq.com>
2022-06-13 10:44:20 +0000

    master - 93e0304c Merge 5eda481400d4385e61e03b3bd26b5a10e165c928 into 563815698c47720dc863c32bc9125e8dd4d7d86f

--- @delon/abc/fesm2015/st.mjs
+++ @delon/abc/fesm2015/st.mjs
@@ -1518,10 +1518,12 @@ class STComponent {
     set res(value) {
         const item = (this._res = deepMergeKey({}, true, this.cog.res, value));
         const reName = item.reName;
-        if (!Array.isArray(reName.list))
-            reName.list = reName.list.split('.');
-        if (!Array.isArray(reName.total))
-            reName.total = reName.total.split('.');
+        if (typeof reName !== 'function') {
+            if (!Array.isArray(reName.list))
+                reName.list = reName.list.split('.');
+            if (!Array.isArray(reName.total))
+                reName.total = reName.total.split('.');
+        }
         this._res = item;
     }
     get page() {
