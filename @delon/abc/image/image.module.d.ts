9c94072a9b2e98d21ae2e1709a9b2f313b1abab3
卡色 <cipchk@qq.com>
2022-08-14 07:46:24 +0000

    master - 6204cb8c refactor: remove `_src` component (#1488)

--- @delon/abc/image/image.module.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./image.directive";
-import * as i2 from "@angular/common";
-import * as i3 from "ng-zorro-antd/modal";
-export declare class ImageModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<ImageModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<ImageModule, [typeof i1.ImageDirective], [typeof i2.CommonModule, typeof i3.NzModalModule], [typeof i1.ImageDirective]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<ImageModule>;
-}
