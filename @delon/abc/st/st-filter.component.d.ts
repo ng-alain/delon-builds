c7d45fee8a5a2ba51ff06494c7cff4081bc68bc4
卡色 <cipchk@qq.com>
2022-09-08 10:57:08 +0000

    master - d18551c7 Merge 88bea627391eb3e4d8a38eee7c7f5b497154363a into b56d75ba203afbbc04875265edd80e119817e383

--- @delon/abc/st/st-filter.component.d.ts
+++ @delon/abc/st/st-filter.component.d.ts
@@ -1,9 +1,10 @@
-import { EventEmitter } from '@angular/core';
+import { ChangeDetectorRef, EventEmitter } from '@angular/core';
 import { LocaleData } from '@delon/theme';
 import { STColumnFilter, STColumnFilterMenu, STIcon } from './st.interfaces';
 import { _STColumn } from './st.types';
 import * as i0 from "@angular/core";
 export declare class STFilterComponent {
+    private cdr;
     visible: boolean;
     col: _STColumn;
     locale: LocaleData;
@@ -11,9 +12,11 @@ export declare class STFilterComponent {
     readonly n: EventEmitter<unknown>;
     readonly handle: EventEmitter<boolean>;
     get icon(): STIcon;
-    show($event: MouseEvent): void;
+    constructor(cdr: ChangeDetectorRef);
+    stopPropagation($event: MouseEvent): void;
     checkboxChange(): void;
     radioChange(item: STColumnFilterMenu): void;
+    close(): void;
     confirm(): void;
     reset(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<STFilterComponent, never>;
