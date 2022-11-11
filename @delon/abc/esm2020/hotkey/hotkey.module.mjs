9d735b7025923019b4ae5d5cda3f65e21798dd94
卡色 <cipchk@qq.com>
2022-11-11 11:17:55 +0000

    master - 6913f01f feat(abc:hotkey): add hotkey component (#1538)

---
+++ @delon/abc/esm2020/hotkey/hotkey.module.mjs
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { HotkeyDirective } from './hotkey.directive';
+import * as i0 from "@angular/core";
+const DIRECTIVES = [HotkeyDirective];
+export class HotkeyModule {
+}
+HotkeyModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.10", ngImport: i0, type: HotkeyModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+HotkeyModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "14.2.10", ngImport: i0, type: HotkeyModule, declarations: [HotkeyDirective], exports: [HotkeyDirective] });
+HotkeyModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "14.2.10", ngImport: i0, type: HotkeyModule });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.10", ngImport: i0, type: HotkeyModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    declarations: DIRECTIVES,
+                    exports: DIRECTIVES
+                }]
+        }] });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG90a2V5Lm1vZHVsZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uL3BhY2thZ2VzL2FiYy9ob3RrZXkvaG90a2V5Lm1vZHVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQUUsUUFBUSxFQUFFLE1BQU0sZUFBZSxDQUFDO0FBRXpDLE9BQU8sRUFBRSxlQUFlLEVBQUUsTUFBTSxvQkFBb0IsQ0FBQzs7QUFFckQsTUFBTSxVQUFVLEdBQUcsQ0FBQyxlQUFlLENBQUMsQ0FBQztBQU1yQyxNQUFNLE9BQU8sWUFBWTs7MEdBQVosWUFBWTsyR0FBWixZQUFZLGlCQU5MLGVBQWUsYUFBZixlQUFlOzJHQU10QixZQUFZOzRGQUFaLFlBQVk7a0JBSnhCLFFBQVE7bUJBQUM7b0JBQ1IsWUFBWSxFQUFFLFVBQVU7b0JBQ3hCLE9BQU8sRUFBRSxVQUFVO2lCQUNwQiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE5nTW9kdWxlIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5cbmltcG9ydCB7IEhvdGtleURpcmVjdGl2ZSB9IGZyb20gJy4vaG90a2V5LmRpcmVjdGl2ZSc7XG5cbmNvbnN0IERJUkVDVElWRVMgPSBbSG90a2V5RGlyZWN0aXZlXTtcblxuQE5nTW9kdWxlKHtcbiAgZGVjbGFyYXRpb25zOiBESVJFQ1RJVkVTLFxuICBleHBvcnRzOiBESVJFQ1RJVkVTXG59KVxuZXhwb3J0IGNsYXNzIEhvdGtleU1vZHVsZSB7fVxuIl19
