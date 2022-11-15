4626afad2003d941dba2a863dd23bffc8726f492
卡色 <cipchk@qq.com>
2022-11-15 11:11:58 +0000

    master - 2691520e Merge e77524810497a0a5e72df221eb830ba3d429070e into f71351bbd6a5841cae4ef077cae62b72255bdc81

--- @delon/util/config/config.types.d.ts
+++ @delon/util/config/config.types.d.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import { AlainDateRangePickerConfig, AlainErrorCollectConfig, AlainExceptionType, AlainImageConfig, AlainLoadingConfig, AlainLodopConfig, AlainMediaConfig, AlainOnboardingConfig, AlainPageHeaderConfig, AlainPdfConfig, AlainQRConfig, AlainSEConfig, AlainSGConfig, AlainSTConfig, AlainSVConfig, AlainXlsxConfig, AlainZipConfig } from './abc/index';
+import { AlainCellConfig, AlainDateRangePickerConfig, AlainErrorCollectConfig, AlainExceptionType, AlainImageConfig, AlainLoadingConfig, AlainLodopConfig, AlainMediaConfig, AlainOnboardingConfig, AlainPageHeaderConfig, AlainPdfConfig, AlainQRConfig, AlainSEConfig, AlainSGConfig, AlainSTConfig, AlainSVConfig, AlainXlsxConfig, AlainZipConfig } from './abc/index';
 import { AlainACLConfig } from './acl/acl.type';
 import { AlainAuthConfig } from './auth/auth.type';
 import { AlainCacheConfig } from './cache/cache.type';
@@ -24,6 +24,7 @@ export interface AlainConfig {
     sv?: AlainSVConfig;
     st?: AlainSTConfig;
     sf?: AlainSFConfig;
+    cell?: AlainCellConfig;
     xlsx?: AlainXlsxConfig;
     zip?: AlainZipConfig;
     pdf?: AlainPdfConfig;
