d913b338081b0624170c873931b8ff894e34a853
卡色 <cipchk@qq.com>
2023-08-01 08:12:29 +0000

    master - 54f802614 Merge 85cc1f1a257552f84781df7cbbebe572d4b6987e into 2c7def75a5b219a58319ab129407f4058010fc44

--- @delon/theme/src/pipes/yn/yn.pipe.d.ts
+++ @delon/theme/src/pipes/yn/yn.pipe.d.ts
@@ -2,6 +2,12 @@ import { PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as i0 from "@angular/core";
 export type YNMode = 'full' | 'icon' | 'text';
+export interface YNOptions {
+    yes?: string;
+    no?: string;
+    mode?: YNMode;
+}
+export declare function yn(value: boolean, opt?: YNOptions): string;
 export declare class YNPipe implements PipeTransform {
     private dom;
     constructor(dom: DomSanitizer);
