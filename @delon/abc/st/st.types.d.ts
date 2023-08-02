ee83c7b3e51ea0a9cd534889ab44931e12da7292
卡色 <cipchk@qq.com>
2023-08-02 15:07:23 +0000

    master - a02580587 Merge 5ca979562cf6677ed274fc6e767e84dfdbdbc00d into d815dbd1fc1f57a1a56b04a2380fd0ff86e63bf4

--- @delon/abc/st/st.types.d.ts
+++ @delon/abc/st/st.types.d.ts
@@ -1,6 +1,5 @@
 import { TemplateRef } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
-import type { CellOptions } from '@delon/abc/cell';
 import type { NgClassType } from 'ng-zorro-antd/core/types';
 import type { STColumn, STColumnButton, STColumnSafeType, STData, STIcon, STOnCellResult, STSortMap } from './st.interfaces';
 /**
@@ -72,8 +71,8 @@ export interface _STDataValue {
     _text: SafeHtml;
     org?: any;
     color?: string;
+    tooltip?: string;
     safeType: STColumnSafeType;
     buttons?: _STColumnButton[];
     props?: STOnCellResult | null;
-    cell?: CellOptions;
 }
