1ddc8012e8681edad3d0c85a8f1cfba689af3d7d
卡色 <cipchk@qq.com>
2023-09-16 16:12:06 +0000

    master - 5402797ec Merge 966d98ad0ce04fa9884067889f20b87da6f10f1e into 7a219338660bfbbe18f5ee35ccd1caaabf6e40a4

--- @delon/abc/onboarding/onboarding.types.d.ts
+++ @delon/abc/onboarding/onboarding.types.d.ts
@@ -5,6 +5,14 @@ import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 export type OnboardingOpType = 'next' | 'prev' | 'skip' | 'done';
 export interface OnboardingConfig {
     /**
+     * Storage identification Key, The default is `localStorage` local storage, allowing the use of `ONBOARDING_STORE_TOKEN` to change the storage method
+     */
+    key?: string;
+    /**
+     * Current version
+     */
+    keyVersion?: unknown;
+    /**
      * Onboarding items
      */
     items?: OnboardingItem[];
