508c969c53c31456b9aaa14ac56cf6750ddc3457
卡色 <cipchk@qq.com>
2023-10-21 10:35:05 +0000

    master - 5521a98e3 Merge 29baafb20421190d81c18fa274275a054e95fb22 into 3d6e01602077ad6be27f9c3cb6af0f7981db7416

--- ng-alain/ng-update/index.js
+++ ng-alain/ng-update/index.js
@@ -1,15 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.postUpdate = exports.updateToV16 = void 0;
+exports.postUpdate = exports.updateToV17 = exports.updateToV16 = void 0;
 const schematics_1 = require("@angular/cdk/schematics");
 const schematics_2 = require("@angular-devkit/schematics");
 const upgrade_data_1 = require("./upgrade-data");
 const v16_1 = require("./upgrade-rules/v16");
+const v17_1 = require("./upgrade-rules/v17");
 const migrations = [];
 function updateToV16() {
     return (0, schematics_2.chain)([(0, v16_1.v16Rule)(), (0, schematics_1.createMigrationSchematicRule)(schematics_1.TargetVersion.V16, migrations, upgrade_data_1.ruleUpgradeData, postUpdate)]);
 }
 exports.updateToV16 = updateToV16;
+function updateToV17() {
+    // , createMigrationSchematicRule(TargetVersion.V17, migrations, ruleUpgradeData, postUpdate)
+    return (0, schematics_2.chain)([(0, v17_1.v17Rule)()]);
+}
+exports.updateToV17 = updateToV17;
 /** Post-update schematic to be called when update is finished. */
 function postUpdate(context, targetVersion, hasFailures) {
     context.logger.info('');
