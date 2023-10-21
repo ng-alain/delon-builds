0dd0d441d87d1d767ae63d801efaff8c2cd5c727
卡色 <cipchk@qq.com>
2023-10-21 12:10:38 +0000

    master - a7aca5bd0 Merge bd6512122374128e388430618381c913f9ca1cc5 into 3d6e01602077ad6be27f9c3cb6af0f7981db7416

---
+++ @delon/form/fesm2022/widgets-qr-code.mjs
@@ -0,0 +1,92 @@
+import * as i0 from '@angular/core';
+import { Component, ViewEncapsulation, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as i1 from '@delon/form';
+import { ControlUIWidget, DelonFormModule } from '@delon/form';
+import * as i2 from 'ng-zorro-antd/qr-code';
+import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
+
+class QrCodeWidget extends ControlUIWidget {
+    static { this.KEY = 'qr-code'; }
+    refresh(qr) {
+        this.setValue(qr);
+        if (this.ui.refresh)
+            this.ui.refresh(qr);
+    }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidget, deps: null, target: i0.ɵɵFactoryTarget.Component }); }
+    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "16.2.10", type: QrCodeWidget, selector: "sf-qr-code", usesInheritance: true, ngImport: i0, template: `<sf-item-wrap
+    [id]="id"
+    [schema]="schema"
+    [ui]="ui"
+    [showError]="showError"
+    [error]="error"
+    [showTitle]="schema.title"
+  >
+    <nz-qrcode
+      [nzValue]="value"
+      [nzPadding]="ui.padding ?? 0"
+      [nzColor]="ui.color ?? '#000'"
+      [nzBgColor]="ui.bgColor ?? '#FFF'"
+      [nzSize]="ui.qrSize ?? 160"
+      [nzIcon]="ui.icon ?? ''"
+      [nzIconSize]="ui.iconSize ?? 40"
+      [nzBordered]="ui.bordered ?? false"
+      [nzStatus]="ui.status ?? 'active'"
+      [nzLevel]="ui.level ?? 'M'"
+      (nzRefresh)="refresh($event)"
+    />
+  </sf-item-wrap>`, isInline: true, dependencies: [{ kind: "component", type: i1.SFItemWrapComponent, selector: "sf-item-wrap", inputs: ["id", "schema", "ui", "showError", "error", "showTitle", "title"] }, { kind: "component", type: i2.NzQRCodeComponent, selector: "nz-qrcode", inputs: ["nzValue", "nzPadding", "nzColor", "nzBgColor", "nzSize", "nzIcon", "nzIconSize", "nzBordered", "nzStatus", "nzLevel"], outputs: ["nzRefresh"], exportAs: ["nzQRCode"] }], encapsulation: i0.ViewEncapsulation.None }); }
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidget, decorators: [{
+            type: Component,
+            args: [{
+                    selector: 'sf-qr-code',
+                    template: `<sf-item-wrap
+    [id]="id"
+    [schema]="schema"
+    [ui]="ui"
+    [showError]="showError"
+    [error]="error"
+    [showTitle]="schema.title"
+  >
+    <nz-qrcode
+      [nzValue]="value"
+      [nzPadding]="ui.padding ?? 0"
+      [nzColor]="ui.color ?? '#000'"
+      [nzBgColor]="ui.bgColor ?? '#FFF'"
+      [nzSize]="ui.qrSize ?? 160"
+      [nzIcon]="ui.icon ?? ''"
+      [nzIconSize]="ui.iconSize ?? 40"
+      [nzBordered]="ui.bordered ?? false"
+      [nzStatus]="ui.status ?? 'active'"
+      [nzLevel]="ui.level ?? 'M'"
+      (nzRefresh)="refresh($event)"
+    />
+  </sf-item-wrap>`,
+                    preserveWhitespaces: false,
+                    encapsulation: ViewEncapsulation.None
+                }]
+        }] });
+
+class QrCodeWidgetModule {
+    constructor(widgetRegistry) {
+        widgetRegistry.register(QrCodeWidget.KEY, QrCodeWidget);
+    }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidgetModule, deps: [{ token: i1.WidgetRegistry }], target: i0.ɵɵFactoryTarget.NgModule }); }
+    static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidgetModule, declarations: [QrCodeWidget], imports: [FormsModule, DelonFormModule, NzQRCodeModule] }); }
+    static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidgetModule, imports: [FormsModule, DelonFormModule, NzQRCodeModule] }); }
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: QrCodeWidgetModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    imports: [FormsModule, DelonFormModule, NzQRCodeModule],
+                    declarations: [QrCodeWidget]
+                }]
+        }], ctorParameters: function () { return [{ type: i1.WidgetRegistry }]; } });
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { QrCodeWidget, QrCodeWidgetModule };
+//# sourceMappingURL=widgets-qr-code.mjs.map
