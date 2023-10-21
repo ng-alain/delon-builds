6aa3ce0712ebc34a641e80f19e4b7a748f17647d
卡色 <cipchk@qq.com>
2023-10-21 13:00:22 +0000

    master - 77baaa620 Merge af4cc8eec0184991a295cddadd26cdee4d88068e into 3d6e01602077ad6be27f9c3cb6af0f7981db7416

--- @delon/form/fesm2022/widgets-color.mjs
+++ @delon/form/fesm2022/widgets-color.mjs
@@ -1,26 +1,30 @@
-import * as i2 from '@angular/common';
-import { CommonModule } from '@angular/common';
 import * as i0 from '@angular/core';
 import { Component, ViewEncapsulation, NgModule } from '@angular/core';
 import * as i1 from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import * as i1$1 from '@delon/form';
-import { ControlUIWidget, DelonFormModule } from '@delon/form';
-import * as i4 from 'ng-zorro-antd/color-picker';
-import { NzColorPickerModule } from 'ng-zorro-antd/color-picker';
+import { ControlUIWidget, getData, DelonFormModule } from '@delon/form';
+import * as i3 from 'ng-zorro-antd/segmented';
+import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
 
-class ColorWidget extends ControlUIWidget {
-    static { this.KEY = 'color'; }
-    _change(ev) {
-        if (this.ui.change)
-            this.ui.change(ev);
+class SegmentedWidget extends ControlUIWidget {
+    static { this.KEY = 'segmented'; }
+    get list() {
+        return this._list ?? [];
     }
-    _formatChange(ev) {
-        if (this.ui.formatChange)
-            this.ui.formatChange(ev);
+    reset(value) {
+        getData(this.schema, this.ui, value).subscribe(list => {
+            this._list = list;
+            this.detectChanges();
+        });
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: ColorWidget, deps: null, target: i0.ɵɵFactoryTarget.Component }); }
-    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "16.2.10", type: ColorWidget, selector: "sf-color", usesInheritance: true, ngImport: i0, template: `<sf-item-wrap
+    valueChange(index) {
+        if (this.ui.valueChange) {
+            this.ui.valueChange({ index, item: this.list[index] });
+        }
+    }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidget, deps: null, target: i0.ɵɵFactoryTarget.Component }); }
+    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "16.2.10", type: SegmentedWidget, selector: "sf-segmented", usesInheritance: true, ngImport: i0, template: `<sf-item-wrap
     [id]="id"
     [schema]="schema"
     [ui]="ui"
@@ -28,29 +32,22 @@ class ColorWidget extends ControlUIWidget {
     [error]="error"
     [showTitle]="schema.title"
   >
-    <nz-color-block *ngIf="ui.block" [nzColor]="value" [nzSize]="$any(ui.size)" />
-    <nz-color-picker
-      *ngIf="!ui.block"
+    <nz-segmented
       [ngModel]="value"
       (ngModelChange)="setValue($event)"
       [nzDisabled]="disabled"
       [nzSize]="$any(ui.size)"
-      [nzDefaultValue]="ui.defaultValue ?? ''"
-      [nzFormat]="ui.format ?? null"
-      [nzTrigger]="ui.trigger ?? 'click'"
-      [nzTitle]="ui.title ?? ''"
-      [nzFlipFlop]="$any(ui.flipFlop)"
-      [nzShowText]="ui.showText"
-      [nzAllowClear]="ui.allowClear"
-      (nzOnChange)="_change($event)"
-      (nzOnFormatChange)="_formatChange($event)"
+      [nzBlock]="ui.block ?? false"
+      [nzOptions]="list"
+      [nzLabelTemplate]="ui.labelTemplate ?? null"
+      (nzValueChange)="valueChange($event)"
     />
-  </sf-item-wrap>`, isInline: true, dependencies: [{ kind: "directive", type: i1.NgControlStatus, selector: "[formControlName],[ngModel],[formControl]" }, { kind: "directive", type: i1.NgModel, selector: "[ngModel]:not([formControlName]):not([formControl])", inputs: ["name", "disabled", "ngModel", "ngModelOptions"], outputs: ["ngModelChange"], exportAs: ["ngModel"] }, { kind: "directive", type: i2.NgIf, selector: "[ngIf]", inputs: ["ngIf", "ngIfThen", "ngIfElse"] }, { kind: "component", type: i1$1.SFItemWrapComponent, selector: "sf-item-wrap", inputs: ["id", "schema", "ui", "showError", "error", "showTitle", "title"] }, { kind: "component", type: i4.NzColorPickerComponent, selector: "nz-color-picker", inputs: ["nzFormat", "nzValue", "nzSize", "nzDefaultValue", "nzTrigger", "nzTitle", "nzFlipFlop", "nzShowText", "nzOpen", "nzAllowClear", "nzDisabled"], outputs: ["nzOnChange", "nzOnFormatChange", "nzOnClear", "nzOnOpenChange"], exportAs: ["NzColorPicker"] }, { kind: "component", type: i4.NzColorBlockComponent, selector: "nz-color-block", inputs: ["nzColor", "nzSize"], outputs: ["nzOnClick"], exportAs: ["NzColorBlock"] }], encapsulation: i0.ViewEncapsulation.None }); }
+  </sf-item-wrap>`, isInline: true, dependencies: [{ kind: "directive", type: i1.NgControlStatus, selector: "[formControlName],[ngModel],[formControl]" }, { kind: "directive", type: i1.NgModel, selector: "[ngModel]:not([formControlName]):not([formControl])", inputs: ["name", "disabled", "ngModel", "ngModelOptions"], outputs: ["ngModelChange"], exportAs: ["ngModel"] }, { kind: "component", type: i1$1.SFItemWrapComponent, selector: "sf-item-wrap", inputs: ["id", "schema", "ui", "showError", "error", "showTitle", "title"] }, { kind: "component", type: i3.NzSegmentedComponent, selector: "nz-segmented", inputs: ["nzBlock", "nzDisabled", "nzOptions", "nzSize", "nzLabelTemplate"], outputs: ["nzValueChange"], exportAs: ["nzSegmented"] }], encapsulation: i0.ViewEncapsulation.None }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: ColorWidget, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidget, decorators: [{
             type: Component,
             args: [{
-                    selector: 'sf-color',
+                    selector: 'sf-segmented',
                     template: `<sf-item-wrap
     [id]="id"
     [schema]="schema"
@@ -59,22 +56,15 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImpo
     [error]="error"
     [showTitle]="schema.title"
   >
-    <nz-color-block *ngIf="ui.block" [nzColor]="value" [nzSize]="$any(ui.size)" />
-    <nz-color-picker
-      *ngIf="!ui.block"
+    <nz-segmented
       [ngModel]="value"
       (ngModelChange)="setValue($event)"
       [nzDisabled]="disabled"
       [nzSize]="$any(ui.size)"
-      [nzDefaultValue]="ui.defaultValue ?? ''"
-      [nzFormat]="ui.format ?? null"
-      [nzTrigger]="ui.trigger ?? 'click'"
-      [nzTitle]="ui.title ?? ''"
-      [nzFlipFlop]="$any(ui.flipFlop)"
-      [nzShowText]="ui.showText"
-      [nzAllowClear]="ui.allowClear"
-      (nzOnChange)="_change($event)"
-      (nzOnFormatChange)="_formatChange($event)"
+      [nzBlock]="ui.block ?? false"
+      [nzOptions]="list"
+      [nzLabelTemplate]="ui.labelTemplate ?? null"
+      (nzValueChange)="valueChange($event)"
     />
   </sf-item-wrap>`,
                     preserveWhitespaces: false,
@@ -82,19 +72,19 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImpo
                 }]
         }] });
 
-class ColorWidgetModule {
+class SegmentedWidgetModule {
     constructor(widgetRegistry) {
-        widgetRegistry.register(ColorWidget.KEY, ColorWidget);
+        widgetRegistry.register(SegmentedWidget.KEY, SegmentedWidget);
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: ColorWidgetModule, deps: [{ token: i1$1.WidgetRegistry }], target: i0.ɵɵFactoryTarget.NgModule }); }
-    static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "16.2.10", ngImport: i0, type: ColorWidgetModule, declarations: [ColorWidget], imports: [FormsModule, CommonModule, DelonFormModule, NzColorPickerModule] }); }
-    static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: ColorWidgetModule, imports: [FormsModule, CommonModule, DelonFormModule, NzColorPickerModule] }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidgetModule, deps: [{ token: i1$1.WidgetRegistry }], target: i0.ɵɵFactoryTarget.NgModule }); }
+    static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidgetModule, declarations: [SegmentedWidget], imports: [FormsModule, DelonFormModule, NzSegmentedModule] }); }
+    static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidgetModule, imports: [FormsModule, DelonFormModule, NzSegmentedModule] }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: ColorWidgetModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImport: i0, type: SegmentedWidgetModule, decorators: [{
             type: NgModule,
             args: [{
-                    imports: [FormsModule, CommonModule, DelonFormModule, NzColorPickerModule],
-                    declarations: [ColorWidget]
+                    imports: [FormsModule, DelonFormModule, NzSegmentedModule],
+                    declarations: [SegmentedWidget]
                 }]
         }], ctorParameters: function () { return [{ type: i1$1.WidgetRegistry }]; } });
 
@@ -102,5 +92,5 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.10", ngImpo
  * Generated bundle index. Do not edit.
  */
 
-export { ColorWidget, ColorWidgetModule };
+export { SegmentedWidget, SegmentedWidgetModule };
 //# sourceMappingURL=widgets-color.mjs.map
