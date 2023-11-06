8c6e4165d4da41d3e22c6f30a131a7d798a80cb4
卡色 <cipchk@qq.com>
2023-11-06 07:01:04 +0000

    master - 042d89c5b Merge d7aa0347f4469c52eaa44385f5c6c8599bf9e875 into 66e612dd07144c2e04f0e4a73d7300d9a1e4e191

--- @delon/mock/src/interface.d.ts
+++ @delon/mock/src/interface.d.ts
@@ -1,5 +1,6 @@
 import type { HttpRequest } from '@angular/common/http';
 import type { Observable } from 'rxjs';
+export type MockCallback = any | Observable<any> | Promise<any>;
 export declare class MockOptions {
     data?: any;
 }
@@ -9,7 +10,7 @@ export interface MockCachedRule {
     url: string;
     martcher: RegExp | null;
     segments: string[];
-    callback(req: MockRequest): any;
+    callback(req: MockRequest): MockCallback;
 }
 export interface MockRule {
     [key: string]: any;
@@ -17,7 +18,7 @@ export interface MockRule {
     url: string;
     /** 路由参数 */
     params?: any;
-    callback(req: MockRequest): any | Observable<any> | Promise<any>;
+    callback(req: MockRequest): MockCallback;
 }
 export interface MockRequest {
     /** 路由参数 */
