0207e00b3c66de196231207300770472a9fbf183
marbleqi <marbleqi@163.com>
2023-11-13 04:28:54 +0000

    master - aa4f7e229 Merge 7ace736d2b7945df4b7bf3294722254875252c7f into 2f85357887df11ee1b8bc229c2c724474607843c

--- ng-alain/application/files/src/app/layout/basic/basic.component.ts
+++ ng-alain/application/files/src/app/layout/basic/basic.component.ts
@@ -23,7 +23,7 @@ import { environment } from '@env/environment';
         </div>
       </layout-default-header-item>
       <layout-default-header-item direction="middle">
-        <header-search class="alain-default__search" [toggleChange]="searchToggleStatus"></header-search>
+        <header-search class="alain-default__search" [toggleChange]="searchToggleStatus" />
       </layout-default-header-item>
       <layout-default-header-item direction="right" hidden="mobile">
         <div layout-default-header-item-trigger nz-dropdown [nzDropdownMenu]="settingsMenu" nzTrigger="click" nzPlacement="bottomRight">
@@ -38,7 +38,7 @@ import { environment } from '@env/environment';
               <header-clear-storage />
             </div><% if (i18n) { %>
             <div nz-menu-item>
-              <header-i18n></header-i18n>
+              <header-i18n />
             </div><% } %>
           </div>
         </nz-dropdown-menu>
@@ -48,7 +48,7 @@ import { environment } from '@env/environment';
       </layout-default-header-item>
       <ng-template #asideUserTpl>
         <div nz-dropdown nzTrigger="click" [nzDropdownMenu]="userMenu" class="alain-default__aside-user">
-          <nz-avatar class="alain-default__aside-user-avatar" [nzSrc]="user.avatar"></nz-avatar>
+          <nz-avatar class="alain-default__aside-user-avatar" [nzSrc]="user.avatar" />
           <div class="alain-default__aside-user-info">
             <strong>{{ user.name }}</strong>
             <p class="mb0">{{ user.email }}</p>
@@ -65,15 +65,14 @@ import { environment } from '@env/environment';
         <router-outlet />
       </ng-template>
     </layout-default>
-
     <setting-drawer *ngIf="showSettingDrawer" />
     <theme-btn />
-  `,
+  `
 })
 export class LayoutBasicComponent {
   options: LayoutDefaultOptions = {
     logoExpanded: `./assets/logo-full.svg`,
-    logoCollapsed: `./assets/logo.svg`,
+    logoCollapsed: `./assets/logo.svg`
   };
   searchToggleStatus = false;
   showSettingDrawer = !environment.production;
