ec12dd92fa3f941ee010a42875a490d705e16422
卡色 <cipchk@qq.com>
2023-11-24 04:14:38 +0000

    master - 402ea19bf Merge 3ab12ceb61ae46ff3400305354733d25522ecb6f into fa4dc948236ca02e3de9aa731b1cde4b45959929

--- @delon/abc/cell/provide.d.ts
+++ @delon/abc/cell/provide.d.ts
@@ -1,9 +1,10 @@
 import { EnvironmentProviders } from '@angular/core';
 import type { NzSafeAny } from 'ng-zorro-antd/core/types';
+export interface CellWidgetProvideConfig {
+    KEY: string;
+    type: NzSafeAny;
+}
 /**
  * Just only using Standalone widgets
  */
-export declare function provideCellWidgets(...widgets: Array<{
-    KEY: string;
-    type: NzSafeAny;
-}>): EnvironmentProviders;
+export declare function provideCellWidgets(...widgets: CellWidgetProvideConfig[]): EnvironmentProviders;
--- @delon/abc/st/provide.d.ts
+++ @delon/abc/st/provide.d.ts
@@ -1,9 +1,10 @@
 import { EnvironmentProviders } from '@angular/core';
 import type { NzSafeAny } from 'ng-zorro-antd/core/types';
+export interface STWidgetProvideConfig {
+    KEY: string;
+    type: NzSafeAny;
+}
 /**
  * Just only using Standalone widgets
  */
-export declare function provideSTWidgets(...widgets: Array<{
-    KEY: string;
-    type: NzSafeAny;
-}>): EnvironmentProviders;
+export declare function provideSTWidgets(...widgets: STWidgetProvideConfig[]): EnvironmentProviders;
