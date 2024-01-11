2f1503f334dfe830e33ffe81239a714a79042078
cipchk <cipchk@qq.com>
2024-01-11 08:47:31 +0000

    master - 3fefbc3 feat(abc:ellipsis): support standalone

--- @delon/abc/ellipsis/ellipsis.module.d.ts
+++ @delon/abc/ellipsis/ellipsis.module.d.ts
@@ -1,10 +1,10 @@
 import * as i0 from "@angular/core";
-import * as i1 from "./ellipsis.component";
-import * as i2 from "@angular/common";
-import * as i3 from "@angular/cdk/observers";
-import * as i4 from "ng-zorro-antd/tooltip";
+import * as i1 from "@angular/common";
+import * as i2 from "@angular/cdk/observers";
+import * as i3 from "ng-zorro-antd/tooltip";
+import * as i4 from "./ellipsis.component";
 export declare class EllipsisModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<EllipsisModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<EllipsisModule, [typeof i1.EllipsisComponent], [typeof i2.CommonModule, typeof i3.ObserversModule, typeof i4.NzToolTipModule], [typeof i1.EllipsisComponent]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<EllipsisModule, never, [typeof i1.CommonModule, typeof i2.ObserversModule, typeof i3.NzToolTipModule, typeof i4.EllipsisComponent], [typeof i4.EllipsisComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<EllipsisModule>;
 }
