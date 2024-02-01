16767e48fba736b4505bbcc357c96443d3fbf36b
卡色 <cipchk@qq.com>
2024-02-01 10:25:31 +0000

    master - 837f4f0 feat(cache): add `CacheInterceptor` (#1576)

---
+++ @delon/cache/src/cache.interceptor.d.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+/**
+ * Cache interceptor
+ *
+ * 缓存拦截器
+ *
+ * @example
+ * provideHttpClient(withInterceptors([httpCacheInterceptor])),
+ */
+export declare const httpCacheInterceptor: HttpInterceptorFn;
