c0e4c26ed196286f27a43aeb870f6d08d3cbc868
卡色 <cipchk@qq.com>
2024-03-18 07:06:28 +0000

    master - fix(abc:st): fix resizeable will trigger sorting or filter

--- @delon/abc/cell/cell-host.directive.d.ts
+++ @delon/abc/cell/cell-host.directive.d.ts
@@ -1,11 +1,11 @@
-import { OnChanges } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { CellTextResult } from './cell.types';
 import * as i0 from "@angular/core";
-export declare class CellHostDirective implements OnChanges {
+export declare class CellHostDirective implements OnInit {
     private readonly srv;
-    private readonly vcr;
+    private readonly viewContainerRef;
     data: CellTextResult;
-    ngOnChanges(): void;
+    ngOnInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CellHostDirective, never>;
     static ɵdir: i0.ɵɵDirectiveDeclaration<CellHostDirective, "[cell-widget-host]", never, { "data": { "alias": "data"; "required": false; }; }, {}, never, never, true, never>;
 }
