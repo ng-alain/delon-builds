55cdaf7ba0f7087796d810281950de81a31688f0
卡色 <cipchk@qq.com>
2024-08-02 14:52:14 +0000

    master - feat(theme): support custom process of `_date` pipe

--- @delon/util/config/theme/pipe.type.d.ts
+++ @delon/util/config/theme/pipe.type.d.ts
@@ -2,6 +2,6 @@ export interface AlainThemePipeConfig {
     dateFormat?: string;
     dateFormatCustom?: AlainThemePipeDateFormatCustom;
 }
-export type AlainThemePipeDateFormatCustom = (value: Date | string | number, formatString?: string | null, options?: {
+export type AlainThemePipeDateFormatCustom = (value: Date, formatString?: string | null, options?: {
     locale?: Locale;
 }) => string;
