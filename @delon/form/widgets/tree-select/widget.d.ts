2dd1e69f3ccd77139ff20ce6b4f6adbd8ff001af
卡色 <cipchk@qq.com>
2024-08-14 13:52:17 +0000

    master - feat(form:tree-select): add `change` of schema

--- @delon/form/widgets/tree-select/widget.d.ts
+++ @delon/form/widgets/tree-select/widget.d.ts
@@ -1,6 +1,7 @@
 import { OnInit } from '@angular/core';
 import { ControlUIWidget, SFSchemaEnum, SFValue } from '@delon/form';
 import { NzFormatEmitEvent } from 'ng-zorro-antd/core/tree';
+import type { NzSafeAny } from 'ng-zorro-antd/core/types';
 import type { SFTreeSelectWidgetSchema } from './schema';
 import * as i0 from "@angular/core";
 export declare class TreeSelectWidget extends ControlUIWidget<SFTreeSelectWidgetSchema> implements OnInit {
@@ -10,7 +11,7 @@ export declare class TreeSelectWidget extends ControlUIWidget<SFTreeSelectWidget
     asyncData: boolean;
     ngOnInit(): void;
     reset(value: SFValue): void;
-    change(value: string[] | string): void;
+    change(value: NzSafeAny[] | NzSafeAny): void;
     expandChange(e: NzFormatEmitEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<TreeSelectWidget, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<TreeSelectWidget, "sf-tree-select", never, {}, {}, never, never, true, never>;
