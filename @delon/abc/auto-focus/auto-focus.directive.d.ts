94d84620447f34d082598d60f3daa9dc9e7b49b3
卡色 <cipchk@qq.com>
2025-01-18 12:34:02 +0000

    master - refactor(abc:auto-focus): bump signal

--- @delon/abc/auto-focus/auto-focus.directive.d.ts
+++ @delon/abc/auto-focus/auto-focus.directive.d.ts
@@ -4,11 +4,9 @@ export declare class AutoFocusDirective implements AfterViewInit {
     private readonly el;
     private readonly platform;
     private readonly destroy$;
-    enabled: boolean;
-    delay: number;
+    enabled: import("@angular/core").InputSignalWithTransform<boolean, string | boolean | null | undefined>;
+    delay: import("@angular/core").InputSignalWithTransform<number, string | number | null | undefined>;
     ngAfterViewInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<AutoFocusDirective, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<AutoFocusDirective, "[auto-focus], input[autofocus=\"autofocus\"], textarea[autofocus=\"autofocus\"]", ["autoFocus"], { "enabled": { "alias": "enabled"; "required": false; }; "delay": { "alias": "delay"; "required": false; }; }, {}, never, never, true, never>;
-    static ngAcceptInputType_enabled: unknown;
-    static ngAcceptInputType_delay: unknown;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<AutoFocusDirective, "[auto-focus], input[autofocus=\"autofocus\"], textarea[autofocus=\"autofocus\"]", ["autoFocus"], { "enabled": { "alias": "enabled"; "required": false; "isSignal": true; }; "delay": { "alias": "delay"; "required": false; "isSignal": true; }; }, {}, never, never, true, never>;
 }
