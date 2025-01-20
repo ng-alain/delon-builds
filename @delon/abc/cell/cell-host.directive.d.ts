42bb13e096a6c60e822a1704e2a0df183b955c2b
卡色 <cipchk@qq.com>
2025-01-20 08:08:10 +0000

    master - refactor: migrate to signals

--- @delon/abc/cell/cell-host.directive.d.ts
+++ @delon/abc/cell/cell-host.directive.d.ts
@@ -1,11 +1,10 @@
-import { OnChanges } from '@angular/core';
 import { CellTextResult } from './cell.types';
 import * as i0 from "@angular/core";
-export declare class CellHostDirective implements OnChanges {
+export declare class CellHostDirective {
     private readonly srv;
     private readonly vcr;
-    data: CellTextResult;
-    ngOnChanges(): void;
+    data: import("@angular/core").InputSignal<CellTextResult>;
+    constructor();
     static ɵfac: i0.ɵɵFactoryDeclaration<CellHostDirective, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<CellHostDirective, "[cell-widget-host]", never, { "data": { "alias": "data"; "required": false; }; }, {}, never, never, true, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<CellHostDirective, "[cell-widget-host]", never, { "data": { "alias": "data"; "required": true; "isSignal": true; }; }, {}, never, never, true, never>;
 }
