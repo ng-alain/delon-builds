08531f88596e5853b3f6396c9325f8f76502b9e2
卡色 <cipchk@qq.com>
2025-06-19 11:45:10 +0000

    master - refactor(abc:footer-toolbar): migrate to signals

--- @delon/abc/exception/index.d.ts
+++ @delon/abc/exception/index.d.ts
@@ -1,47 +1,50 @@
-import * as _angular_platform_browser from '@angular/platform-browser';
-import * as _angular_cdk_bidi_module_d_IN1Vp56w from '@angular/cdk/bidi-module.d-IN1Vp56w';
-import * as _angular_core from '@angular/core';
+import { Direction } from '@angular/cdk/bidi';
+import * as i0 from '@angular/core';
+import { OnInit } from '@angular/core';
+import { SafeUrl, SafeHtml } from '@angular/platform-browser';
 import * as i4 from '@delon/theme';
 import { LocaleData } from '@delon/theme';
+import { NzSafeAny } from 'ng-zorro-antd/core/types';
 import * as i1 from '@angular/common';
 import * as i2 from '@angular/cdk/observers';
 import * as i3 from '@angular/router';
 import * as i5 from 'ng-zorro-antd/button';
 
 type ExceptionType = 403 | 404 | 500;
-declare class ExceptionComponent {
+declare class ExceptionComponent implements OnInit {
+    static ngAcceptInputType_type: ExceptionType | string;
     private readonly i18n;
     private readonly dom;
     private readonly directionality;
+    private readonly cdr;
+    private readonly destroy$;
     private readonly cogSrv;
-    private readonly conTpl;
-    locale: _angular_core.Signal<LocaleData>;
-    dir: _angular_core.Signal<_angular_cdk_bidi_module_d_IN1Vp56w.a>;
-    hasCon: _angular_core.WritableSignal<boolean>;
+    private conTpl;
+    _type: ExceptionType;
+    locale: LocaleData;
+    hasCon: boolean;
+    dir?: Direction;
+    _img: SafeUrl;
+    _title: SafeHtml;
+    _desc: SafeHtml;
     private typeDict;
-    typeItem: _angular_core.WritableSignal<{
-        img: string;
-        title: string;
-        desc?: string;
-    } | null>;
-    type: _angular_core.InputSignal<ExceptionType>;
-    img: _angular_core.InputSignal<string | undefined>;
-    title: _angular_core.InputSignal<string | undefined>;
-    desc: _angular_core.InputSignal<string | undefined>;
-    backRouterLink: _angular_core.InputSignal<string | any[]>;
-    _img: _angular_core.Signal<_angular_platform_browser.SafeStyle | null>;
-    _title: _angular_core.Signal<_angular_platform_browser.SafeHtml | null>;
-    _desc: _angular_core.Signal<_angular_platform_browser.SafeHtml | null>;
-    constructor();
+    set type(value: ExceptionType);
+    private fixImg;
+    set img(value: string);
+    set title(value: string);
+    set desc(value: string);
+    backRouterLink: string | NzSafeAny[];
     checkContent(): void;
-    static ɵfac: _angular_core.ɵɵFactoryDeclaration<ExceptionComponent, never>;
-    static ɵcmp: _angular_core.ɵɵComponentDeclaration<ExceptionComponent, "exception", ["exception"], { "type": { "alias": "type"; "required": false; "isSignal": true; }; "img": { "alias": "img"; "required": false; "isSignal": true; }; "title": { "alias": "title"; "required": false; "isSignal": true; }; "desc": { "alias": "desc"; "required": false; "isSignal": true; }; "backRouterLink": { "alias": "backRouterLink"; "required": false; "isSignal": true; }; }, {}, never, ["*"], true, never>;
+    constructor();
+    ngOnInit(): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<ExceptionComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ExceptionComponent, "exception", ["exception"], { "type": { "alias": "type"; "required": false; }; "img": { "alias": "img"; "required": false; }; "title": { "alias": "title"; "required": false; }; "desc": { "alias": "desc"; "required": false; }; "backRouterLink": { "alias": "backRouterLink"; "required": false; }; }, {}, never, ["*"], true, never>;
 }
 
 declare class ExceptionModule {
-    static ɵfac: _angular_core.ɵɵFactoryDeclaration<ExceptionModule, never>;
-    static ɵmod: _angular_core.ɵɵNgModuleDeclaration<ExceptionModule, never, [typeof i1.CommonModule, typeof i2.ObserversModule, typeof i3.RouterModule, typeof i4.DelonLocaleModule, typeof i5.NzButtonModule, typeof ExceptionComponent], [typeof ExceptionComponent]>;
-    static ɵinj: _angular_core.ɵɵInjectorDeclaration<ExceptionModule>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<ExceptionModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<ExceptionModule, never, [typeof i1.CommonModule, typeof i2.ObserversModule, typeof i3.RouterModule, typeof i4.DelonLocaleModule, typeof i5.NzButtonModule, typeof ExceptionComponent], [typeof ExceptionComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<ExceptionModule>;
 }
 
 export { ExceptionComponent, ExceptionModule };
