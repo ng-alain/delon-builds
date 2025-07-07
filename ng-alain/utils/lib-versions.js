ee84f09f134a58b9100b179dd325f8d44afb4306
卡色 <cipchk@qq.com>
2025-07-07 08:58:59 +0000

    master - release: 20.beta

--- ng-alain/utils/lib-versions.js
+++ ng-alain/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZORROVERSION = exports.VERSION = void 0;
-exports.VERSION = '^19.2.0';
+exports.VERSION = '^20.0.0-beta.0';
 exports.ZORROVERSION = '^20.0.0';
 //# sourceMappingURL=lib-versions.js.map
--- ng-alain/utils/lib-versions.ts
+++ ng-alain/utils/lib-versions.ts
@@ -1,2 +1,2 @@
-export const VERSION = '^19.2.0';
+export const VERSION = '^20.0.0-beta.0';
 export const ZORROVERSION = '^20.0.0';
