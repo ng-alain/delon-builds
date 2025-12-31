61389fbffee2d91f633c73541d3b76ea394304da
卡色 <cipchk@qq.com>
2025-12-31 13:11:14 +0000

    master - refactor(*): bump to signal

--- @delon/abc/fesm2022/quick-menu.mjs
+++ @delon/abc/fesm2022/quick-menu.mjs
@@ -1,84 +1,92 @@
 import * as i0 from '@angular/core';
-import { inject, ChangeDetectorRef, ElementRef, Renderer2, EventEmitter, booleanAttribute, numberAttribute, Output, Input, ViewEncapsulation, ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { inject, ElementRef, Renderer2, input, numberAttribute, booleanAttribute, output, signal, computed, effect, ViewEncapsulation, ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { NzStringTemplateOutletDirective, NzOutletModule } from 'ng-zorro-antd/core/outlet';
 import { NzIconDirective, NzIconModule } from 'ng-zorro-antd/icon';
 import { CommonModule } from '@angular/common';
 
 class QuickMenuComponent {
-    cdr = inject(ChangeDetectorRef);
     el = inject(ElementRef).nativeElement;
     render = inject(Renderer2);
-    ctrlStyle = {};
-    icon = 'question-circle';
-    top = 120;
-    width = 200;
-    bgColor;
-    borderColor;
-    expand = false;
-    expandChange = new EventEmitter();
-    show = false;
-    initFlag = false;
-    _click() {
-        this.show = !this.show;
-        this.expandChange.emit(this.show);
-        this.setStyle();
-    }
-    setStyle() {
-        this.ctrlStyle = {
-            'background-color': this.bgColor,
-            'border-color': this.borderColor
+    icon = input('question-circle', ...(ngDevMode ? [{ debugName: "icon" }] : []));
+    top = input(120, { ...(ngDevMode ? { debugName: "top" } : {}), transform: numberAttribute });
+    width = input(200, { ...(ngDevMode ? { debugName: "width" } : {}), transform: numberAttribute });
+    bgColor = input(...(ngDevMode ? [undefined, { debugName: "bgColor" }] : []));
+    borderColor = input(...(ngDevMode ? [undefined, { debugName: "borderColor" }] : []));
+    expand = input(false, { ...(ngDevMode ? { debugName: "expand" } : {}), transform: booleanAttribute });
+    expandChange = output();
+    show = signal(false, ...(ngDevMode ? [{ debugName: "show" }] : []));
+    ctrlStyle = computed(() => {
+        const ret = {
+            'background-color': this.bgColor(),
+            'border-color': this.borderColor()
         };
-        const res = [
-            `top:${this.top}px`,
-            `width:${this.width}px`,
-            `margin-right:-${this.show ? 0 : this.width}px`
-        ];
-        if (this.bgColor) {
-            res.push(`background-color:${this.bgColor}`);
-        }
-        if (this.borderColor) {
-            res.push(`border-color:${this.borderColor}`);
-        }
-        this.render.setAttribute(this.el, 'style', res.join(';'));
-        this.cdr.detectChanges();
+        return ret;
+    }, ...(ngDevMode ? [{ debugName: "ctrlStyle" }] : []));
+    constructor() {
+        effect(() => {
+            this.show.set(this.expand());
+        });
+        effect(() => {
+            const res = [
+                `top:${this.top()}px`,
+                `width:${this.width()}px`,
+                `margin-right:-${this.show() ? 0 : this.width()}px`
+            ];
+            if (this.bgColor) {
+                res.push(`background-color:${this.bgColor()}`);
+            }
+            if (this.borderColor) {
+                res.push(`border-color:${this.borderColor()}`);
+            }
+            this.render.setAttribute(this.el, 'style', res.join(';'));
+        });
     }
-    ngOnInit() {
-        this.initFlag = true;
-        this.setStyle();
-    }
-    ngOnChanges() {
-        this.show = this.expand;
-        if (this.initFlag) {
-            this.setStyle();
-        }
+    _click() {
+        this.show.set(!this.show());
+        this.expandChange.emit(this.show());
     }
     static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "21.0.6", ngImport: i0, type: QuickMenuComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
-    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "16.1.0", version: "21.0.6", type: QuickMenuComponent, isStandalone: true, selector: "quick-menu", inputs: { icon: "icon", top: ["top", "top", numberAttribute], width: ["width", "width", numberAttribute], bgColor: "bgColor", borderColor: "borderColor", expand: ["expand", "expand", booleanAttribute] }, outputs: { expandChange: "expandChange" }, host: { listeners: { "click": "_click()" }, properties: { "class.quick-menu": "true" } }, exportAs: ["quickMenu"], usesOnChanges: true, ngImport: i0, template: "<div class=\"quick-menu__inner\">\n  <div class=\"quick-menu__ctrl\" [style]=\"ctrlStyle\">\n    <div class=\"quick-menu__ctrl-icon\">\n      <ng-container *nzStringTemplateOutlet=\"icon\">\n        <nz-icon [nzType]=\"$any(icon)\" />\n      </ng-container>\n    </div>\n  </div>\n  <ng-content />\n</div>\n", dependencies: [{ kind: "directive", type: NzIconDirective, selector: "nz-icon,[nz-icon]", inputs: ["nzSpin", "nzRotate", "nzType", "nzTheme", "nzTwotoneColor", "nzIconfont"], exportAs: ["nzIcon"] }, { kind: "directive", type: NzStringTemplateOutletDirective, selector: "[nzStringTemplateOutlet]", inputs: ["nzStringTemplateOutletContext", "nzStringTemplateOutlet"], exportAs: ["nzStringTemplateOutlet"] }], changeDetection: i0.ChangeDetectionStrategy.OnPush, encapsulation: i0.ViewEncapsulation.None });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "17.1.0", version: "21.0.6", type: QuickMenuComponent, isStandalone: true, selector: "quick-menu", inputs: { icon: { classPropertyName: "icon", publicName: "icon", isSignal: true, isRequired: false, transformFunction: null }, top: { classPropertyName: "top", publicName: "top", isSignal: true, isRequired: false, transformFunction: null }, width: { classPropertyName: "width", publicName: "width", isSignal: true, isRequired: false, transformFunction: null }, bgColor: { classPropertyName: "bgColor", publicName: "bgColor", isSignal: true, isRequired: false, transformFunction: null }, borderColor: { classPropertyName: "borderColor", publicName: "borderColor", isSignal: true, isRequired: false, transformFunction: null }, expand: { classPropertyName: "expand", publicName: "expand", isSignal: true, isRequired: false, transformFunction: null } }, outputs: { expandChange: "expandChange" }, host: { listeners: { "click": "_click()" }, classAttribute: "quick-menu" }, exportAs: ["quickMenu"], ngImport: i0, template: `
+    <div class="quick-menu__inner">
+      <div class="quick-menu__ctrl" [style]="ctrlStyle()">
+        <div class="quick-menu__ctrl-icon">
+          @let ic = icon();
+          <ng-container *nzStringTemplateOutlet="ic">
+            <nz-icon [nzType]="$any(ic)" />
+          </ng-container>
+        </div>
+      </div>
+      <ng-content />
+    </div>
+  `, isInline: true, dependencies: [{ kind: "directive", type: NzIconDirective, selector: "nz-icon,[nz-icon]", inputs: ["nzSpin", "nzRotate", "nzType", "nzTheme", "nzTwotoneColor", "nzIconfont"], exportAs: ["nzIcon"] }, { kind: "directive", type: NzStringTemplateOutletDirective, selector: "[nzStringTemplateOutlet]", inputs: ["nzStringTemplateOutletContext", "nzStringTemplateOutlet"], exportAs: ["nzStringTemplateOutlet"] }], changeDetection: i0.ChangeDetectionStrategy.OnPush, encapsulation: i0.ViewEncapsulation.None });
 }
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "21.0.6", ngImport: i0, type: QuickMenuComponent, decorators: [{
             type: Component,
-            args: [{ selector: 'quick-menu', exportAs: 'quickMenu', host: {
-                        '[class.quick-menu]': 'true',
+            args: [{
+                    selector: 'quick-menu',
+                    exportAs: 'quickMenu',
+                    template: `
+    <div class="quick-menu__inner">
+      <div class="quick-menu__ctrl" [style]="ctrlStyle()">
+        <div class="quick-menu__ctrl-icon">
+          @let ic = icon();
+          <ng-container *nzStringTemplateOutlet="ic">
+            <nz-icon [nzType]="$any(ic)" />
+          </ng-container>
+        </div>
+      </div>
+      <ng-content />
+    </div>
+  `,
+                    host: {
+                        class: 'quick-menu',
                         '(click)': '_click()'
-                    }, changeDetection: ChangeDetectionStrategy.OnPush, encapsulation: ViewEncapsulation.None, imports: [NzIconDirective, NzStringTemplateOutletDirective], template: "<div class=\"quick-menu__inner\">\n  <div class=\"quick-menu__ctrl\" [style]=\"ctrlStyle\">\n    <div class=\"quick-menu__ctrl-icon\">\n      <ng-container *nzStringTemplateOutlet=\"icon\">\n        <nz-icon [nzType]=\"$any(icon)\" />\n      </ng-container>\n    </div>\n  </div>\n  <ng-content />\n</div>\n" }]
-        }], propDecorators: { icon: [{
-                type: Input
-            }], top: [{
-                type: Input,
-                args: [{ transform: numberAttribute }]
-            }], width: [{
-                type: Input,
-                args: [{ transform: numberAttribute }]
-            }], bgColor: [{
-                type: Input
-            }], borderColor: [{
-                type: Input
-            }], expand: [{
-                type: Input,
-                args: [{ transform: booleanAttribute }]
-            }], expandChange: [{
-                type: Output
-            }] } });
+                    },
+                    changeDetection: ChangeDetectionStrategy.OnPush,
+                    encapsulation: ViewEncapsulation.None,
+                    imports: [NzIconDirective, NzStringTemplateOutletDirective]
+                }]
+        }], ctorParameters: () => [], propDecorators: { icon: [{ type: i0.Input, args: [{ isSignal: true, alias: "icon", required: false }] }], top: [{ type: i0.Input, args: [{ isSignal: true, alias: "top", required: false }] }], width: [{ type: i0.Input, args: [{ isSignal: true, alias: "width", required: false }] }], bgColor: [{ type: i0.Input, args: [{ isSignal: true, alias: "bgColor", required: false }] }], borderColor: [{ type: i0.Input, args: [{ isSignal: true, alias: "borderColor", required: false }] }], expand: [{ type: i0.Input, args: [{ isSignal: true, alias: "expand", required: false }] }], expandChange: [{ type: i0.Output, args: ["expandChange"] }] } });
 
 const COMPONENTS = [QuickMenuComponent];
 class QuickMenuModule {
--- @delon/abc/types/quick-menu.d.ts
+++ @delon/abc/types/quick-menu.d.ts
@@ -1,38 +1,31 @@
-import * as i0 from '@angular/core';
-import { OnInit, OnChanges, TemplateRef, EventEmitter } from '@angular/core';
+import * as _angular_core from '@angular/core';
+import { TemplateRef } from '@angular/core';
 import * as i1 from '@angular/common';
 import * as i2 from 'ng-zorro-antd/icon';
 import * as i3 from 'ng-zorro-antd/core/outlet';
 
-declare class QuickMenuComponent implements OnInit, OnChanges {
-    private readonly cdr;
+declare class QuickMenuComponent {
     private readonly el;
     private readonly render;
-    ctrlStyle: Record<string, string | undefined>;
-    icon: string | TemplateRef<void>;
-    top: number;
-    width: number;
-    bgColor?: string;
-    borderColor?: string;
-    expand: boolean;
-    readonly expandChange: EventEmitter<boolean>;
+    readonly icon: _angular_core.InputSignal<string | TemplateRef<void>>;
+    readonly top: _angular_core.InputSignalWithTransform<number, unknown>;
+    readonly width: _angular_core.InputSignalWithTransform<number, unknown>;
+    readonly bgColor: _angular_core.InputSignal<string | undefined>;
+    readonly borderColor: _angular_core.InputSignal<string | undefined>;
+    readonly expand: _angular_core.InputSignalWithTransform<boolean, unknown>;
+    readonly expandChange: _angular_core.OutputEmitterRef<boolean>;
     private show;
-    private initFlag;
+    protected ctrlStyle: _angular_core.Signal<Record<string, string | undefined>>;
+    constructor();
     _click(): void;
-    private setStyle;
-    ngOnInit(): void;
-    ngOnChanges(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<QuickMenuComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<QuickMenuComponent, "quick-menu", ["quickMenu"], { "icon": { "alias": "icon"; "required": false; }; "top": { "alias": "top"; "required": false; }; "width": { "alias": "width"; "required": false; }; "bgColor": { "alias": "bgColor"; "required": false; }; "borderColor": { "alias": "borderColor"; "required": false; }; "expand": { "alias": "expand"; "required": false; }; }, { "expandChange": "expandChange"; }, never, ["*"], true, never>;
-    static ngAcceptInputType_top: unknown;
-    static ngAcceptInputType_width: unknown;
-    static ngAcceptInputType_expand: unknown;
+    static ɵfac: _angular_core.ɵɵFactoryDeclaration<QuickMenuComponent, never>;
+    static ɵcmp: _angular_core.ɵɵComponentDeclaration<QuickMenuComponent, "quick-menu", ["quickMenu"], { "icon": { "alias": "icon"; "required": false; "isSignal": true; }; "top": { "alias": "top"; "required": false; "isSignal": true; }; "width": { "alias": "width"; "required": false; "isSignal": true; }; "bgColor": { "alias": "bgColor"; "required": false; "isSignal": true; }; "borderColor": { "alias": "borderColor"; "required": false; "isSignal": true; }; "expand": { "alias": "expand"; "required": false; "isSignal": true; }; }, { "expandChange": "expandChange"; }, never, ["*"], true, never>;
 }
 
 declare class QuickMenuModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<QuickMenuModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<QuickMenuModule, never, [typeof i1.CommonModule, typeof i2.NzIconModule, typeof i3.NzOutletModule, typeof QuickMenuComponent], [typeof QuickMenuComponent]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<QuickMenuModule>;
+    static ɵfac: _angular_core.ɵɵFactoryDeclaration<QuickMenuModule, never>;
+    static ɵmod: _angular_core.ɵɵNgModuleDeclaration<QuickMenuModule, never, [typeof i1.CommonModule, typeof i2.NzIconModule, typeof i3.NzOutletModule, typeof QuickMenuComponent], [typeof QuickMenuComponent]>;
+    static ɵinj: _angular_core.ɵɵInjectorDeclaration<QuickMenuModule>;
 }
 
 export { QuickMenuComponent, QuickMenuModule };
